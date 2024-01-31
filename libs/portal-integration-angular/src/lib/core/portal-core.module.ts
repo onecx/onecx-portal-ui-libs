e55f9980fec962b4ecd94ff9082de591bdc4cc75
kim.tran <kim.tran@capgemini.com>
2024-01-31 10:36:38 +0100

    fix: use attr.aria-label to avoid cant bind since it isn't a known property

--- libs/portal-integration-angular/src/lib/core/portal-core.module.ts
+++ libs/portal-integration-angular/src/lib/core/portal-core.module.ts
@@ -167,7 +167,6 @@ export class PortalMissingTranslationHandler implements MissingTranslationHandle
     {
       provide: LOCALE_ID,
       useFactory: (userService: UserService) => {
-        console.log('Using locale: ' + userService.lang$.getValue())
         return userService.lang$.getValue()
       },
       deps: [UserService],
