bcbdb6dfcdd8e3b640b19588219d995d12fdfcac
Jan-Gerrit Schettler-Koehler <jan-gerrit.schettler-koehler@capgemini.com>
2024-02-01 10:47:07 +0100

    fix: linter issues

--- libs/portal-integration-angular/src/lib/core/utils/caching-translate-loader.utils.spec.ts
+++ libs/portal-integration-angular/src/lib/core/utils/caching-translate-loader.utils.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http'
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 import { of } from 'rxjs'
 import { TranslationCacheService } from '../../services/translation-cache.service'
