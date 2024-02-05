06ec0517c974e2477c1996547b49a1e2b4f2f89f
Bastian Jakobi <bastian.jakobi@capgemini.com>
2024-02-05 12:38:58 +0100

    test: implement page header tests based on harness

--- libs/portal-integration-angular/src/lib/core/components/page-header/page-header.component.spec.ts
+++ libs/portal-integration-angular/src/lib/core/components/page-header/page-header.component.spec.ts
@@ -3,7 +3,6 @@ import { Action, PageHeaderComponent } from './page-header.component'
 import { RouterTestingModule } from '@angular/router/testing'
 import { ConfigurationService } from '../../../services/configuration.service'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
-import { Component } from '@angular/core'
 import { TranslateTestingModule } from 'ngx-translate-testing'
 import { BreadcrumbModule } from 'primeng/breadcrumb'
 import { MenuModule } from 'primeng/menu'
@@ -11,6 +10,9 @@ import { ButtonModule } from 'primeng/button'
 import { AppStateService } from '../../../services/app-state.service'
 import { UserService } from '../../../services/user.service'
 import { MockUserService } from '../../../../../mocks/mock-user-service'
+import { PageHeaderHarness, TestbedHarnessEnvironment } from '../../../../../testing'
+import { DynamicPipe } from '../../pipes/dynamic.pipe'
+import { PrimeIcons } from 'primeng/api'
 
 const mockActions: Action[] = [
   {
@@ -30,15 +32,6 @@ const mockActions: Action[] = [
     permission: 'TEST#TEST_PERMISSION',
   },
 ]
-
-// Mock host component that's used in our testBed instead of ocx-page-header
-// Using this mock host allows us to simulate Angular @Input mechanisms
-@Component({
-  template: '<ocx-page-header [actions]="actions"></ocx-page-header>',
-})
-class TestHostComponent {
-  actions: Action[] | undefined
-}
 
 describe('PageHeaderComponent', () => {
   const origAddEventListener = window.addEventListener
@@ -63,13 +56,14 @@ describe('PageHeaderComponent', () => {
     window.postMessage = origPostMessage
   })
 
-  let component: TestHostComponent
-  let fixture: ComponentFixture<TestHostComponent>
+  let component: PageHeaderComponent
+  let fixture: ComponentFixture<PageHeaderComponent>
+  let pageHeaderHarness: PageHeaderHarness
   let userServiceSpy: jest.SpyInstance<boolean, [permissionKey: string], any>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PageHeaderComponent, TestHostComponent],
+      declarations: [PageHeaderComponent, PageHeaderComponent, DynamicPipe],
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
@@ -79,7 +73,7 @@ describe('PageHeaderComponent', () => {
         }),
         BreadcrumbModule,
         MenuModule,
-        ButtonModule,
+        ButtonModule
       ],
       providers: [ConfigurationService, AppStateService, { provide: UserService, useClass: MockUserService }],
     }).compileComponents()
@@ -93,66 +87,143 @@ describe('PageHeaderComponent', () => {
     })
   })
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestHostComponent)
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(PageHeaderComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
+    pageHeaderHarness = await TestbedHarnessEnvironment.harnessForFixture(fixture, PageHeaderHarness)
     const userService = fixture.debugElement.injector.get(UserService)
     userServiceSpy = jest.spyOn(userService, 'hasPermission')
   })
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy()
-    expect(fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-wrapper"]')).toHaveLength(
-      1
-    )
+    const pageHeaderWrapper = await pageHeaderHarness.getPageHeaderWrapperHarness()
+    expect(pageHeaderWrapper).toBeTruthy()
   })
 
-  it('should check permissions and render buttons accordingly', () => {
+  it('should check permissions and render buttons accordingly', async () => {
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-inline-action-button"]')
+      await pageHeaderHarness.getInlineActionButtons()
     ).toHaveLength(0)
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-overflow-action-button"]')
+      await pageHeaderHarness.getOverflowActionButtons()
     ).toHaveLength(0)
 
     component.actions = mockActions
     fixture.detectChanges()
 
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-inline-action-button"]')
+      await pageHeaderHarness.getInlineActionButtons()
     ).toHaveLength(1)
-    expect(fixture.debugElement.nativeElement.querySelector('[title="My Test Action"]')).toBeTruthy()
+    expect(await pageHeaderHarness.getElementByTitle('My Test Action')).toBeTruthy()
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-overflow-action-button"]')
+      await pageHeaderHarness.getOverflowActionButtons()
     ).toHaveLength(1)
-    expect(fixture.debugElement.nativeElement.querySelector('[title="More actions"]')).toBeTruthy()
-    expect(userServiceSpy).toHaveBeenCalledTimes(4)
+    expect(await pageHeaderHarness.getElementByTitle('More actions')).toBeTruthy()
+    expect(userServiceSpy).toHaveBeenCalledTimes(2)
   })
 
-  it("should check permissions and not render button that user isn't allowed to see", () => {
+  it("should check permissions and not render button that user isn't allowed to see", async () => {
     userServiceSpy.mockClear()
 
     userServiceSpy.mockReturnValue(false)
 
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-inline-action-button"]')
+      await pageHeaderHarness.getInlineActionButtons()
     ).toHaveLength(0)
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-overflow-action-button"]')
+      await pageHeaderHarness.getOverflowActionButtons()
     ).toHaveLength(0)
 
     component.actions = mockActions
     fixture.detectChanges()
 
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-inline-action-button"]')
+      await pageHeaderHarness.getInlineActionButtons()
     ).toHaveLength(0)
-    expect(fixture.debugElement.nativeElement.querySelector('[title="My Test Action"]')).toBeFalsy()
+    expect(await pageHeaderHarness.getElementByTitle('My Test Action')).toBeFalsy()
     expect(
-      fixture.debugElement.nativeElement.querySelectorAll('[data-testid="ocx-page-header-overflow-action-button"]')
+      await pageHeaderHarness.getOverflowActionButtons()
     ).toHaveLength(0)
-    expect(fixture.debugElement.nativeElement.querySelector('[title="More actions"]')).toBeFalsy()
-    expect(userServiceSpy).toHaveBeenCalledTimes(4)
+    expect(await pageHeaderHarness.getElementByTitle('More actions')).toBeFalsy()
+    expect(userServiceSpy).toHaveBeenCalledTimes(2)
+  })
+
+  it("should render objectDetails as object info in the page header", async () => {
+    const objectDetailsWithoutIcons = [
+      {
+        label: 'Venue',
+        value: 'AIE Munich',
+      },
+      {
+        label: 'Status',
+        value: 'Confirmed',
+      },
+    ]
+    expect((await pageHeaderHarness.getObjectInfos()).length).toEqual(0)
+
+    component.objectDetails = objectDetailsWithoutIcons
+    
+    expect((await pageHeaderHarness.getObjectInfos()).length).toEqual(2)
+    const objectDetailLabels = await pageHeaderHarness.getObjectDetailLabels()
+    const objectDetailValues = await pageHeaderHarness.getObjectDetailValues()
+    const objectDetailIcons = await pageHeaderHarness.getObjectDetailIcons()
+    expect(objectDetailLabels.length).toEqual(2)
+    expect(objectDetailValues.length).toEqual(2)
+    expect(objectDetailIcons.length).toEqual(0)
+
+    objectDetailLabels.forEach(async (label, i) => {
+      expect(await label.text()).toEqual(objectDetailsWithoutIcons[i].label)
+    })
+
+    objectDetailValues.forEach(async (value, i) => {
+      expect(await value.text()).toEqual(objectDetailsWithoutIcons[i].value)
+    })
+  })
+
+  it("should render objectDetails with icons as object info in the page header", async () => {
+    const objectDetailsWithIcons = [
+      {
+        label: 'Venue',
+        value: 'AIE Munich',
+      },
+      {
+        label: 'Status',
+        value: 'Confirmed',
+        icon: PrimeIcons.CHECK
+      },
+      {
+        label: 'Done?',
+        icon: PrimeIcons.EXCLAMATION_CIRCLE
+      },
+      {
+        label: 'Empty'
+      }
+    ]
+    expect((await pageHeaderHarness.getObjectInfos()).length).toEqual(0)
+
+    component.objectDetails = objectDetailsWithIcons
+    
+    expect((await pageHeaderHarness.getObjectInfos()).length).toEqual(4)
+    const objectDetailLabels = await pageHeaderHarness.getObjectDetailLabels()
+    const objectDetailValues = await pageHeaderHarness.getObjectDetailValues()
+    const objectDetailIcons = await pageHeaderHarness.getObjectDetailIcons()
+    expect(objectDetailLabels.length).toEqual(4)
+    expect(objectDetailValues.length).toEqual(3)
+    expect(objectDetailIcons.length).toEqual(2)
+
+    objectDetailLabels.forEach(async (label, i) => {
+      expect(await label.text()).toEqual(objectDetailsWithIcons[i].label)
+    })
+
+    objectDetailValues.forEach(async (value, i) => {
+      if(objectDetailsWithIcons[i].value) {
+        expect(await value.text()).toEqual(objectDetailsWithIcons[i].value)
+      }
+    })
+
+    expect(await objectDetailIcons[0].getAttribute('class')).toEqual(objectDetailsWithIcons[1].icon)
+    expect(await objectDetailIcons[1].getAttribute('class')).toEqual(objectDetailsWithIcons[2].icon)
   })
 })
