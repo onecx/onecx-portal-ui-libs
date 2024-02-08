ea6441289966247e85576b3f7a836ab045de246d
Bastian Jakobi <bastian.jakobi@capgemini.com>
2024-02-08 13:32:08 +0100

    feat: add freeze functionality to table view

---
+++ libs/portal-integration-angular/src/lib/core/components/custom-group-column-selector/custom-group-column-selector.component.stories.ts
@@ -0,0 +1,91 @@
+import { CustomGroupColumnSelectorComponent } from './custom-group-column-selector.component';
+import { Meta, moduleMetadata, applicationConfig, StoryFn } from '@storybook/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColumnType } from '../../../model/column-type.model';
+import { DropdownModule } from 'primeng/dropdown';
+import { DialogModule } from 'primeng/dialog';
+import { PickListModule } from 'primeng/picklist';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
+
+
+type CustomGroupColumnSelectorInputs = Pick<CustomGroupColumnSelectorComponent, 'columns' | 'displayedColumns' |'actionColumnPosition' | 'stickyActionColumn' | 'dialogTitle' | 'saveButtonLabel' | 'cancelButtonLabel' | 'activeColumnsLabel' | 'inactiveColumnsLabel'>
+const CustomGroupColumnSelectorComponentSBConfig: Meta<CustomGroupColumnSelectorComponent>  = {
+    title: 'CustomGroupColumnSelectorComponent',
+    component: CustomGroupColumnSelectorComponent,
+    decorators: [
+        applicationConfig({
+            providers: [
+                importProvidersFrom(BrowserModule),
+                importProvidersFrom(BrowserAnimationsModule),
+                importProvidersFrom(TranslateModule.forRoot({})),         
+            ]
+        }),
+        moduleMetadata({
+            declarations: [CustomGroupColumnSelectorComponent],
+            imports: [
+                DropdownModule,
+                DialogModule,
+                TranslateModule,
+                PickListModule,
+                ButtonModule,
+                CheckboxModule,
+                FormsModule,
+                SelectButtonModule
+            ]
+        })
+    ]
+}
+const Template: StoryFn = (args) => ({
+    props: args,
+})
+
+const defaultComponentArgs: CustomGroupColumnSelectorInputs = {
+    columns: [
+        {
+            id: "product",
+            columnType: ColumnType.STRING,
+            nameKey: "Product",
+            sortable: false,
+        },
+        {
+            id: "amount",
+            columnType: ColumnType.NUMBER,
+            nameKey: "Amount",
+            sortable: true
+        }
+    ],
+    displayedColumns: [
+        {
+            id: "date",
+            columnType: ColumnType.DATE,
+            nameKey: "Date",
+            sortable: false,
+        },
+    ],
+    stickyActionColumn: true,
+    actionColumnPosition: 'right',
+    dialogTitle: 'Column configurator',
+    saveButtonLabel: 'Save',
+    cancelButtonLabel: 'Cancel',
+    activeColumnsLabel: 'Active',
+    inactiveColumnsLabel: 'Inactive'
+}
+
+export const Default = {
+    render: Template,
+    args: {
+        ...defaultComponentArgs,
+        actionColumnConfigChanged: ($event: any) => console.log("Action column config changed ", $event)
+    },
+    argTypes: {
+        actionColumnConfigChanged: {action: 'actionColumnConfigChanged'}
+    },
+}
+
+export default CustomGroupColumnSelectorComponentSBConfig;
--- libs/portal-integration-angular/src/lib/core/components/custom-group-column-selector/custom-group-column-selector.component.ts
+++ libs/portal-integration-angular/src/lib/core/components/custom-group-column-selector/custom-group-column-selector.component.ts
@@ -3,6 +3,10 @@ import { TranslateService } from '@ngx-translate/core'
 import { DataTableColumn } from '../../../model/data-table-column.model'
 
 export type ColumnSelectionChangedEvent = { activeColumns: DataTableColumn[] }
+export type ActionColumnChangedEvent = {
+  stickyActionColumn: boolean
+  actionColumnPosition: 'left' | 'right'
+}
 
 @Component({
   selector: 'ocx-custom-group-column-selector',
@@ -18,12 +22,38 @@ export class CustomGroupColumnSelectorComponent {
   @Input() cancelButtonLabel = ''
   @Input() activeColumnsLabel = ''
   @Input() inactiveColumnsLabel = ''
+  @Input() stickyActionColumn = false
+  @Input() actionColumnPosition: 'left' | 'right' = 'right'
 
   @Output() columnSelectionChanged: EventEmitter<ColumnSelectionChangedEvent> = new EventEmitter()
+  @Output() actionColumnConfigChanged: EventEmitter<ActionColumnChangedEvent> = new EventEmitter()
 
   hiddenColumnsModel: DataTableColumn[] = []
   displayedColumnsModel: DataTableColumn[] = []
+  stickyActionColumnModel = false
+  actionColumnPositionModel: 'left' | 'right' = 'right'
   visible = false
+  alignmentOptions = [
+    {
+      label: 'OCX_CUSTOM_GROUP_COLUMN_SELECTOR.CONFIGURE_ACTION_COLUMN.LEFT',
+      value: 'left',
+    },
+    {
+      label: 'OCX_CUSTOM_GROUP_COLUMN_SELECTOR.CONFIGURE_ACTION_COLUMN.RIGHT',
+      value: 'right',
+    },
+  ]
+
+  stickyOptions = [
+    {
+      label: 'OCX_CUSTOM_GROUP_COLUMN_SELECTOR.CONFIGURE_ACTION_COLUMN.YES',
+      value: true,
+    },
+    {
+      label: 'OCX_CUSTOM_GROUP_COLUMN_SELECTOR.CONFIGURE_ACTION_COLUMN.NO',
+      value: false,
+    },
+  ]
 
   constructor(private translate: TranslateService) {}
 
@@ -32,12 +62,24 @@ export class CustomGroupColumnSelectorComponent {
     this.hiddenColumnsModel = this.columns.filter(
       (column) => !this.displayedColumnsModel.map((c) => c.id).includes(column.id)
     )
+    this.stickyActionColumnModel = this.stickyActionColumn
+    this.actionColumnPositionModel = this.actionColumnPosition
     this.visible = true
   }
 
   onSaveClick() {
     this.visible = false
-    this.columnSelectionChanged.emit({ activeColumns: [...this.displayedColumnsModel] })
+    const colIdsBefore = this.displayedColumns.map(column => column.id).sort()
+    const colIdsAfter = this.displayedColumnsModel.map(column => column.id).sort()
+    if(!colIdsBefore.every((colId, i) => colId === colIdsAfter[i])) {
+      this.columnSelectionChanged.emit({ activeColumns: [...this.displayedColumnsModel] })
+    }
+    if(this.stickyActionColumn != this.stickyActionColumnModel || this.actionColumnPosition != this.actionColumnPositionModel) {
+      this.actionColumnConfigChanged.emit({
+        stickyActionColumn: this.stickyActionColumnModel,
+        actionColumnPosition: this.actionColumnPositionModel,
+      })
+    }
   }
 
   onCancelClick() {
