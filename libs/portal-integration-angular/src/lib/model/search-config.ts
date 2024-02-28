f901657e1696bdf48d24031994566c6ace89b03b
Annika Nowak <annika.nowak@capgemini.com>
2024-02-28 09:38:39 +0100

    fix: adapt SearchConfig interface

--- libs/portal-integration-angular/src/lib/model/search-config.ts
+++ libs/portal-integration-angular/src/lib/model/search-config.ts
@@ -2,8 +2,8 @@ export type SearchConfigPrimitive = string | number | bigint | boolean | Date | 
 export interface SearchConfig {
   id: string
   name: string
-  version: number
-  readonly: boolean
+  fieldListVersion: number
+  isReadonly: boolean
   isAdvanced: boolean
   values: Record<string, SearchConfigPrimitive>
 }
