a3a009d744cd8fbe6203fdd99c7f85d4b8b826d6
markuczy <129275100+markuczy@users.noreply.github.com>
2024-03-04 10:59:24 +0100

    feat: basic search to simple search with aria labels improvements (#146)
    * refactor: basic renamed to simple
    
    * feat: search header now has buttons with informational aria labels
    
    * feat: german translations
    
    * refactor: reverted naming changes
    
    * fix: basic directive error throw adjusted
    
    * feat: search-header tooltips

--- libs/portal-integration-angular/src/lib/services/portal-dialog.service.spec.ts
+++ libs/portal-integration-angular/src/lib/services/portal-dialog.service.spec.ts
@@ -209,7 +209,7 @@ describe('PortalDialogService', () => {
   let fixture: ComponentFixture<BaseTestComponent>
 
   const translations = {
-    TITLE_TRANSLATE: 'basicTitle',
+    TITLE_TRANSLATE: 'simpleTitle',
     TITLE_TRANSLATE_PARAM: 'translatedTitle {{val}}',
     MESSAGE: 'myMessage',
     MESSAGE_PARAM: 'myMessage {{val}}',
