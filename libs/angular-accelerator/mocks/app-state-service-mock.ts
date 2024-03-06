43c38f3257cbb8439b094306398ee31d501fab76
markuczy <martin.kuczynski@capgemini.com>
2024-03-06 15:07:22 +0100

    feat: working storybook target

---
+++ libs/angular-accelerator/mocks/app-state-service-mock.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core'
+import { PageInfo, Portal } from '@onecx/integration-interface'
+// eslint-disable-next-line
+import { AppStateService } from '@onecx/angular-accelerator'
+import { FakeTopic } from './fake-topic'
+
+/**
+ * @deprecated use provideAppStateServiceMock()
+ */
+export function provideAppServiceMock() {
+  return provideAppStateServiceMock()
+}
+
+export function provideAppStateServiceMock() {
+  return [{ provide: AppStateService, useClass: AppStateServiceMock }]
+}
+
+@Injectable()
+export class AppStateServiceMock {
+  globalError$ = new FakeTopic()
+  globalLoading$ = new FakeTopic(false)
+  currentMfe$ = new FakeTopic({ mountPath: '/', remoteBaseUrl: '.', baseHref: '/', shellName: 'test' })
+  currentPage$ = new FakeTopic<PageInfo | undefined>(undefined)
+  currentPortal$ = new FakeTopic<Portal>({ baseUrl: '/', microfrontendRegistrations: [], portalName: 'Test portal' })
+  isAuthenticated$ = new FakeTopic<null>(null)
+}
--- libs/angular-accelerator/src/lib/core/components/page-header/page-header.component.stories.ts
+++ libs/angular-accelerator/src/lib/core/components/page-header/page-header.component.stories.ts
@@ -1,338 +1,338 @@
-// import { DatePipe } from '@angular/common'
-// import { importProvidersFrom } from '@angular/core'
-// import { BrowserModule } from '@angular/platform-browser'
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-// import { RouterModule } from '@angular/router'
-// import { TranslatePipe } from '@ngx-translate/core'
-// import { action } from '@storybook/addon-actions'
-// import { Meta, StoryFn, applicationConfig, moduleMetadata } from '@storybook/angular'
-// import { PrimeIcons } from 'primeng/api'
-// import { BreadcrumbModule } from 'primeng/breadcrumb'
-// import { ButtonModule } from 'primeng/button'
-// import { MenuModule } from 'primeng/menu'
-// import { SkeletonModule } from 'primeng/skeleton'
-// import { DynamicPipe } from '../../pipes/dynamic.pipe'
-// import { StorybookTranslateModule } from '../../storybook-translate.module'
-// import { StorybookBreadcrumbModule } from './../../storybook-breadcrumb.module'
-// import { Action, ObjectDetailItem, PageHeaderComponent } from './page-header.component'
+import { DatePipe } from '@angular/common'
+import { importProvidersFrom } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { TranslatePipe } from '@ngx-translate/core'
+import { action } from '@storybook/addon-actions'
+import { Meta, StoryFn, applicationConfig, moduleMetadata } from '@storybook/angular'
+import { PrimeIcons } from 'primeng/api'
+import { BreadcrumbModule } from 'primeng/breadcrumb'
+import { ButtonModule } from 'primeng/button'
+import { MenuModule } from 'primeng/menu'
+import { SkeletonModule } from 'primeng/skeleton'
+import { DynamicPipe } from '../../pipes/dynamic.pipe'
+import { StorybookTranslateModule } from '../../storybook-translate.module'
+import { StorybookBreadcrumbModule } from './../../storybook-breadcrumb.module'
+import { Action, ObjectDetailItem, PageHeaderComponent } from './page-header.component'
 
-// export default {
-//   title: 'PageHeaderComponent',
-//   component: PageHeaderComponent,
-//   decorators: [
-//     applicationConfig({
-//       providers: [
-//         importProvidersFrom(BrowserModule),
-//         importProvidersFrom(BrowserAnimationsModule),
-//         importProvidersFrom(RouterModule.forRoot([], { useHash: true })),
-//       ],
-//     }),
-//     moduleMetadata({
-//       declarations: [PageHeaderComponent, DynamicPipe],
-//       imports: [
-//         MenuModule,
-//         BreadcrumbModule,
-//         ButtonModule,
-//         SkeletonModule,
-//         StorybookTranslateModule,
-//         StorybookBreadcrumbModule.init([
-//           { label: 'Level 1', routerLink: ['/something'] },
-//           { label: 'Level 2', url: '/' },
-//         ]),
-//       ],
-//     }),
-//   ],
-// } as Meta<PageHeaderComponent>
+export default {
+  title: 'PageHeaderComponent',
+  component: PageHeaderComponent,
+  decorators: [
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserModule),
+        importProvidersFrom(BrowserAnimationsModule),
+        importProvidersFrom(RouterModule.forRoot([], { useHash: true })),
+      ],
+    }),
+    moduleMetadata({
+      declarations: [PageHeaderComponent, DynamicPipe],
+      imports: [
+        MenuModule,
+        BreadcrumbModule,
+        ButtonModule,
+        SkeletonModule,
+        StorybookTranslateModule,
+        StorybookBreadcrumbModule.init([
+          { label: 'Level 1', routerLink: ['/something'] },
+          { label: 'Level 2', url: '/' },
+        ]),
+      ],
+    }),
+  ],
+} as Meta<PageHeaderComponent>
 
-// const Template: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
-//   props: args,
-// })
+const Template: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
+  props: args,
+})
 
-// const demoActions: Action[] = [
-//   {
-//     label: 'Save',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Save'`)
-//       action('actionButtonClick')({ button: 'Save' })
-//     },
-//     title: 'Tooltip for Save',
-//   },
-//   {
-//     label: 'Reload',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Reload'`)
-//       action('actionButtonClick')({ button: 'Reload' })
-//     },
-//     title: 'Tooltip for Reload',
-//     show: 'always',
-//     icon: PrimeIcons.REFRESH,
-//   },
-//   {
-//     label: 'Delete',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Delete'`)
-//       action('actionButtonClick')({ button: 'Delete' })
-//     },
-//     title: 'Tooltip for Delete',
-//     show: 'always',
-//     icon: PrimeIcons.TRASH,
-//   },
-//   {
-//     label: 'Some action',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Some action'`)
-//     },
-//     show: 'asOverflow',
-//   },
-//   {
-//     label: 'Other action',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Other Action'`)
-//     },
-//     show: 'asOverflow',
-//   },
-//   {
-//     label: 'Disabled',
-//     actionCallback: () => {
-//       console.log(`you clicked 'Disabled'`)
-//     },
-//     title: 'Tooltip for Disabled',
-//     disabled: true,
-//   },
-// ]
+const demoActions: Action[] = [
+  {
+    label: 'Save',
+    actionCallback: () => {
+      console.log(`you clicked 'Save'`)
+      action('actionButtonClick')({ button: 'Save' })
+    },
+    title: 'Tooltip for Save',
+  },
+  {
+    label: 'Reload',
+    actionCallback: () => {
+      console.log(`you clicked 'Reload'`)
+      action('actionButtonClick')({ button: 'Reload' })
+    },
+    title: 'Tooltip for Reload',
+    show: 'always',
+    icon: PrimeIcons.REFRESH,
+  },
+  {
+    label: 'Delete',
+    actionCallback: () => {
+      console.log(`you clicked 'Delete'`)
+      action('actionButtonClick')({ button: 'Delete' })
+    },
+    title: 'Tooltip for Delete',
+    show: 'always',
+    icon: PrimeIcons.TRASH,
+  },
+  {
+    label: 'Some action',
+    actionCallback: () => {
+      console.log(`you clicked 'Some action'`)
+    },
+    show: 'asOverflow',
+  },
+  {
+    label: 'Other action',
+    actionCallback: () => {
+      console.log(`you clicked 'Other Action'`)
+    },
+    show: 'asOverflow',
+  },
+  {
+    label: 'Disabled',
+    actionCallback: () => {
+      console.log(`you clicked 'Disabled'`)
+    },
+    title: 'Tooltip for Disabled',
+    disabled: true,
+  },
+]
 
-// const demoFields: ObjectDetailItem[] = [
-//   {
-//     label: 'Venue',
-//     value: 'AIE Munich ',
-//   },
-//   {
-//     label: 'Status',
-//     labelPipe: TranslatePipe,
-//     value: 'Confirmed',
-//   },
-//   {
-//     label: 'Start Date',
-//     value: '14.3.2022',
-//   },
-//   {
-//     label: 'End Date',
-//     value: new Date().toISOString(),
-//     valuePipe: DatePipe,
-//   },
-// ]
+const demoFields: ObjectDetailItem[] = [
+  {
+    label: 'Venue',
+    value: 'AIE Munich ',
+  },
+  {
+    label: 'Status',
+    labelPipe: TranslatePipe,
+    value: 'Confirmed',
+  },
+  {
+    label: 'Start Date',
+    value: '14.3.2022',
+  },
+  {
+    label: 'End Date',
+    value: new Date().toISOString(),
+    valuePipe: DatePipe,
+  },
+]
 
-// export const Primary = {
-//   render: Template,
+export const Primary = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     loading: false,
-//     objectDetails: demoFields,
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'My subtitle',
+    loading: false,
+    objectDetails: demoFields,
+    showBreadcrumbs: false,
+  },
+}
 
-// export const TitleBarOnly = {
-//   render: Template,
+export const TitleBarOnly = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'My subtitle',
+    showBreadcrumbs: false,
+  },
+}
 
-// export const WithCustomButtons = {
-//   render: Template,
+export const WithCustomButtons = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     disableDefaultActions: true,
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'My subtitle',
+    actions: demoActions,
+    objectDetails: demoFields,
+    disableDefaultActions: true,
+    showBreadcrumbs: false,
+  },
+}
 
-// export const WithBreadcrumbs = {
-//   render: Template,
+export const WithBreadcrumbs = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     disableDefaultActions: true,
-//     showBreadcrumbs: true,
-//     manualBreadcrumbs: true,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'My subtitle',
+    actions: demoActions,
+    objectDetails: demoFields,
+    disableDefaultActions: true,
+    showBreadcrumbs: true,
+    manualBreadcrumbs: true,
+  },
+}
 
-// export const WithImageUrl = {
-//   render: Template,
+export const WithImageUrl = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     disableDefaultActions: true,
-//     showBreadcrumbs: true,
-//     manualBreadcrumbs: true,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'My subtitle',
+    actions: demoActions,
+    objectDetails: demoFields,
+    disableDefaultActions: true,
+    showBreadcrumbs: true,
+    manualBreadcrumbs: true,
+  },
+}
 
-// export const WithImageWithoutBG = {
-//   render: Template,
+export const WithImageWithoutBG = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'Figure/image is shown without colored background',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     disableDefaultActions: true,
-//     showBreadcrumbs: true,
-//     figureBackground: false,
-//     manualBreadcrumbs: true,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'Figure/image is shown without colored background',
+    actions: demoActions,
+    objectDetails: demoFields,
+    disableDefaultActions: true,
+    showBreadcrumbs: true,
+    figureBackground: false,
+    manualBreadcrumbs: true,
+  },
+}
 
-// export const WithoutFigure = {
-//   render: Template,
+export const WithoutFigure = {
+  render: Template,
 
-//   args: {
-//     header: 'My title',
-//     subheader: 'No figure/image is shown to the left',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     disableDefaultActions: true,
-//     showBreadcrumbs: true,
-//     showFigure: false,
-//     manualBreadcrumbs: true,
-//   },
-// }
+  args: {
+    header: 'My title',
+    subheader: 'No figure/image is shown to the left',
+    actions: demoActions,
+    objectDetails: demoFields,
+    disableDefaultActions: true,
+    showBreadcrumbs: true,
+    showFigure: false,
+    manualBreadcrumbs: true,
+  },
+}
 
-// export const WithLoadingState = {
-//   render: Template,
+export const WithLoadingState = {
+  render: Template,
 
-//   args: {
-//     loading: true,
-//     header: 'My title',
-//     subheader: 'My subtitle',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    loading: true,
+    header: 'My title',
+    subheader: 'My subtitle',
+    actions: demoActions,
+    objectDetails: demoFields,
+    showBreadcrumbs: false,
+  },
+}
 
-// const TemplateWithProjection: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
-//   props: args,
-//   template: `
-//   <ocx-page-header [header]="title" [subheader]="subTitle" [actions]="actions">
-//     <div>
-//       <span>My dynamic content</span>
-//       <ul>
-//         <li>Can be anything you want</li>
-//         <li>Will be rendered under title bar</li>
-//       </ul>
-//     </div>
-//   </ocx-page-header>`,
-// })
+const TemplateWithProjection: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
+  props: args,
+  template: `
+  <ocx-page-header [header]="title" [subheader]="subTitle" [actions]="actions">
+    <div>
+      <span>My dynamic content</span>
+      <ul>
+        <li>Can be anything you want</li>
+        <li>Will be rendered under title bar</li>
+      </ul>
+    </div>
+  </ocx-page-header>`,
+})
 
-// const TemplateWithFigureProjection: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
-//   props: args,
-//   template: `
-//   <ocx-page-header [header]="header" [subheader]="subheader" [actions]="actions" [figureBackground]="false">
-//     <div figureImage  class="bg-orange-500 text-white w-full h-full">
-//       <div><i class="pi pi-user"></i></div>
-//     </div>
-//     <div>
-//       The figure is an html with the following content:
-//       <textarea readonly class="block w-full"><div class="bg-orange-500 text-white"><i class="pi pi-user"></i></div></textarea>
-//     </div>
-//   </ocx-page-header>`,
-// })
+const TemplateWithFigureProjection: StoryFn<PageHeaderComponent> = (args: PageHeaderComponent) => ({
+  props: args,
+  template: `
+  <ocx-page-header [header]="header" [subheader]="subheader" [actions]="actions" [figureBackground]="false">
+    <div figureImage  class="bg-orange-500 text-white w-full h-full">
+      <div><i class="pi pi-user"></i></div>
+    </div>
+    <div>
+      The figure is an html with the following content:
+      <textarea readonly class="block w-full"><div class="bg-orange-500 text-white"><i class="pi pi-user"></i></div></textarea>
+    </div>
+  </ocx-page-header>`,
+})
 
-// export const WithCustomFigureContent = {
-//   render: TemplateWithFigureProjection,
+export const WithCustomFigureContent = {
+  render: TemplateWithFigureProjection,
 
-//   args: {
-//     header: 'My header title',
-//     subheader: 'Figure to the left is completely controlled by the host',
-//     actions: demoActions,
-//     figureBackground: false,
-//     objectDetails: demoFields,
-//   },
-// }
+  args: {
+    header: 'My header title',
+    subheader: 'Figure to the left is completely controlled by the host',
+    actions: demoActions,
+    figureBackground: false,
+    objectDetails: demoFields,
+  },
+}
 
-// export const WithCustomContent = {
-//   render: TemplateWithProjection,
+export const WithCustomContent = {
+  render: TemplateWithProjection,
 
-//   args: {
-//     header: 'My header title',
-//     subheader: 'My subtitle',
-//     actions: demoActions,
-//     objectDetails: demoFields,
-//   },
-// }
+  args: {
+    header: 'My header title',
+    subheader: 'My subtitle',
+    actions: demoActions,
+    objectDetails: demoFields,
+  },
+}
 
-// const objectDetailsWithoutIcons: ObjectDetailItem[] = [
-//   {
-//     label: 'Venue',
-//     value: 'AIE Munich ',
-//   },
-//   {
-//     label: 'Status',
-//     value: 'Confirmed',
-//   },
-//   {
-//     label: 'Start Date',
-//     value: '14.3.2022',
-//   },
-// ]
+const objectDetailsWithoutIcons: ObjectDetailItem[] = [
+  {
+    label: 'Venue',
+    value: 'AIE Munich ',
+  },
+  {
+    label: 'Status',
+    value: 'Confirmed',
+  },
+  {
+    label: 'Start Date',
+    value: '14.3.2022',
+  },
+]
 
-// export const WithObjectDetails = {
-//   render: Template,
+export const WithObjectDetails = {
+  render: Template,
 
-//   args: {
-//     header: 'Test Page',
-//     subheader: 'Page header with text based objectDetails and no icons',
-//     loading: false,
-//     objectDetails: objectDetailsWithoutIcons,
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    header: 'Test Page',
+    subheader: 'Page header with text based objectDetails and no icons',
+    loading: false,
+    objectDetails: objectDetailsWithoutIcons,
+    showBreadcrumbs: false,
+  },
+}
 
-// const objectDetailsWithIcons: ObjectDetailItem[] = [
-//   {
-//     label: 'Venue',
-//     value: 'AIE Munich ',
-//   },
-//   {
-//     label: 'Event Completed',
-//     icon: PrimeIcons.CHECK_CIRCLE,
-//   },
-//   {
-//     label: 'Start Date',
-//     value: '14.3.2022',
-//     icon: PrimeIcons.CLOCK,
-//   },
-//   {
-//     label: 'I have no value',
-//   },
-// ]
+const objectDetailsWithIcons: ObjectDetailItem[] = [
+  {
+    label: 'Venue',
+    value: 'AIE Munich ',
+  },
+  {
+    label: 'Event Completed',
+    icon: PrimeIcons.CHECK_CIRCLE,
+  },
+  {
+    label: 'Start Date',
+    value: '14.3.2022',
+    icon: PrimeIcons.CLOCK,
+  },
+  {
+    label: 'I have no value',
+  },
+]
 
-// export const WithObjectDetailsAndIcons = {
-//   render: Template,
+export const WithObjectDetailsAndIcons = {
+  render: Template,
 
-//   args: {
-//     header: 'Test Page',
-//     subheader: 'Page header with text and icon based objectDetails',
-//     loading: false,
-//     objectDetails: objectDetailsWithIcons,
-//     showBreadcrumbs: false,
-//   },
-// }
+  args: {
+    header: 'Test Page',
+    subheader: 'Page header with text and icon based objectDetails',
+    loading: false,
+    objectDetails: objectDetailsWithIcons,
+    showBreadcrumbs: false,
+  },
+}
