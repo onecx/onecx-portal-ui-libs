6458bcc9b1012a4b47bda0423411e244d81f215e
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2024-03-16 22:39:51 +0100

    fix: fixed build error

--- libs/angular-standalone-shell/src/lib/services/standalone-slot.service.ts
+++ libs/angular-standalone-shell/src/lib/services/standalone-slot.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, Type } from '@angular/core'
-import { SlotService } from '@onecx/shell-core'
 import { Observable, of } from 'rxjs'
 import { StandaloneMenuComponent } from '../components/standalone-menu/standalone-menu.component'
-import { RemoteComponentInfo } from '@onecx/angular-remote-components'
+import { RemoteComponentInfo, SlotService } from '@onecx/angular-remote-components'
 
 @Injectable()
 export class StandaloneSlotService implements SlotService {
