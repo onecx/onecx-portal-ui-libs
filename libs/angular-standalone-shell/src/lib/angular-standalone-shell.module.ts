c23e73639ea4f1bfc8dc9b136e4777466b391821
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2024-03-16 22:42:21 +0100

    feat: provided SLOT_SERVICE in AngularStandaloneShellModule

--- libs/angular-standalone-shell/src/lib/angular-standalone-shell.module.ts
+++ libs/angular-standalone-shell/src/lib/angular-standalone-shell.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { StandaloneSlotService } from './services/standalone-slot.service'
+import { SLOT_SERVICE } from '@onecx/angular-remote-components'
 
 @NgModule({
   imports: [CommonModule],
+  providers: [
+    StandaloneSlotService,
+    {
+      provide: SLOT_SERVICE,
+      useExisting: StandaloneSlotService
+    }
+  ]
 })
 export class AngularStandaloneShellModule {}
