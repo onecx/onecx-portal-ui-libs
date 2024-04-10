527290d029b09d61777d80ea02261d41e1f73aad
Annika Nowak <annika.nowak@capgemini.com>
2024-04-10 08:49:54 +0200

    feat: add script and add set the flag for chmod permission

--- release-script.sh
+++ release-script.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-VERSION=14.3.1
 packageJsonData=$(cat package.json)
 topLevelPackageVersion=$(echo "$packageJsonData" | jq -r '.version')
-echo $topLevelPackageVersion
 if [ $topLevelPackageVersion != $VERSION ]
 then
-    echo Version is not up to date
     npx -p replace-json-property rjp ./package.json version $VERSION
 fi
 
@@ -16,20 +13,13 @@ while IFS= read -r folder; do
 done < <(find "$directory" -mindepth 1 -maxdepth 1 -type d | awk -F "/" '{print $NF}' | sort | uniq)
 
 for folder in "${folder_names[@]}"; do
-    packageJsonDataLib=$(cat libs/angular-accelerator/package.json)
+    packageJsonDataLib=$(cat libs/$folder/package.json)
     libPackageVersion=$(echo "$packageJsonDataLib" | jq -r '.version')
-    peerDependencies=$(echo "$packageJsonDataLib" | jq -r '.peerDependencies')
-    ocx_properties=$(echo "$packageJsonDataLib" | jq -r '."@onecx/accelerator"')
-    packageJsonDataLib=$(echo "$packageJsonDataLib" | sed -E 's/(@onecx[^"]+?": *?")([^"]+)"/\1^'$libPackageVersion'"/')
-    echo "$packageJsonDataLib" > libs/angular-accelerator/package.json
-    # 
-    if [ $libPackageVersion != $VERSION ]
+    packageJsonDataLib=$(echo "$packageJsonDataLib" | sed -E 's/(@onecx[^"]+?": *?")([^"]+)"/\1^'$VERSION'"/')
+if [ $libPackageVersion != $VERSION ]
 then
-    echo Version is not up to date
     npx -p replace-json-property rjp libs/$folder/package.json version $VERSION
-    #npx nx run-many -t test
-    ## the keyword test will be removed later and replaced with release
-    #npx nx run $folder:test
+    npx nx run $folder:release
 fi  
 done
 
