d2fbb13a0a3ea8977ca520e9aacd120799f74c51
Annika Nowak <139357202+anninowak@users.noreply.github.com>
2024-04-16 08:09:18 +0200

    feat: add angular auth lib and add config key to angular-integration-interface (#222)

---
+++ libs/angular-auth/src/lib/token.interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { AuthServiceWrapper } from './auth-service-wrapper'
+
+const WHITELIST = ['assets']
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthServiceWrapper) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const skip = WHITELIST.some((str) => request.url.includes(str))
+    if (skip) {
+      return next.handle(request)
+    }
+    const headerValues = this.authService.getHeaderValues()
+    let headers = request.headers
+    for (const header in headerValues) {
+      headers = headers.set(header, headerValues[header])
+    }
+    const authenticatedReq: HttpRequest<unknown> = request.clone({
+      headers: headers,
+    })
+    return next.handle(authenticatedReq)
+  }
+}
