7e315db6c883cd045abd51a730a5d9ea39620859
markuczy <martin.kuczynski@capgemini.com>
2024-05-07 10:54:00 +0200

    refactor: moved to portal-dialog.service

--- libs/portal-integration-angular/src/lib/core/utils/provide-portal-dialog-service.utils.ts
+++
@@ -1,6 +0,0 @@
-import { DialogService } from 'primeng/dynamicdialog'
-import { PortalDialogService } from '../../services/portal-dialog.service'
-
-export function providePortalDialogService() {
-  return [DialogService, PortalDialogService]
-}
