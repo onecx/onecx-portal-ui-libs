a86e682dc05f12b8a84b246ee7ae0e5655a329a7
markuczy <129275100+markuczy@users.noreply.github.com>
2024-05-15 14:57:24 +0200

    feat: slot service implementation in angular-remote-components (#251)
    * feat: slot service with permission service to be proxied
    
    * feat: lint
    
    * feat: new topic and structure
    
    * feat: extended SlotService with component presence check
    
    * fix: topic not synchronizable
    
    * refactor: someComponentDefinedForSlot refactor
    
    * fix: fixed comparsion between strings

---
+++ libs/integration-interface/src/lib/topics/remote-components/v1/remote-components-info.model.ts
@@ -0,0 +1,4 @@
+import { RemoteComponent } from './remote-component.model'
+import { Slot } from './slot.model'
+
+export type RemoteComponentsInfo = { components: RemoteComponent[]; slots: Slot[] }
