fa80fe92e2347b93ae903facae3aed1267e1247a
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2024-06-19 09:36:49 +0200

    feat: make page header more customizable + add grid layout capabilities (#274)
    * feat: allow specification of valueTooltip and labelTooltip
    
    * feat: allow adding action button to header detail item
    
    * feat: add improved grid layout capabilities to page header
    
    * fix: fix GitHub workflow config for SB deploys
    
    * fix: implement chromatic secret check
    
    * fix: fix step name
    
    * fix: improve storybook CI
    
    * fix: improve storybook CI
    
    * fix: remove redundant file

--- libs/angular-accelerator/src/lib/components/page-header/page-header.component.ts
+++ libs/angular-accelerator/src/lib/components/page-header/page-header.component.ts
@@ -46,18 +46,28 @@ export interface Action {
 export interface ObjectDetailItem {
   label: string
   value?: string
+  /**
+   * @deprecated Use `valueTooltip` instead
+   */
   tooltip?: string
+  labelTooltip?: string
+  valueTooltip?: string
   icon?: PrimeIcon
   labelPipe?: Type<any>
   valuePipe?: Type<any>
   valuePipeArgs?: string
   valueCssClass?: string
+  actionItemIcon?: PrimeIcon
+  actionItemCallback?: () => void
+  actionItemTooltip?: string
 }
 
 export interface HomeItem {
   menuItem: MenuItem
   page?: string
 }
+
+export type GridColumnOptions = 1 | 2 | 3 | 4 | 6 | 12
 
 @Component({
   selector: 'ocx-page-header',
@@ -108,6 +118,12 @@ export class PageHeaderComponent implements OnInit, OnChanges {
   @Input()
   manualBreadcrumbs = false
 
+  @Input()
+  enableGridView: undefined | boolean
+
+  @Input()
+  gridLayoutDesktopColumns: undefined | GridColumnOptions
+
   @Output()
   save = new EventEmitter()
 
@@ -124,7 +140,17 @@ export class PageHeaderComponent implements OnInit, OnChanges {
 
   home$!: Observable<HomeItem>
 
-  figureImageLoadError = false;
+  figureImageLoadError = false
+
+  objectPanelGridLayoutClasses = 'grid row-gap-2 m-0'
+  objectPanelColumnLayoutClasses = 'flex flex-row justify-content-between overflow-x-auto'
+
+  objectPanelDefaultLayoutClasses = 'flex flex-column row-gap-2 md:flex-row md:justify-content-between'
+
+  objectInfoGridLayoutClasses = 'col-12 flex gap-4 md:col-6 align-items-center p-0'
+  objectInfoColumnLayoutClasses = 'flex flex-column align-items-center gap-2 min-w-120'
+  
+  objectInfoDefaultLayoutClasses = 'flex flex-row md:flex-column md:align-items-center md:gap-2'
 
   protected breadcrumbs: BreadcrumbService
 
@@ -184,6 +210,28 @@ export class PageHeaderComponent implements OnInit, OnChanges {
     if (item.icon) style = style.concat(style, ' ', 'gap-1 align-items-center')
     if (item.valueCssClass) style = style.concat(style, ' ', item.valueCssClass)
     return style
+  }
+
+  public getObjectPanelLayoutClasses() {
+    if (this.enableGridView) {
+      return this.objectPanelGridLayoutClasses
+    }
+    if (this.enableGridView === false) {
+      return this.objectPanelColumnLayoutClasses
+    }
+    return this.objectPanelDefaultLayoutClasses
+  }
+
+  public getObjectInfoLayoutClasses() {
+    if (this.enableGridView) {
+      return `${this.objectInfoGridLayoutClasses} lg:col-${
+        this.gridLayoutDesktopColumns ? 12 / this.gridLayoutDesktopColumns : 4
+      }`
+    }
+    if (this.enableGridView === false) {
+      return this.objectInfoColumnLayoutClasses
+    }
+    return this.objectInfoDefaultLayoutClasses
   }
 
   /**
