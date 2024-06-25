73a7f72426dfbf0a21209e11c43acfcca9f64641
markuczy <martin.kuczynski@capgemini.com>
2024-06-25 12:33:48 +0200

    feat: breadcrumb service for matcher routes

--- libs/angular-accelerator/src/lib/services/breadcrumb.service.ts
+++ libs/angular-accelerator/src/lib/services/breadcrumb.service.ts
@@ -13,38 +13,59 @@ export class BreadcrumbService {
   itemsHandler = this.itemsSource.asObservable()
 
   constructor(private router: Router, private activeRoute: ActivatedRoute, private translateService: TranslateService) {
-    const breadcrumbs: MenuItem[] = []
-    this.addBreadcrumb(this.activeRoute.snapshot, [], breadcrumbs)
-    this.generatedItemsSource.next(breadcrumbs)
+    this.generateBreadcrumbs(this.activeRoute.snapshot)
     this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
       const root = this.router.routerState.snapshot.root
-      const breadcrumbs: MenuItem[] = []
-      this.addBreadcrumb(root, [], breadcrumbs)
-
-      this.generatedItemsSource.next(breadcrumbs)
+      this.generateBreadcrumbs(root)
     })
+  }
+
+  private generateBreadcrumbs(route: ActivatedRouteSnapshot | null) {
+    if (route && route.url) {
+      if (route.data['mfeInfo']) {
+        const breadcrumbs: MenuItem[] = [
+          {
+            label: route.data['mfeInfo'].productName,
+            routerLink: route.data['mfeInfo'].baseHref,
+          },
+        ]
+        const baseUrl: string[] = (route.data['mfeInfo'].baseHref as string).split('/').filter((value) => value)
+        const parentUrl: string[] = route.url.map((url) => url.path)
+        if (!parentUrl.every((item) => baseUrl.includes(item))) {
+          this.createBreadcrumb(route, parentUrl, breadcrumbs)
+        }
+        this.addBreadcrumb(route.firstChild, parentUrl, breadcrumbs)
+        this.generatedItemsSource.next(breadcrumbs)
+      } else {
+        this.generateBreadcrumbs(route.firstChild)
+      }
+    }
   }
 
   private addBreadcrumb(route: ActivatedRouteSnapshot | null, parentUrl: string[], breadcrumbs: MenuItem[]) {
     if (route && route.url) {
       const routeUrl = parentUrl.concat(route.url.map((url) => url.path))
       if (route.routeConfig?.path) {
-        if (route.data['breadcrumb']) {
-          const breadcrumb: MenuItem = {
-            label: this.getLabel(route.data, route.paramMap),
-            routerLink: '/' + routeUrl.join('/'),
-          }
-          breadcrumbs.push(breadcrumb)
-        } else {
-          const breadcrumb: MenuItem = {
-            label: 'NA',
-            routerLink: '/' + routeUrl.join('/'),
-          }
-          breadcrumbs.push(breadcrumb)
-        }
+        this.createBreadcrumb(route, routeUrl, breadcrumbs)
       }
 
       this.addBreadcrumb(route.firstChild, routeUrl, breadcrumbs)
+    }
+  }
+
+  private createBreadcrumb(route: ActivatedRouteSnapshot, routeUrl: string[], breadcrumbs: MenuItem[]) {
+    if (route.data['breadcrumb']) {
+      const breadcrumb: MenuItem = {
+        label: this.getLabel(route.data, route.paramMap),
+        routerLink: '/' + routeUrl.join('/'),
+      }
+      breadcrumbs.push(breadcrumb)
+    } else {
+      const breadcrumb: MenuItem = {
+        label: 'NA',
+        routerLink: '/' + routeUrl.join('/'),
+      }
+      breadcrumbs.push(breadcrumb)
     }
   }
 
