b28d76269fb58719479badba65677aa7866a837d
markuczy <martin.kuczynski@capgemini.com>
2024-07-01 09:26:59 +0200

    feat: matcher improvements

--- libs/angular-webcomponents/src/lib/utils/webcomponent-router.utils.ts
+++ libs/angular-webcomponents/src/lib/utils/webcomponent-router.utils.ts
@@ -1,17 +1,29 @@
 import { Route, UrlMatcher, UrlSegment, UrlSegmentGroup } from '@angular/router'
 
-export function match(prefix: string): UrlMatcher {
+export function startsWith(prefix: string): UrlMatcher {
   return (url: UrlSegment[], UrlSegmentGroup: UrlSegmentGroup, route: Route) => {
-    const mfeBaseHref: string = route?.data?.['mfeInfo']?.baseHref
-    if (!mfeBaseHref) {
-      console.warn('mfeInfo was not provided for route')
-    }
-    const baseHrefSegmentAmount = mfeBaseHref.split('/').filter((value) => value).length
-    const urlWithoutBaseHref = url.slice(baseHrefSegmentAmount)
-    const index = prefix === '' ? 0 : urlWithoutBaseHref.findIndex((u) => u.path === prefix)
-    if (index >= 0) {
-      return { consumed: url.slice(0, index + baseHrefSegmentAmount) }
+    const urlWithoutBaseHref = sliceBaseHref(route, url)
+
+    const fullUrl = urlWithoutBaseHref.map((u) => u.path).join('/')
+    if (fullUrl.startsWith(prefix)) {
+      const prefixLength = prefix.split('/').filter((value) => value).length
+      return { consumed: url.slice(0, baseHrefSegmentAmount(url, urlWithoutBaseHref) + prefixLength) }
     }
     return null
   }
+}
+
+export function sliceBaseHref(route: Route, url: UrlSegment[]): UrlSegment[] {
+  const mfeBaseHref: string = route?.data?.['mfeInfo']?.baseHref
+  if (!mfeBaseHref) {
+    console.warn(
+      'mfeInfo was not provided for route. initializeRouter function is required to be registered as app initializer.'
+    )
+  }
+  const baseHrefSegmentAmount = mfeBaseHref.split('/').filter((value) => value).length
+  return url.slice(baseHrefSegmentAmount)
+}
+
+export function baseHrefSegmentAmount(url: UrlSegment[], urlWithoutBaseHref: UrlSegment[]) {
+  return url.length - urlWithoutBaseHref.length
 }
