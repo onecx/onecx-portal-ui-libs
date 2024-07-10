e14cb939ffd88e68f7bad3e26dacd61bde587781
jufiedle <juliane.fiedler@capgemini.com>
2024-07-10 10:08:52 +0200

    P002271-7516: code reviewed, added and adapted tests

--- libs/angular-integration-interface/src/lib/services/workspace.service.spec.ts
+++ libs/angular-integration-interface/src/lib/services/workspace.service.spec.ts
@@ -6,9 +6,14 @@ import { AppStateService } from './app-state.service'
 describe('WorkspaceService', () => {
   let service: WorkspaceService
   let mockAppStateService: any
-  const params = new Map<string, string>([
-    ['id', '5']
-  ])
+  const params: Record<string, unknown> = {
+    "id": 5,
+    "key":"xy"
+  };
+
+  const paramsWrong: Record<string, unknown> = {
+    "idx": 5
+  };
 
   beforeEach(() => {
     mockAppStateService = {
@@ -18,7 +23,7 @@ describe('WorkspaceService', () => {
           {
             appId: 'onecx-workspace-ui',
             productName: 'onecx-workspace',
-            baseUrl: '/workspace/baseurl',
+            baseUrl: 'http://example.com/workspace/baseurl',
             endpoints: [
               { name: 'details', path: '/details/{id}' },
               { name: 'edit', path: '[[details]]' },
@@ -40,7 +45,28 @@ describe('WorkspaceService', () => {
     expect(service).toBeTruthy()
   })
 
-  it('should find endpoint and return an observable that emits "/workspace/baseurl/details/{5}"', (done) => {
+  //Testcases
+  //1 Best case scenario
+  //2 workspace baseUrl not there
+  //3 workspace baseUrl is empty
+  //4 workspace has no routes
+  //5 workspace has emtpy routes
+  //6 route was not found by appId and productName
+  //7 workspace.route baseUrl not there
+  //8 workspace.route baseUrl is empty
+  //9 endpoints are empty
+  //10 endpoint with name was not found
+  //11 test endpoint with 1 alias
+  //12 test endpoint with 2 alias
+  //13 test endpoint with wrong alias
+  //14 param was not found
+  //15 no params given as param of getUrl
+  //16 2 params
+  //17 no endpoints available
+  //18 test getUrl with double //
+
+  //1
+  it('should find endpoint and return an observable that emits correct url ', (done) => {
     service
     .getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', params)
     .subscribe((url) => {
@@ -49,10 +75,257 @@ describe('WorkspaceService', () => {
     });
   });
 
-  it('should find only app and return an observable that emits "/workspace/baseurl"', (done) => {
+  //2
+  it('should return empty string when workspace baseUrl is not set"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      routes: []
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('');
+        done();
+      })
+  })
+
+  //3
+  it('should return empty string when workspace baseUrl is not set"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: '',
+      routes: []
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('');
+        done();
+      })
+  })
+
+  //4
+  it('should return workspace baseUrl when workspace has no routes at all"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com'
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com');
+        done();
+      })
+  })
+
+  //5
+  it('should return workspace baseUrl when workspace.routes is empty"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: []
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com');
+        done();
+      })
+  })
+
+  //6
+  it('should return workspace baseUrl when route for appId and productName was not found"', (done) => {
+    service
+    .getUrl('onecx-workspace-uix', 'onecx-workspace', 'details', {})
+    .subscribe((url) => {
+      expect(url).toBe('http://example.com');
+      done();
+    });
+  });
+
+  //7
+  it('should return workspace baseUrl when route has no baseUrl', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          endpoints: [],
+        },
+      ],
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com');
+        done();
+      })
+  })
+
+  //8
+  it('should return workspace baseUrl when route has empty baseUrl', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          endpoints: [
+            { name: 'details', path: '/details/{id}' },
+            { name: 'edit', path: '[[details]]' },
+            { name: 'change', path: '[[edit]]' },
+          ],
+        },
+      ],
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com');
+        done();
+      })
+  })
+
+  //9
+  it('should return route.baseUrl when endpoints are empty"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          baseUrl: 'http://example.com/workspace/baseurl',
+          endpoints: [],
+        },
+      ],
+    });
+    
     service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
       .subscribe((url) => {
         expect(url).toBe('http://example.com/workspace/baseurl');
+        done();
+      })
+  })
+
+  //10
+  it('should find only app and return route.baseUrl', (done) => {
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl');
+        done();
+      })
+  })
+
+  //11
+  it('should return well formed url for endpoint with 1 alias', (done) => {
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'edit', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl/details/5');
+        done();
+      })
+  })
+
+  //12
+  it('should return well formed url for endpoint with 2 alias', (done) => {
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'change', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl/details/5');
+        done();
+      })
+  })
+
+  //13
+  it('should return well formed url for endpoint with wrong alias', (done) => {
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'changexy', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl');
+        done();
+      })
+  })
+
+  //14
+  it('should find endpoint and return correct url despite of wrong params"', (done) => {
+    service
+    .getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', paramsWrong)
+    .subscribe((url) => {
+      expect(url).toBe('http://example.com/workspace/baseurl');
+      done();
+    });
+  });
+
+  //15
+  it('should find endpoint and return expected url despite of empty params"', (done) => {
+    service
+    .getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', {})
+    .subscribe((url) => {
+      expect(url).toBe('http://example.com/workspace/baseurl');
+      done();
+    });
+  });
+
+  //16
+  it('should return well formed url with 2 params in endpoint', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          baseUrl: 'http://example.com/workspace/baseurl/',
+          endpoints: [
+            { name: 'details', path: '/details/{id}/{key}' },
+            { name: 'edit', path: '[[details]]' },
+            { name: 'change', path: '[[edit]]' },
+          ],
+        },
+      ],
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl/details/5/xy');
+        done();
+      })
+  })
+
+  //17
+  it('should return route.baseUrl when no endpoints are available"', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          baseUrl: 'http://example.com/workspace/baseurl',
+        },
+      ],
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'detailswrong', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl');
+        done();
+      })
+  })
+
+  //18
+  it('should return well formed url although double / are retrieved', (done) => {
+    mockAppStateService.currentWorkspace$ = of({
+      baseUrl: 'http://example.com',
+      routes: [
+        {
+          appId: 'onecx-workspace-ui',
+          productName: 'onecx-workspace',
+          baseUrl: 'http://example.com/workspace/baseurl/',
+          endpoints: [
+            { name: 'details', path: '//details/{id}' },
+            { name: 'edit', path: '[[details]]' },
+            { name: 'change', path: '[[edit]]' },
+          ],
+        },
+      ],
+    });
+    
+    service.getUrl('onecx-workspace-ui', 'onecx-workspace', 'details', params)
+      .subscribe((url) => {
+        expect(url).toBe('http://example.com/workspace/baseurl/details/5');
         done();
       })
   })
@@ -64,10 +337,11 @@ describe('WorkspaceService', () => {
       { name: 'edit', path: '[[details]]' },
     ]
 
-    const resultEndpoint = service.dissolveEndpoint(endpoint.name, endpoints)
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
 
     expect(resultEndpoint?.path).toBe('/details/{id}')
   })
+  
 
   it('it should return unchanged Endpoint without alias "', () => {
     const endpoint = { name: 'details', path: '/details/{id}' }
@@ -76,7 +350,7 @@ describe('WorkspaceService', () => {
       { name: 'edit', path: '[[details]]' },
     ]
 
-    const resultEndpoint = service.dissolveEndpoint(endpoint.name, endpoints)
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
 
     expect(resultEndpoint?.path).toBe('/details/{id}')
   })
@@ -89,9 +363,22 @@ describe('WorkspaceService', () => {
       { name: 'change', path: '[[edit]]' },
     ]
 
-    const resultEndpoint = service.dissolveEndpoint(endpoint.name, endpoints)
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
 
     expect(resultEndpoint?.path).toBe('/details/{id}')
+  })
+
+  it('it should return undefined for wrong endpoint name"', () => {
+    const endpoint = { name: 'change', path: '[[edit]]' }
+    const endpoints = [
+      { name: 'detailsx', path: '/details/{id}' },
+      { name: 'edit', path: '[[details]]' },
+      { name: 'change', path: '[[edit]]' },
+    ]
+
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
+
+    expect(resultEndpoint).toBe(undefined)
   })
 
   it('it should return undefined when alias was not found "', () => {
@@ -101,7 +388,7 @@ describe('WorkspaceService', () => {
       { name: 'change', path: '[[edit]]' },
     ]
 
-    const resultEndpoint = service.dissolveEndpoint(endpoint.name, endpoints)
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
 
     expect(resultEndpoint).toBe(undefined)
   })
@@ -113,32 +400,51 @@ describe('WorkspaceService', () => {
       { name: 'edit', path: '[[details]]' },
     ]
 
-    const resultEndpoint = service.dissolveEndpoint(endpoint.name, endpoints)
+    const resultEndpoint = service['dissolveEndpoint'](endpoint.name, endpoints)
 
     expect(resultEndpoint).toBe(undefined)
   })
 
   it('it should return url with filled params from Map "', () => {
     const path = 'details/{id}'
-    const params = new Map<string, string>([
-      ['id', '5'],
-      ['name', 'xy'],
-    ])
+    const params: Record<string, unknown> = {
+      "id": 5,
+      "name": "xy"
+  };
 
-    const resultPath = service.fillParamsForPath(path, params)
+    const resultPath = service['fillParamsForPath'](path, params)
 
     expect(resultPath).toBe('details/5')
   })
 
   it('it should return url with filled params from Map with two params"', () => {
     const path = 'details/{name}/{id}'
-    const params = new Map<string, string>([
-      ['id', '5'],
-      ['name', 'xy'],
-    ])
+    const params: Record<string, unknown> = {
+      "id": 5,
+      "name": "xy"
+  };
 
-    const resultPath = service.fillParamsForPath(path, params)
+    const resultPath = service['fillParamsForPath'](path, params)
 
     expect(resultPath).toBe('details/xy/5')
   })
+
+  it('should concat two strings with one / and leave :// as it is', () => {
+    let result1 = service['joinWithSlashAndDoubleCheck']("a", "b");
+    let result2 = service['joinWithSlashAndDoubleCheck']("a/", "b");
+    let result3 = service['joinWithSlashAndDoubleCheck']("a/", "/b");
+    let result4 = service['joinWithSlashAndDoubleCheck']("/a/", "b/");
+    let result5 = service['joinWithSlashAndDoubleCheck']("a", "");
+    let result6 = service['joinWithSlashAndDoubleCheck']("", "b");
+    let result7 = service['joinWithSlashAndDoubleCheck']("https://a", "b");
+
+    expect(result1).toBe('a/b')
+    expect(result2).toBe('a/b')
+    expect(result3).toBe('a/b')
+    expect(result4).toBe('/a/b/')
+    expect(result5).toBe('a')
+    expect(result6).toBe('b')
+    expect(result7).toBe('https://a/b')
+  })
+
 })
--- libs/angular-integration-interface/src/lib/services/workspace.service.ts
+++ libs/angular-integration-interface/src/lib/services/workspace.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AppStateService } from './app-state.service';
 import { Observable, of, switchMap} from 'rxjs';
-import { Route } from 'libs/integration-interface/src/lib/topics/current-workspace/v1/route.model';
-import { Endpoint } from 'libs/integration-interface/src/lib/topics/current-workspace/v1/endpoint.model';
+import { Route } from '@onecx/integration-interface';
+import { Endpoint } from '@onecx/integration-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -19,94 +19,136 @@ export class WorkspaceService {
     protected appStateService: AppStateService) 
     { }
 
-     getUrl(appId: string, productName: string, endpointname: string, params: Map<string, string>): Observable<string> {
+     getUrl(appId: string, productName: string, endpointName: string, params: Record<string, unknown>={}): Observable<string> {
       return this.appStateService.currentWorkspace$.pipe(
         switchMap((workspace) => {
-          let finalUrl = "";
-          const baseUrl = workspace.baseUrl;
-          if (baseUrl !== undefined) {
-            finalUrl = finalUrl.concat(baseUrl);
-          }
-    
-          const routes = workspace.routes;
-          if (routes !== undefined && routes.length > 0) {
-            const route = this.filterRouteFromList(routes, appId, productName);
-            if (route !== undefined && route.baseUrl !== undefined) {
-              finalUrl = finalUrl.concat(route.baseUrl);
-    
-              const endpoints = route.endpoints;
-              if (endpoints !== undefined && endpoints.length > 0) {
-                const finalEndpoint = this.dissolveEndpoint(endpointname, endpoints);
-                if (finalEndpoint !== undefined && finalEndpoint.path !== undefined) {
-                  const paramsFilled = this.fillParamsForPath(finalEndpoint.path, params);
-                  if(paramsFilled !== undefined){
-                    finalUrl = finalUrl.concat(paramsFilled);
-                  }
-                  
-                }
-              }
-            }
-          }
-    
+          let finalUrl = this.constructRouteUrl(workspace, appId, productName, endpointName, params)
           return of(finalUrl);
-          //return of("ws.baseUrl + route.baseUrl + url"); // Wrap finalUrl in an Observable
         })
       );
     }
 
-    private filterRouteFromList( routes: Array<Route>, appId: string, productName: string): Route | undefined{
-      if(routes != undefined){
-        let productRoutes = routes.filter(route => (route.appId === appId && route.productName === productName));
-        if(productRoutes.length != 1){
-          //Problemfall //TODO
-        }
-        else{
-          let resultRoute = productRoutes.at(0);
-          return resultRoute!; //TODO
-        }
+    private constructBaseUrlFromWorkspace(workspace: any): string {
+      if (workspace.baseUrl === undefined) {
+        console.log("WARNING: There was no baseUrl for received workspace.");
+        return "";
       }
-      return undefined;
+      return workspace.baseUrl;
     }
 
-    dissolveEndpoint( endpointname: string, endpoints: Array<Endpoint>): Endpoint | undefined{
-      let endpoint = endpoints.filter(ep => (ep.name === endpointname)).at(0) as Endpoint; // abfangen wenn fehler
-      if(endpoint == undefined){
+    private constructRouteUrl(workspace: any, appId: string, productName: string, endpointName: string, params: Record<string, unknown>): string {
+      const route = this.filterRouteFromList(workspace.routes, appId, productName);
+      if (!route || route.baseUrl === undefined) {
+        console.log(`WARNING: No route.baseUrl could be found for given appId "${appId}" and productName "${productName}"`);
+
+        return this.constructBaseUrlFromWorkspace(workspace);
+      }
+    
+      let routeUrl = route.baseUrl;
+      routeUrl = this.joinWithSlashAndDoubleCheck(routeUrl,this.constructEndpointUrl(route, endpointName, params));
+      return routeUrl;
+    }
+
+    private constructEndpointUrl(route: any, endpointName: string, params: Record<string, unknown>): string {
+      if (!route.endpoints) {
+        return "";  
+      }
+      const finalEndpoint = this.dissolveEndpoint(endpointName, route.endpoints);
+      if (!finalEndpoint || finalEndpoint.path === undefined) {
+        console.log("WARNING: No endpoint.path could be found");
+        return "";
+      }
+    
+      const paramsFilled = this.fillParamsForPath(finalEndpoint.path, params);
+      if (paramsFilled === undefined) {
+        console.log("WARNING: Params could not be filled correctly");
+        return "";
+      }
+      
+      return paramsFilled;
+    }
+
+     private filterRouteFromList(routes: Array<Route>, appId: string, productName: string): Route | undefined {
+      if (!routes) {
         return undefined;
       }
-      // check if endpoint contains Aliases
-      while(endpoint.path?.includes(this.aliasStart)){
-        let path = endpoint.path;
-        let aliasName = path.substring(
-          path.indexOf(this.aliasStart) + this.aliasStart.length, 
-          path.lastIndexOf(this.aliasEnd)
-        );
-        let filteredEndpoints = endpoints.filter(ep => (ep.name === aliasName));
-        if(filteredEndpoints == undefined || filteredEndpoints.length == 0){
-          return undefined;
-        }
-        else{
-          endpoint = filteredEndpoints.at(0)!;
-        }
-
+    
+      const productRoutes = routes.filter(route => route.appId === appId && route.productName === productName);
+    
+      if (productRoutes.length === 0) {
+        return undefined;
       }
+    
+      if (productRoutes.length > 1) {
+        console.log("WARNING: Es wurden mehr als eine route gefunden. Die erste wird verwendet.");
+      }
+    
+      return productRoutes[0]; 
+    }
 
+     private dissolveEndpoint(endpointName: string, endpoints: Array<Endpoint>): Endpoint | undefined {
+      let endpoint = endpoints.find(ep => ep.name === endpointName);
+    
+      if (!endpoint) {
+        return undefined;  
+      }
+    
+      while (endpoint.path?.includes(this.aliasStart)) {
+        const path : string = endpoint.path;
+        const startIdx = path.indexOf(this.aliasStart) + this.aliasStart.length;
+        const endIdx = path.lastIndexOf(this.aliasEnd);
+        if (endIdx <= startIdx) {
+          return undefined; 
+        }
+        const aliasName = path.substring(startIdx, endIdx);
+        endpoint = endpoints.find(ep => ep.name === aliasName);
+    
+        if (!endpoint) {
+          return undefined; 
+        }
+      }
+    
       return endpoint;
     }
 
-    fillParamsForPath( path: string, params: Map<string,string>): string{
+    private fillParamsForPath( path: string, params: Record<string,unknown>): string{
       while(path.includes(this.paramStart)){
         let paramName = path.substring(
           path.indexOf(this.paramStart) + this.paramStart.length, 
           path.indexOf(this.paramEnd)
         );
-        let paramValue = params.get(paramName);
+        let paramValue = this.getStringFromUnknown(params[paramName]);
         if(paramValue != undefined && paramValue.length>0){
           path = path.replace(this.paramStart.concat(paramName).concat(this.paramEnd), paramValue);
+        }
+        else{ 
+          console.log(`WARNING: Searched param "${paramName}" was not found in given param list `);
+          return "";
         }
         
         
 
       }
       return path;
+    }
+
+    private getStringFromUnknown(value: unknown): string {
+      if (value === null || value === undefined) {
+        return "";  
+      } else if (typeof value === "string") {
+        return value;  
+      } else {
+        return String(value);  
+      }
+    }
+
+    private joinWithSlashAndDoubleCheck(string1: string, string2: string) : string{
+      if(string2.length==0){
+        return string1;
+      }
+      if(string1.length==0){
+        return string2;
+      }
+      return [string1,string2].join("/").replace(/(?<=[a-zA-Z])\/{2,}/g,'/');
     }
 }
