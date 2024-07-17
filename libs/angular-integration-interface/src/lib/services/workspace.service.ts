d3c600681894355f6afe3d82cfdd48dde37f68ae
jufiedle <juliane.fiedler@capgemini.com>
2024-07-17 13:02:14 +0200

    P002271-7516: formatted code

--- libs/angular-integration-interface/src/lib/services/workspace.service.ts
+++ libs/angular-integration-interface/src/lib/services/workspace.service.ts
@@ -1,149 +1,154 @@
-import { Injectable } from '@angular/core';
-import { AppStateService } from './app-state.service';
-import { Observable, of, map} from 'rxjs';
-import { Route } from '@onecx/integration-interface';
-import { Endpoint } from '@onecx/integration-interface';
+import { Injectable } from '@angular/core'
+import { AppStateService } from './app-state.service'
+import { Observable, of, map } from 'rxjs'
+import { Route } from '@onecx/integration-interface'
+import { Endpoint } from '@onecx/integration-interface'
 import { Location } from '@angular/common'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkspaceService {
+  private aliasStart = '[['
+  private aliasEnd = ']]'
+  private paramStart = '{'
+  private paramEnd = '}'
 
-  private aliasStart = '[[';
-  private aliasEnd = ']]';
-  private paramStart = "{";
-  private paramEnd = "}"
+  constructor(protected appStateService: AppStateService) {}
 
+  getUrl(
+    appId: string,
+    productName: string,
+    endpointName: string,
+    params: Record<string, unknown> = {}
+  ): Observable<string> {
+    return this.appStateService.currentWorkspace$.pipe(
+      map((workspace) => {
+        let finalUrl = this.constructRouteUrl(workspace, appId, productName, endpointName, params)
+        return finalUrl
+      })
+    )
+  }
 
-  constructor(
-    protected appStateService: AppStateService) 
-    { }
+  private constructBaseUrlFromWorkspace(workspace: any): string {
+    if (workspace.baseUrl === undefined) {
+      console.log('WARNING: There was no baseUrl for received workspace.')
+      return ''
+    }
+    return workspace.baseUrl
+  }
 
-     getUrl(appId: string, productName: string, endpointName: string, params: Record<string, unknown>={}): Observable<string> {
-      return this.appStateService.currentWorkspace$.pipe(
-        map((workspace) => {
-          let finalUrl = this.constructRouteUrl(workspace, appId, productName, endpointName, params)
-          return finalUrl;
-        })
-      );
+  private constructRouteUrl(
+    workspace: any,
+    appId: string,
+    productName: string,
+    endpointName: string,
+    params: Record<string, unknown>
+  ): string {
+    const route = this.filterRouteFromList(workspace.routes, appId, productName)
+    let url = this.constructBaseUrlFromWorkspace(workspace)
+    if (!route) {
+      console.log(
+        `WARNING: No route.baseUrl could be found for given appId "${appId}" and productName "${productName}"`
+      )
+
+      return url
     }
 
-    private constructBaseUrlFromWorkspace(workspace: any): string {
-      if (workspace.baseUrl === undefined) {
-        console.log("WARNING: There was no baseUrl for received workspace.");
-        return "";
-      }
-      return workspace.baseUrl;
+    if (route.baseUrl !== undefined && route.baseUrl.length > 0) {
+      url = route.baseUrl
     }
 
-    private constructRouteUrl(workspace: any, appId: string, productName: string, endpointName: string, params: Record<string, unknown>): string {
-      const route = this.filterRouteFromList(workspace.routes, appId, productName);
-      let url = this.constructBaseUrlFromWorkspace(workspace);
-      if (!route) {
-        console.log(`WARNING: No route.baseUrl could be found for given appId "${appId}" and productName "${productName}"`);
+    url = Location.joinWithSlash(url, this.constructEndpointUrl(route, endpointName, params))
+    return url
+  }
 
-        return url;
-      }
-      
-      if(route.baseUrl !== undefined && route.baseUrl.length > 0){
-        url = route.baseUrl;
-      }
-
-      url = Location.joinWithSlash(url,this.constructEndpointUrl(route, endpointName, params));
-      return url;
+  private constructEndpointUrl(route: any, endpointName: string, params: Record<string, unknown>): string {
+    if (!route.endpoints) {
+      return ''
+    }
+    const finalEndpoint = this.dissolveEndpoint(endpointName, route.endpoints)
+    if (!finalEndpoint || finalEndpoint.path === undefined) {
+      console.log('WARNING: No endpoint or endpoint.path could be found')
+      return ''
     }
 
-    private constructEndpointUrl(route: any, endpointName: string, params: Record<string, unknown>): string {
-      if (!route.endpoints) {
-        return "";  
-      }
-      const finalEndpoint = this.dissolveEndpoint(endpointName, route.endpoints);
-      if (!finalEndpoint || finalEndpoint.path === undefined) {
-        console.log("WARNING: No endpoint or endpoint.path could be found");
-        return "";
-      }
-    
-      const paramsFilled = this.fillParamsForPath(finalEndpoint.path, params);
-      if (paramsFilled === undefined) {
-        console.log("WARNING: Params could not be filled correctly");
-        return "";
-      }
-      
-      return paramsFilled;
+    const paramsFilled = this.fillParamsForPath(finalEndpoint.path, params)
+    if (paramsFilled === undefined) {
+      console.log('WARNING: Params could not be filled correctly')
+      return ''
     }
 
-     private filterRouteFromList(routes: Array<Route>, appId: string, productName: string): Route | undefined {
-      if (!routes) {
-        return undefined;
-      }
-    
-      const productRoutes = routes.filter(route => route.appId === appId && route.productName === productName);
-    
-      if (productRoutes.length === 0) {
-        return undefined;
-      }
-    
-      if (productRoutes.length > 1) {
-        console.log("WARNING: There were more than one route. First route has been used.");
-      }
-    
-      return productRoutes[0]; 
+    return paramsFilled
+  }
+
+  private filterRouteFromList(routes: Array<Route>, appId: string, productName: string): Route | undefined {
+    if (!routes) {
+      return undefined
     }
 
-     private dissolveEndpoint(endpointName: string, endpoints: Array<Endpoint>): Endpoint | undefined {
-      let endpoint = endpoints.find(ep => ep.name === endpointName);
-    
+    const productRoutes = routes.filter((route) => route.appId === appId && route.productName === productName)
+
+    if (productRoutes.length === 0) {
+      return undefined
+    }
+
+    if (productRoutes.length > 1) {
+      console.log('WARNING: There were more than one route. First route has been used.')
+    }
+
+    return productRoutes[0]
+  }
+
+  private dissolveEndpoint(endpointName: string, endpoints: Array<Endpoint>): Endpoint | undefined {
+    let endpoint = endpoints.find((ep) => ep.name === endpointName)
+
+    if (!endpoint) {
+      return undefined
+    }
+
+    while (endpoint.path?.includes(this.aliasStart)) {
+      const path: string = endpoint.path
+      const startIdx = path.indexOf(this.aliasStart) + this.aliasStart.length
+      const endIdx = path.lastIndexOf(this.aliasEnd)
+      if (endIdx <= startIdx) {
+        return undefined
+      }
+      const aliasName = path.substring(startIdx, endIdx)
+      endpoint = endpoints.find((ep) => ep.name === aliasName)
+
       if (!endpoint) {
-        return undefined;  
+        return undefined
       }
-    
-      while (endpoint.path?.includes(this.aliasStart)) {
-        const path : string = endpoint.path;
-        const startIdx = path.indexOf(this.aliasStart) + this.aliasStart.length;
-        const endIdx = path.lastIndexOf(this.aliasEnd);
-        if (endIdx <= startIdx) {
-          return undefined; 
-        }
-        const aliasName = path.substring(startIdx, endIdx);
-        endpoint = endpoints.find(ep => ep.name === aliasName);
-    
-        if (!endpoint) {
-          return undefined; 
-        }
-      }
-    
-      return endpoint;
     }
 
-    private fillParamsForPath( path: string, params: Record<string,unknown>): string{
-      while(path.includes(this.paramStart)){
-        let paramName = path.substring(
-          path.indexOf(this.paramStart) + this.paramStart.length, 
-          path.indexOf(this.paramEnd)
-        );
-        let paramValue = this.getStringFromUnknown(params[paramName]);
-        if(paramValue != undefined && paramValue.length>0){
-          path = path.replace(this.paramStart.concat(paramName).concat(this.paramEnd), paramValue);
-        }
-        else{ 
-          console.log(`WARNING: Searched param "${paramName}" was not found in given param list `);
-          return "";
-        }
-        
-        
+    return endpoint
+  }
 
-      }
-      return path;
-    }
-
-    private getStringFromUnknown(value: unknown): string {
-      if (value === null || value === undefined) {
-        return "";  
-      } else if (typeof value === "string") {
-        return value;  
+  private fillParamsForPath(path: string, params: Record<string, unknown>): string {
+    while (path.includes(this.paramStart)) {
+      let paramName = path.substring(
+        path.indexOf(this.paramStart) + this.paramStart.length,
+        path.indexOf(this.paramEnd)
+      )
+      let paramValue = this.getStringFromUnknown(params[paramName])
+      if (paramValue != undefined && paramValue.length > 0) {
+        path = path.replace(this.paramStart.concat(paramName).concat(this.paramEnd), paramValue)
       } else {
-        return String(value);  
+        console.log(`WARNING: Searched param "${paramName}" was not found in given param list `)
+        return ''
       }
     }
+    return path
+  }
+
+  private getStringFromUnknown(value: unknown): string {
+    if (value === null || value === undefined) {
+      return ''
+    } else if (typeof value === 'string') {
+      return value
+    } else {
+      return String(value)
+    }
+  }
 }
