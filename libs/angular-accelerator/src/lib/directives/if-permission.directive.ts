fbd575d7d608798767673a3978c778b69a221a2c
Benjamin Pabst <Benjamin.Pabst@capgemini.com>
2024-07-18 09:36:29 +0200

    feat: added always grant permission checker

--- libs/angular-accelerator/src/lib/directives/if-permission.directive.ts
+++ libs/angular-accelerator/src/lib/directives/if-permission.directive.ts
@@ -15,6 +15,15 @@ export interface HasPermissionChecker {
   hasPermission(permissionKey: string): boolean
 }
 
+/**
+ * This checker always returns true, basically disabling the permission system on the UI side
+ */
+export class AlwaysGrantPermissionChecker implements HasPermissionChecker {
+  hasPermission(_permissionKey: string): boolean {
+    return true;
+  }
+}
+
 export const HAS_PERMISSION_CHECKER = new InjectionToken<HasPermissionChecker>('hasPermission')
 
 @Directive({ selector: '[ocxIfPermission], [ocxIfNotPermission]' })
