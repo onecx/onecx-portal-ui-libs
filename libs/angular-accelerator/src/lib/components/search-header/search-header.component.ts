e3a9f30b3035a503e2e617b7e7518d6ae78687ad
markuczy <martin.kuczynski@capgemini.com>
2024-07-30 14:40:46 +0200

    feat: search and reset buttons can be disabled

--- libs/angular-accelerator/src/lib/components/search-header/search-header.component.ts
+++ libs/angular-accelerator/src/lib/components/search-header/search-header.component.ts
@@ -49,6 +49,8 @@ export class SearchHeaderComponent implements AfterViewInit {
     this._actions = value
     this.updateHeaderActions()
   }
+  @Input() searchButtonDisabled: boolean = false
+  @Input() resetButtonDisabled: boolean = false
 
   @Output() searched: EventEmitter<any> = new EventEmitter()
   @Output() resetted: EventEmitter<any> = new EventEmitter()
