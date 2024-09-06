3baa19dbe4c4a1e71257a2a41dcfe4acb76e8384
kim.tran <kim.tran@capgemini.com>
2024-09-06 12:08:10 +0200

    fix: rename validator

--- libs/angular-accelerator/src/lib/functions/at-least-one-field-filled-validator.ts
+++ libs/angular-accelerator/src/lib/functions/at-least-one-field-filled-validator.ts
@@ -1,6 +1,6 @@
 import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms'
 
-export const searchCriteriaNotEmptyValidator: ValidatorFn = (form: AbstractControl): ValidationErrors | null => {
+export const atLeastOneFieldFilledValidator: ValidatorFn = (form: AbstractControl): ValidationErrors | null => {
   if (Object.values(form.value).every((x) => x === null || x === undefined || x === '')) {
     return { allFieldsEmpty: true }
   }
