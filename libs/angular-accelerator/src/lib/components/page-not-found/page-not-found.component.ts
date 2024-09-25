e866992217c680a5e51bd36ef9f4e15d57f35ac6
kim.tran <kim.tran@capgemini.com>
2024-09-25 08:01:15 +0200

    fix: edit page not found translations

--- libs/angular-accelerator/src/lib/components/page-not-found/page-not-found.component.ts
+++ libs/angular-accelerator/src/lib/components/page-not-found/page-not-found.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
   template: `<div class="p-4">
-    <h1>{{ 'OCX_PAGE_NOT_FOUND' | translate }}</h1>
-    {{ 'OCX_PAGE_NOT_FOUND_MESSAGE' | translate }}
+    <h1>{{ 'OCX_PAGE_NOT_FOUND.TITLE' | translate }}</h1>
+    {{ 'OCX_PAGE_NOT_FOUND.MESSAGE' | translate }}
   </div> `,
 })
 export class PageNotFoundComponent {}
