d0e1d8dfe0997cbd348e43d88bb67e6cc5b149ea
kim.tran <kim.tran@capgemini.com>
2024-09-26 14:30:08 +0200

    fix: renaming component

--- libs/angular-accelerator/src/lib/components/data-loading-error/data-loading-error.component.ts
+++ libs/angular-accelerator/src/lib/components/data-loading-error/data-loading-error.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core'
  * Show this data not found page component if the main entity of a page could not be loaded.
  */
 @Component({
-  selector: 'ocx-data-not-loaded',
+  selector: 'ocx-data-loading-error',
   template: `<div class="p-4">
-    <h1>{{ 'OCX_DATA_NOT_LOADED.TITLE' | translate }}</h1>
+    <h1>{{ 'OCX_DATA_LOADING_ERROR.TITLE' | translate }}</h1>
     <div style="white-space: pre-line">
-      {{ 'OCX_DATA_NOT_LOADED.MESSAGE' | translate }}
+      {{ 'OCX_DATA_LOADING_ERROR.MESSAGE' | translate }}
     </div>
   </div> `,
 })
-export class DataNotLoadedComponent {}
+export class DataLoadingErrorComponent {}
