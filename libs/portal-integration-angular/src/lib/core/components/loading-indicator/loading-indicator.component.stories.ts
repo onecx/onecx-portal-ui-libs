2adece90d2e24fea38c20031b2fcd3405c83847b
markuczy <129275100+markuczy@users.noreply.github.com>
2024-09-27 16:00:23 +0200

    feat: button-dialog and data-view-controls accessibility (#509)
    * feat: button-dialog and data-view-controls accessibility
    
    * fix: lint
    
    * fix: tooltip instead of title
    
    * fix: removed hoverevent for tooltip

---
+++ libs/portal-integration-angular/src/lib/core/components/loading-indicator/loading-indicator.component.stories.ts
@@ -0,0 +1,29 @@
+import { importProvidersFrom } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { Meta, StoryFn, applicationConfig, moduleMetadata } from '@storybook/angular'
+import { StorybookTranslateModule } from '../../storybook-translate.module'
+import { LoadingIndicatorComponent } from './loading-indicator.component'
+
+export default {
+  title: 'LoadaingIndicatorComponent',
+  component: LoadingIndicatorComponent,
+  decorators: [
+    applicationConfig({
+      providers: [importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)],
+    }),
+    moduleMetadata({
+      declarations: [LoadingIndicatorComponent],
+      imports: [StorybookTranslateModule],
+    }),
+  ],
+} as Meta<LoadingIndicatorComponent>
+
+const Template: StoryFn<LoadingIndicatorComponent> = (args: LoadingIndicatorComponent) => ({
+  props: args,
+})
+
+export const Basic = {
+  render: Template,
+  args: {},
+}
