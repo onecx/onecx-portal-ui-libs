2e9d17272356bd05c40d0478ad47c3e0b2de1aa7
Benjamin Pabst <Benjamin.Pabst@capgemini.com>
2024-10-01 14:53:16 +0200

    feat: added method to correct utc date

--- libs/accelerator/src/lib/utils/date.utils.ts
+++ libs/accelerator/src/lib/utils/date.utils.ts
@@ -2,3 +2,15 @@ export function isValidDate(value: any): value is Date {
     return value instanceof Date && !isNaN(value as any);
   }
   
+
+/**
+ * This function removes time info from a JS DateTime Object and returns
+ * the local date as a correct UTC Date
+ * @param date a date-time Date object
+ * @returns the date without time / timezone issues
+ */
+export function getUTCDateWithoutTimezoneIssues(date: Date) {
+  return new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0),
+  );
+}  
