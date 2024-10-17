a6e021ca0212d3dd3a9a776499267e55bfaf6b64
kim.tran <kim.tran@capgemini.com>
2024-10-17 18:26:38 +0200

    fix: persist displayed columns after page reload for column groups

--- libs/angular-accelerator/src/lib/components/custom-group-column-selector/custom-group-column-selector.component.ts
+++ libs/angular-accelerator/src/lib/components/custom-group-column-selector/custom-group-column-selector.component.ts
@@ -24,7 +24,21 @@ export interface CustomGroupColumnSelectorComponentState {
 })
 export class CustomGroupColumnSelectorComponent implements OnInit {
   @Input() columns: DataTableColumn[] = []
-  @Input() displayedColumns: DataTableColumn[] = []
+  private _displayedColumns: DataTableColumn[] = []
+  @Input()
+  get displayedColumns() {
+    return this._displayedColumns
+  }
+  set displayedColumns(value: DataTableColumn[]) {
+    this._displayedColumns = value
+    this.componentStateChanged.emit({
+      actionColumnConfig: {
+        frozen: this.frozenActionColumn,
+        position: this.actionColumnPosition,
+      },
+      displayedColumns: this._displayedColumns,
+    })
+  }
   @Input() customGroupKey = ''
   @Input() dialogTitle = ''
   @Input() dialogTitleKey = ''
@@ -109,11 +123,6 @@ export class CustomGroupColumnSelectorComponent implements OnInit {
       this.columnSelectionChanged.emit({ activeColumns: [...this.displayedColumnsModel] })
       this.componentStateChanged.emit({
         displayedColumns: [...this.displayedColumnsModel],
-        actionColumnConfig: {
-          frozen: this.frozenActionColumnModel,
-          position: this.actionColumnPositionModel,
-        },
-        activeColumnGroupKey: this.customGroupKey,
       })
     }
 
