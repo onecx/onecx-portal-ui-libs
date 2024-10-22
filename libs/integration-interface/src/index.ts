049e1ce0e9b262720d7aa26620fa8051dcade0a1
markuczy <martin.kuczynski@capgemini.com>
2024-10-22 13:55:37 +0200

    feat: NavigatedEvent type created

--- libs/integration-interface/src/index.ts
+++ libs/integration-interface/src/index.ts
@@ -38,4 +38,5 @@ export * from './lib/topics/permissions-rpc/v1/permissions-rpc.model'
 
 export * from './lib/topics/events/v1/events-topic'
 export * from './lib/topics/events/v1/topic-event-type'
+export * from './lib/topics/events/v1/navigated-event-type'
 export * from './lib/topics/events/v1/navigated-event-payload'
