9e3fd15b59a621764a69c93d5b17663179caea72
markuczy <martin.kuczynski@capgemini.com>
2024-11-13 13:43:03 +0100

    fix: checkbox checked when all selectable items selected

--- libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
+++ libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
@@ -422,6 +422,7 @@ export class DataTableComponent extends DataSortBase implements OnInit, AfterCon
         )
       )
     )
+    this.rowSelectable = this.rowSelectable.bind(this)
   }
 
   ngOnInit(): void {
@@ -662,6 +663,14 @@ export class DataTableComponent extends DataSortBase implements OnInit, AfterCon
         })
       })
     )
+  }
+
+  isRowSelectionDisabled(rowObject: Row) {
+    return !!this.selectionEnabledField && !this.fieldIsTruthy(rowObject, this.selectionEnabledField)
+  }
+
+  rowSelectable(event: any) {
+    return !this.isRowSelectionDisabled(event.data)
   }
 
   onSelectionChange(selection: Row[]) {
