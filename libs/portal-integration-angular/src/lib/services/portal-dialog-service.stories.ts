39230d616b8a8b255a3594f3b46d0b391472cf0e
markuczy <129275100+markuczy@users.noreply.github.com>
2024-12-10 14:03:33 +0100

    feat: allow to specify autofocus for buttons (#634)
    * feat: allow to specify autofocus for buttons
    
    * fix: fixed lint
    
    * fix: storybook
    
    * fix: reverted ngIf removal

--- libs/portal-integration-angular/src/lib/services/portal-dialog-service.stories.ts
+++ libs/portal-integration-angular/src/lib/services/portal-dialog-service.stories.ts
@@ -1,10 +1,10 @@
-import { Component, Input, importProvidersFrom } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, importProvidersFrom } from '@angular/core'
 import { Meta, applicationConfig, argsToTemplate, componentWrapperDecorator, moduleMetadata } from '@storybook/angular'
 import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog'
 import { BrowserModule } from '@angular/platform-browser'
 import { ButtonModule } from 'primeng/button'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { PortalDialogService } from './portal-dialog.service'
+import { DialogCustomButtonsDisabled, PortalDialogService } from './portal-dialog.service'
 import { ButtonDialogComponent } from '../core/components/button-dialog/button-dialog.component'
 import { StorybookTranslateModule } from '../core/storybook-translate.module'
 import { DialogMessageContentComponent } from '../core/components/button-dialog/dialog-message-content/dialog-message-content.component'
@@ -12,7 +12,7 @@ import { PrimeIcons } from 'primeng/api'
 import { TooltipModule } from 'primeng/tooltip'
 
 @Component({
-  selector: 'ocx-ocx-button-dialog-with-portal-dialog-service',
+  selector: 'ocx-button-dialog-with-portal-dialog-service',
   template: `<button (click)="openDialog()">Open dialog</button>`,
 })
 class ButtonDialogWithPortalDialogServiceComponent {
@@ -88,6 +88,27 @@ export const CustomData = {
   },
 }
 
+export const CustomAutofocus = {
+  render: (args: any) => ({
+    props: {
+      ...args,
+    },
+    template: `
+        <ocx-button-dialog-with-portal-dialog-service ${argsToTemplate(args)}>
+        </ocx-button-dialog-with-portal-dialog-service>
+          `,
+  }),
+  args: {
+    title: 'Custom title',
+    messageOrComponent: 'Custom message',
+    primaryKey: 'Primary Button',
+    secondaryKey: 'Secondary Button',
+    extras: {
+      autoFocusButton: 'secondary',
+    },
+  },
+}
+
 export const CustomDataWithExtendedButtons = {
   render: (args: any) => ({
     props: {
@@ -151,5 +172,99 @@ export const ComponentDisplayed = {
       tooltipPosition: 'left',
     },
     extras: {},
+  },
+}
+
+@Component({
+  selector: 'ocx-my-component-to-display',
+  template: `<p>Hello, its my component to display custom buttons</p>`,
+})
+class ComponentToDisplayCustomButtonsComponent implements DialogCustomButtonsDisabled, OnInit {
+  customButtonEnabled: EventEmitter<{ id: string; enabled: boolean }> = new EventEmitter()
+
+  ngOnInit(): void {
+    this.customButtonEnabled.emit({ id: 'custom1', enabled: true })
+  }
+}
+
+export const CustomButtons = {
+  render: (args: any) => ({
+    props: {
+      ...args,
+    },
+    template: `
+            <ocx-button-dialog-with-portal-dialog-service ${argsToTemplate(args)}>
+            </ocx-button-dialog-with-portal-dialog-service>
+              `,
+  }),
+  args: {
+    title: 'Custom title',
+    messageOrComponent: {
+      type: ComponentToDisplayCustomButtonsComponent,
+    },
+    primaryKey: {
+      key: 'PRIMARY_KEY',
+      icon: PrimeIcons.BOOKMARK,
+      tooltipKey: 'TOOLTIP_KEY',
+      tooltipPosition: 'right',
+    },
+    secondaryKey: {
+      key: 'SECONDARY_KEY',
+      icon: PrimeIcons.SEARCH,
+      tooltipKey: 'TOOLTIP_KEY',
+      tooltipPosition: 'left',
+    },
+    extras: {
+      customButtons: [
+        {
+          id: 'custom1',
+          alignment: 'right',
+          key: 'CUSTOM_KEY',
+          icon: 'pi pi-times',
+        },
+      ],
+    },
+  },
+}
+
+export const CustomButtonsWithAutofocus = {
+  render: (args: any) => ({
+    props: {
+      ...args,
+    },
+    template: `
+            <ocx-button-dialog-with-portal-dialog-service ${argsToTemplate(args)}>
+            </ocx-button-dialog-with-portal-dialog-service>
+              `,
+  }),
+  args: {
+    title: 'Custom title',
+    messageOrComponent: {
+      type: ComponentToDisplayCustomButtonsComponent,
+    },
+    primaryKey: {
+      key: 'PRIMARY_KEY',
+      icon: PrimeIcons.BOOKMARK,
+      tooltipKey: 'TOOLTIP_KEY',
+      tooltipPosition: 'right',
+    },
+    secondaryKey: {
+      key: 'SECONDARY_KEY',
+      icon: PrimeIcons.SEARCH,
+      tooltipKey: 'TOOLTIP_KEY',
+      tooltipPosition: 'left',
+    },
+    extras: {
+      customButtons: [
+        {
+          id: 'custom1',
+          alignment: 'right',
+          key: 'CUSTOM_KEY',
+          icon: 'pi pi-times',
+        },
+      ],
+      autoFocusButton: 'custom',
+      autoFocusButtonCustomId: 'custom1',
+    },
   },
 }
