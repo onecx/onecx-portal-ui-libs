48fc907afc90fead87b6d18f98c3927a37bb417c
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-01-13 12:34:59 +0100

    test: add tests for new createTranslateLoader

--- libs/angular-utils/src/lib/utils/create-translate-loader.utils.spec.ts
+++ libs/angular-utils/src/lib/utils/create-translate-loader.utils.spec.ts
@@ -1,7 +1,67 @@
-// TODO copy + refactor tests from angular-accelerator
+import { TestBed } from '@angular/core/testing'
+import { MockService } from 'ng-mocks'
+import { createTranslateLoader, TRANSLATION_PATH } from './create-translate-loader.utils'
+import { Injector, runInInjectionContext } from '@angular/core'
+import { TranslationCacheService } from '../services/translation-cache.service'
+import { HttpClient } from '@angular/common/http'
+import { of } from 'rxjs'
 
 describe('CreateTranslateLoader', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
+  const httpClientMock = MockService(HttpClient)
+  httpClientMock.get = jest.fn(() => of({}, {}, {})) as any
+  let translationCacheService: TranslationCacheService
+  let injector: Injector
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TRANSLATION_PATH,
+          useValue: 'path_1',
+          multi: true,
+        },
+        {
+          provide: TRANSLATION_PATH,
+          useValue: 'path_2',
+          multi: true,
+        },
+        {
+          provide: TRANSLATION_PATH,
+          useValue: of("path_3"),
+          multi: true,
+        },
+      ],
+    }).compileComponents()
+
+    injector = TestBed.inject(Injector)
+    translationCacheService = TestBed.inject(TranslationCacheService)
+    window['onecxTranslations'] = {}
+    jest.clearAllMocks()
+  })
+
+  describe('without TranslationCache parameter', () => {
+    it('should call httpClient for each TRANSLATION_PATH', (done) => {
+      const translateLoader = runInInjectionContext(injector, () =>
+        createTranslateLoader(httpClientMock, undefined)
+      )
+
+      translateLoader.getTranslation('en').subscribe(() => {
+        expect(httpClientMock.get).toHaveBeenCalledTimes(3)
+        done()
+      })
+    })
+  })
+
+  describe('with TranslationCache parameter', () => {
+    it('should call httpClient for each TRANSLATION_PATH', (done) => {
+      const translateLoader = runInInjectionContext(injector, () =>
+        createTranslateLoader(httpClientMock, undefined, translationCacheService)
+      )
+
+      translateLoader.getTranslation('en').subscribe(() => {
+        expect(httpClientMock.get).toHaveBeenCalledTimes(3)
+        done()
+      })
+    })
   })
 })
