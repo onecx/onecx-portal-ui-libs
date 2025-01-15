fc8a3d3043394730b351ca88b30d0f786b347816
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2025-01-15 08:19:28 +0100

    feat: standalone mode (#614)
    * feat: initial version of standalone mode
    
    * feat: initial version of standalone mode
    
    * fix: fix linter
    
    * fix: add graceful fallback for empty env.json load
    
    * feat: move translation loaders to angular-integration-interface, improve createTranslateLoader and deprecate exports in angular-accelerator
    
    * feat: ensure unique translations in new create-translate-loader
    
    * feat: migrate standalone shell to a module containing shell-viewport wrapper component
    
    * fix: fix CI
    
    * fix: disable oneCXShellLayout by default in standalone-shell
    
    * fix: fix ci
    
    * refactor: provide PIA TRANSLATION_PATH from within PIA
    
    * refactor: provide TRANSLATION_PATH from within angular accelerator
    
    * feat: inject translation paths in libs and remove dependency between shell-core and pia
    
    * refactor: move permission checker utils to angular-integration-interface and remove redundant dependency in standalone-shell
    
    * refactor: remove dependency between shell-core and angular-accelerator
    
    * refactor: move utils to new lib, fix peer deps and peer dep linting
    
    * fix: fix storybook imports
    
    * refactor: export PROVIDE_STANDALONE_PROVIDERS_CONFIG
    
    * refactor: extract and test hasPermissionCheckerFactory
    
    * test: add tests for new createTranslateLoader
    
    * refactor: improve providers
    
    * fix: provide default value for inject
    
    * test: add tests for createTranslateLoader without injected TRANSLATION_PATH
    
    * fix: update package versions

---
+++ libs/angular-utils/src/lib/utils/caching-translate-loader.utils.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http'
+import { TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { Observable } from 'rxjs'
+import { TranslationCacheService } from '../services/translation-cache.service'
+
+export class CachingTranslateLoader implements TranslateLoader {
+  private translateLoader: TranslateHttpLoader
+
+  constructor(
+    private translationCache: TranslationCacheService,
+    private http: HttpClient,
+    private prefix?: string,
+    private suffix?: string
+  ) {
+    this.translateLoader = new TranslateHttpLoader(this.http, this.prefix, this.suffix)
+  }
+
+  getTranslation(lang: string): Observable<any> {
+    const url = `${this.prefix}${lang}${this.suffix}`
+
+    return this.translationCache.getTranslationFile(url, () => this.translateLoader.getTranslation(lang))
+  }
+}
