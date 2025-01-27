34a0be23fcbf944327711aaec1b7d2c269088a0b
Annika Nowak <139357202+anninowak@users.noreply.github.com>
2025-01-27 09:56:39 +0100

    feat: clean-up of unused files

--- libs/angular-utils/src/lib/theme/custom-preset.ts
+++
@@ -1,8 +0,0 @@
-//mypreset.ts
-import { definePreset } from '@primeng/themes'
-import Aura from '@primeng/themes/aura'
-import presetVariables from './experimental/preset-variables'
-
-const CustomPreset = definePreset(Aura, presetVariables)
-
-export default CustomPreset
