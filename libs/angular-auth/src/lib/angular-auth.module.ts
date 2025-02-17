65c9545d947e9e59b466d95ca3e4678e2d8a8e9a
kim.tran <kim.tran@capgemini.com>
2025-02-17 14:28:30 +0100

    feat: remove keycloak-auth and keycloak-angular, changes in angular-auth

--- libs/angular-auth/src/lib/angular-auth.module.ts
+++ libs/angular-auth/src/lib/angular-auth.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core'
+import { APP_INITIALIZER, NgModule, inject, provideAppInitializer } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { ConfigurationService } from '@onecx/angular-integration-interface'
@@ -7,28 +7,20 @@ import { AuthService } from './auth.service'
 import { AuthServiceWrapper } from './auth-service-wrapper'
 import { KeycloakAuthService } from './auth_services/keycloak-auth.service'
 import { AuthProxyService } from './auth-proxy.service'
-import { KeycloakService } from 'keycloak-angular'
-
-function appInitializer(configService: ConfigurationService, authService: AuthService) {
-  return async () => {
-    await configService.isInitialized
-    await authService.init()
-  }
-}
 
 function provideAuthServices() {
-  return [AuthServiceWrapper, KeycloakAuthService, KeycloakService]
+  return [AuthServiceWrapper, KeycloakAuthService]
 }
 
 export function provideAuthService() {
   return [
     provideAuthServices(),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: appInitializer,
-      deps: [ConfigurationService, AuthServiceWrapper],
-      multi: true,
-    },
+    provideAppInitializer(async () => {
+      const configService = inject(ConfigurationService)
+      const authServiceWrapper = inject(AuthServiceWrapper)
+      await configService.isInitialized
+      await authServiceWrapper.init()
+    }),
   ]
 }
 
