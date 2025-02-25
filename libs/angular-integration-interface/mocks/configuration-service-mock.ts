3145551e0a9865f95b1090359a58770604f82fa3
Annika Nowak <139357202+anninowak@users.noreply.github.com>
2025-02-25 12:04:23 +0100

    feat: configuration service mock improved

--- libs/angular-integration-interface/mocks/configuration-service-mock.ts
+++ libs/angular-integration-interface/mocks/configuration-service-mock.ts
@@ -3,6 +3,8 @@ import { Config, ConfigurationTopic } from '@onecx/integration-interface'
 import { APP_CONFIG } from '../src/lib/api/injection-tokens'
 import { CONFIG_KEY } from '../src/lib/model/config-key.model'
 import { FakeTopic } from './fake-topic'
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom'
+import { KeyConfiguration } from '@onecx/ngrx-accelerator'
 
 @Injectable({ providedIn: 'root' })
 export class ConfigurationServiceMock {
@@ -22,12 +24,12 @@ export class ConfigurationServiceMock {
     return this.defaultConfig ? this.defaultConfig[key] : undefined
   }
 
-  // unsure
-  // public async setProperty(key: string, val: string) {
-  //   const currentValues = await firstValueFrom(this.config$.asObservable())
-  //   currentValues[key] = val
-  //   await this.config$.publish(currentValues)
-  // }
+  // TODO: improve as Config
+  public async setProperty(key: string, val: string) {
+    const currentValues = (await firstValueFrom(this.config$.asObservable())) as Config
+    currentValues[key] = val
+    await this.config$.publish(currentValues)
+  }
 
   public getConfig(): Config | undefined {
     return this.defaultConfig
