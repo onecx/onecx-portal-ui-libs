1f60e53194aa19dbb85f4450c5fa620dd36001bd
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-02-28 10:16:57 +0100

    fix: cleanup basic layout: header, footer, menu (#676)
    * fix: cleanup basic layout: header, footer, menu
    
    * fix: remove comment, use 2px for menu separator

--- libs/shell-core/src/lib/components/portal-footer/portal-footer.component.ts
+++ libs/shell-core/src/lib/components/portal-footer/portal-footer.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, OnInit, Optional } from '@angular/core'
+import { Component, Inject, Optional } from '@angular/core'
 import { Router } from '@angular/router'
 import { AppStateService, CONFIG_KEY, ConfigurationService, ThemeService } from '@onecx/angular-integration-interface'
-import { Observable, combineLatest, concat, filter, map, mergeMap, of, withLatestFrom } from 'rxjs'
+import { Observable, combineLatest, filter, map, mergeMap, of, withLatestFrom } from 'rxjs'
 import {
   WORKSPACE_CONFIG_BFF_SERVICE_PROVIDER,
   WorkspaceConfigBffService,
@@ -12,9 +12,8 @@ import {
   templateUrl: './portal-footer.component.html',
   styleUrls: ['./portal-footer.component.scss'],
 })
-export class PortalFooterComponent implements OnInit {
+export class PortalFooterComponent {
   logoUrl$: Observable<string | undefined>
-  copyrightMsg$: Observable<string> | undefined
   versionInfo$: Observable<string | undefined>
 
   constructor(
@@ -49,26 +48,6 @@ export class PortalFooterComponent implements OnInit {
         }
         return of(theme.logoUrl || portalData.logoUrl)
       })
-    )
-  }
-
-  ngOnInit(): void {
-    this.copyrightMsg$ = concat(
-      of('All rights reserved.'),
-      this.appState.currentWorkspace$.pipe(
-        map((portalData) => {
-          if (
-            !(
-              portalData.footerLabel === '' ||
-              portalData.footerLabel === 'string' ||
-              portalData.footerLabel === undefined
-            )
-          ) {
-            return portalData.companyName || portalData.footerLabel || 'All rights reserved.'
-          }
-          return ''
-        })
-      )
     )
   }
 
