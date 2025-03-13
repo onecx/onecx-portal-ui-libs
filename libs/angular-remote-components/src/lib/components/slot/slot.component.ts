88ed0d72ca65cc482dcbda1ab4c826e5a33d310e
markuczy <129275100+markuczy@users.noreply.github.com>
2025-03-13 09:39:07 +0100

    feat: theming adjustments for angular 18 (#677)
    * feat: layout-styles update and slot data-style attributes added
    
    * feat: export platformCache and getNgZone utils
    
    * feat: replaced AppStateService import to remove pia dependency
    
    * feat: data style change
    
    * fix: remove pia from angular-webcomponents
    
    * feat: export window state getter

--- libs/angular-remote-components/src/lib/components/slot/slot.component.ts
+++ libs/angular-remote-components/src/lib/components/slot/slot.component.ts
@@ -176,6 +176,9 @@ export class SlotComponent implements OnInit, OnDestroy {
     viewContainer?.element.nativeElement.replaceChildren()
     if (componentType) {
       const componentRef = viewContainer?.createComponent<any>(componentType)
+      const componentHTML = componentRef?.location.nativeElement as HTMLElement
+      this.addDataStyleId(componentHTML, componentInfo.remoteComponent)
+      this.addDataStyleIsolation(componentHTML)
       if (componentRef && 'ocxInitRemoteComponent' in componentRef.instance) {
         ;(componentRef.instance as ocxRemoteComponent).ocxInitRemoteComponent({
           appId: componentInfo.remoteComponent.appId,
@@ -192,6 +195,8 @@ export class SlotComponent implements OnInit, OnDestroy {
     ) {
       if (componentInfo.remoteComponent.elementName) {
         const element = document.createElement(componentInfo.remoteComponent.elementName)
+        this.addDataStyleId(element, componentInfo.remoteComponent)
+        this.addDataStyleIsolation(element)
         ;(element as any)['ocxRemoteComponentConfig'] = {
           appId: componentInfo.remoteComponent.appId,
           productName: componentInfo.remoteComponent.productName,
@@ -204,6 +209,14 @@ export class SlotComponent implements OnInit, OnDestroy {
     }
 
     return
+  }
+
+  private addDataStyleId(element: HTMLElement, rcInfo: RemoteComponentInfo) {
+    element.dataset['styleId'] = `${rcInfo.productName}|${rcInfo.appId}`
+  }
+
+  private addDataStyleIsolation(element: HTMLElement) {
+    element.dataset['styleIsolation'] = ''
   }
 
   private updateComponentData(
