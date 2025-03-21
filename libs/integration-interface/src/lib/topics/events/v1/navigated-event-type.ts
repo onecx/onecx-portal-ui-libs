5538c33ee54fc2f2fa34ec7d579f79c57b95ec7b
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-03-21 09:55:22 +0100

    fix: add peer dependencies and fix deprecation warning

--- libs/integration-interface/src/lib/topics/events/v1/navigated-event-type.ts
+++ libs/integration-interface/src/lib/topics/events/v1/navigated-event-type.ts
@@ -1,7 +1,7 @@
 import { NavigatedEventPayload } from './navigated-event-payload'
 // TODO: Write migration that detects usage of EventTopic with type 'navigated' and throws a warning/manual todo
 /**
- * @deprecated Use LocationTopic instead of EventsTopic for navigated events
+ * @deprecated Use CurrentLocationTopic instead of EventsTopic for navigated events
  */
 export type NavigatedEvent = {
   type: 'navigated'
