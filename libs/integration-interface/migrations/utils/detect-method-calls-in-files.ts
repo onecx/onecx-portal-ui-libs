74d95334097bbef55d141a0222fb16f3ca020c9e
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-04-11 11:50:50 +0200

    refactor: improve warn-for-events-topic-navigated migration + refactor utils

--- libs/integration-interface/migrations/utils/detect-method-calls-in-files.ts
+++ libs/integration-interface/migrations/utils/detect-method-calls-in-files.ts
@@ -1,10 +1,66 @@
 import { Tree, visitNotIgnoredFiles } from '@nx/devkit'
 import { ast, query } from '@phenomnomnominal/tsquery'
-import { CallExpression, isBinaryExpression, isCallExpression, isExpressionStatement, isPropertyDeclaration, isVariableDeclaration, SyntaxKind } from 'typescript'
+import { CallExpression, isBinaryExpression, isCallExpression, isExpressionStatement, isPropertyDeclaration, isVariableDeclaration, SourceFile, SyntaxKind } from 'typescript'
 
 type MatchingMethodCalls = Map<string, CallExpression[]>
+type DeclarationType = 'PropertyDeclaration' | 'VariableDeclaration'
 
-// TODO: Refactor and add comments
+function getDeclarationNames(contentAst: SourceFile, className: string, type: DeclarationType): string[] {
+  const declarations = query(
+    contentAst,
+    `${type}:has(NewExpression > Identifier[name="${className}"])`
+  )
+  const memberNames = declarations.map((node) => {
+    if ((type === 'VariableDeclaration' && isVariableDeclaration(node)) || (type === 'PropertyDeclaration' && isPropertyDeclaration(node))) {
+      return node.name.getText()
+    }
+    throw new Error(`Node is not a ${type}`)
+  })
+
+  return memberNames
+}
+
+function getAssignmentNames(contentAst: SourceFile, className: string): string[] {
+  const assignments = query(
+    contentAst,
+    `ExpressionStatement:has(BinaryExpression:has(EqualsToken):has(NewExpression > Identifier[name="${className}"]))`
+  )
+  const assignmentNames = assignments.map((node) => {
+    if (isExpressionStatement(node)) {
+      const binaryExpression = node.expression
+      if (isBinaryExpression(binaryExpression)) {
+        if (binaryExpression.operatorToken.kind == SyntaxKind.EqualsToken) {
+          // TODO: Improve typing
+          return ((binaryExpression.left as any).name.escapedText)
+        }
+      }
+    }
+    throw new Error('Node is not an ExpressionStatement')
+  })
+
+  return assignmentNames
+}
+
+function getIdentifierCallQueries(contentAst: SourceFile, className: string, methodName: string): string[] {
+  const variableNames = getDeclarationNames(contentAst, className, 'VariableDeclaration')
+
+  const memberNames = getDeclarationNames(contentAst, className, 'PropertyDeclaration')
+
+  const assignmentNames = getAssignmentNames(contentAst, className);
+
+  const allIdentifiers = [...variableNames, ...memberNames, ...assignmentNames]
+
+  // Only check variables once, even if their value is set multiple times
+  const distinctIdentifiers = Array.from(new Set(allIdentifiers))
+
+  const identifierCallPatterns = distinctIdentifiers.map(
+    (name) =>
+      `CallExpression:has(PropertyAccessExpression > Identifier[name="${name}"]):has(Identifier[name="${methodName}"])`
+  )
+
+  return identifierCallPatterns
+}
+
 export function detectMethodCallsInFiles(
   tree: Tree,
   rootDir: string,
@@ -13,65 +69,27 @@ export function detectMethodCallsInFiles(
 ): MatchingMethodCalls {
   const matchingFiles: MatchingMethodCalls = new Map()
 
+  // Look at each file inside of specified rootDir or any of its subdirectories that isn't gitignored
   visitNotIgnoredFiles(tree, rootDir, (file) => {
     const content = tree.read(file, 'utf-8')
-    // TODO: Check if this works if empty file exists in tree
     if (!content) return
-
     const contentAst = ast(content)
 
-    const variableDeclarations = query(
-      contentAst,
-      `VariableDeclaration:has(NewExpression > Identifier[name="${className}"])`
-    )
-    const variableNames = variableDeclarations.map((node) => {
-      if (isVariableDeclaration(node)) return node.name.getText()
-      throw new Error('Node is not a VariableDeclaration')
-    })
+    // Queries for method calls on identifiers (e.g. x.method() or this.x.method())
+    const identifierCallPatterns = getIdentifierCallQueries(contentAst, className, methodName)
 
-    const memberDeclarations = query(
-      contentAst,
-      `PropertyDeclaration:has(NewExpression > Identifier[name="${className}"])`
-    )
-    const memberNames = memberDeclarations.map((node) => {
-      if (isPropertyDeclaration(node)) return node.name.getText()
-      throw new Error('Node is not a PropertyDeclaration')
-    })
-
-    const assignments = query(
-      contentAst,
-      `ExpressionStatement:has(BinaryExpression:has(EqualsToken):has(NewExpression > Identifier[name="${className}"]))`
-    )
-    const assignmentNames = assignments.map((node) => {
-      if (isExpressionStatement(node)) {
-        const binaryExpression = node.expression
-        if (isBinaryExpression(binaryExpression)) {
-          if (binaryExpression.operatorToken.kind == SyntaxKind.EqualsToken) {
-            // TODO: Improve typing
-            return ((binaryExpression.left as any).name.escapedText)
-          }
-        }
-      }
-      throw new Error('Node is not a VariableDeclaration')
-    })
-
-    const allIdentifiers = [...variableNames, ...memberNames, ...assignmentNames]
-
-    // Only check variables once, even if their value is set multiple times
-    const distinctIdentifiers = Array.from(new Set(allIdentifiers))
-
-    // Detect method calls on identifiers (e.g. x.method() or this.x.method())
-    const identifierCallPatterns = distinctIdentifiers.map(
-      (name) =>
-        `CallExpression:has(PropertyAccessExpression > Identifier[name="${name}"]):has(Identifier[name="${methodName}"])`
-    )
-
-    // Detect inline method calls (e.g. new ClassName().method())
+    // Query for inline method calls (e.g. new ClassName().method())
     const inlineCallPattern = `CallExpression:has(NewExpression > Identifier[name="${className}"]):has(Identifier[name="${methodName}"])`
 
     const allPatterns = [...identifierCallPatterns, inlineCallPattern].join(', ')
+
+    // Search for all elements matching computed queries
     let potentialMethodCalls: CallExpression[] = query(contentAst, allPatterns)
+
+    // If no potential method calls are found, file can be ignored
     if (potentialMethodCalls.length === 0) return
+
+    // Ensure that all detected method calls are indeed CallExpressions and filter out any other types
     potentialMethodCalls = potentialMethodCalls.filter((node) => isCallExpression(node))
     matchingFiles.set(file, potentialMethodCalls)
   })
--- libs/integration-interface/migrations/v5/warn-for-events-topic-navigated.ts
+++ libs/integration-interface/migrations/v5/warn-for-events-topic-navigated.ts
@@ -1,15 +1,52 @@
 import { Tree } from '@nx/devkit'
-import { findPatternInFiles } from '../utils/find-pattern-in-files'
 import printWarnings from '../utils/print-warnings'
+import { detectMethodCallsInFiles } from '../utils/detect-method-calls-in-files'
+import { CallExpression } from 'typescript'
+import { ast, query } from '@phenomnomnominal/tsquery'
+
+/*
+  Case 1:
+  new EventsTopic().pipe(filter((e) => e.type === 'navigated'))
+  Case 2:
+  const eventsTopic = new EventsTopic()
+  eventsTopic.pipe(filter((e) => e.type === 'navigated'))
+  Case 3:
+  public eventsTopic = new EventsTopic()
+  this.eventsTopic.pipe(filter((e) => e.type === 'navigated'))
+ */
+
+function argsContainFilterForTypeNavigated(callExpression: CallExpression): boolean {
+  const callExpressionAst = ast(callExpression.getText())
+  const filterCallsSelector = `CallExpression:has(Identifier[name="filter"]):has(ArrowFunction:has(BinaryExpression:has(PropertyAccessExpression:has(Identifier):has(Identifier[name="type"])):has(StringLiteral[value="navigated"])))`
+  const filterCalls = query(callExpressionAst, filterCallsSelector)
+  return filterCalls.length > 0
+}
 
 export default async function warnForEventsTopicNavigated(tree: Tree) {
-  const warning =
-    '⚠️ You might be using EventsTopic to detect navigations. Please switch to using CurrentLocationTopic or appStateService.currentLocation$.'
-  const pattern = /filter\(\s*\(e\)\s*=>\s*e\.type\s*===\s*['"]navigated['"]\s*\)/
-  console.log("WarnForEventsTopicNavigated")
-  
-  const results = findPatternInFiles(tree, './src', pattern)
+  const warningForPotentialMatches =
+    '⚠️ EventsTopic usages were detected. Please double-check if EventsTopic is used for subscribing to navigation events and switch to CurrentLocationTopic/appStateService.currentLocation$ if so.'
 
-  console.log("Results", results)
-  printWarnings(warning, results)
+  const warningForExactMatches =
+    '⚠️ You are using EventsTopic to publish navigation events. Please switch to using CurrentLocationTopic/appStateService.currentLocation$.'
+
+  const potentialMatches = detectMethodCallsInFiles(tree, './src', 'pipe', 'EventsTopic')
+
+  const exactMatches = new Map<string, CallExpression[]>()
+  const remainingPotentialMatches = new Map<string, CallExpression[]>()
+
+  potentialMatches.forEach((calls, file) => {
+    const filteredCalls = calls.filter((call) => argsContainFilterForTypeNavigated(call))
+    if (filteredCalls.length > 0) {
+      exactMatches.set(file, filteredCalls)
+    } else {
+      remainingPotentialMatches.set(file, calls)
+    }
+  })
+
+  if (exactMatches.size > 0) {
+    printWarnings(warningForExactMatches, Array.from(exactMatches.keys()))
+  }
+  if (remainingPotentialMatches.size > 0) {
+    printWarnings(warningForPotentialMatches, Array.from(remainingPotentialMatches.keys()))
+  }
 }
