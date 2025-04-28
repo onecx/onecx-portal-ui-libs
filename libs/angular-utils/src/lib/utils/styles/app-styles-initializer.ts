af17ea8ea0419380beb55b218956ea12c8f58f23
markuczy <129275100+markuczy@users.noreply.github.com>
2025-04-28 14:48:04 +0200

    feat: replace prefix of primeng variables based on scope

---
+++ libs/angular-utils/src/lib/utils/styles/app-styles-initializer.ts
@@ -0,0 +1,21 @@
+import { inject, provideEnvironmentInitializer } from '@angular/core'
+import { getAppStyleByScope, replaceStyleContent } from './styles.utils'
+import { getScopeIdentifier, replacePrefix } from '../scope.utils'
+import { AppStateService, REMOTE_COMPONENT_CONFIG } from '@onecx/angular-integration-interface'
+import { SKIP_STYLE_SCOPING } from '../../services/custom-use-style.service'
+
+export function provideAppStylesInitializer() {
+  return [provideEnvironmentInitializer(updateAppStyle)]
+}
+
+async function updateAppStyle() {
+  const appStateService = inject(AppStateService)
+  const skipStyleScoping = inject(SKIP_STYLE_SCOPING, { optional: true }) ?? undefined
+  const remoteComponentConfig = inject(REMOTE_COMPONENT_CONFIG, { optional: true }) ?? undefined
+  const scopeId = await getScopeIdentifier(appStateService, skipStyleScoping, remoteComponentConfig)
+
+  const styleElement = getAppStyleByScope(scopeId)
+  if (styleElement && styleElement.textContent) {
+    replaceStyleContent(styleElement, replacePrefix(styleElement.textContent, scopeId))
+  }
+}
