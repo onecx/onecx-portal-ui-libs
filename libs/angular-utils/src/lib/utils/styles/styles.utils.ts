8216919c192a486fc10f38bc06390edb21b1a964
markuczy <129275100+markuczy@users.noreply.github.com>
2025-05-06 14:19:19 +0200

    fix: revert app style scoping change

--- libs/angular-utils/src/lib/utils/styles/styles.utils.ts
+++ libs/angular-utils/src/lib/utils/styles/styles.utils.ts
@@ -103,13 +103,13 @@ export function createScopedCss(css: string, scopeId: string): string {
   return isScopeSupported
     ? `
   ${extractRootRules(css)}
-@scope([${dataStyleIdAttribute}="${scopeId}"][${dataNoPortalLayoutStylesAttribute}]) to ([${dataStyleIsolationAttribute}]) {
+@scope([${dataStyleIdAttribute}="${scopeId}"]) to ([${dataStyleIsolationAttribute}]) {
         ${extractNonRootRules(css)}
     }
 `
     : `
   ${extractRootRules(css)}
-@supports (@scope([${dataStyleIdAttribute}="${scopeId}"][${dataNoPortalLayoutStylesAttribute}]) to ([${dataStyleIsolationAttribute}])) {
+@supports (@scope([${dataStyleIdAttribute}="${scopeId}"]) to ([${dataStyleIsolationAttribute}])) {
         ${extractNonRootRules(css)}
     }
 `
