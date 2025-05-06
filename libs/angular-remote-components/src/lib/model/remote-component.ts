23013580ddb40e6da5d94862f0d6c3cdf6588f87
markuczy <129275100+markuczy@users.noreply.github.com>
2025-05-06 13:02:36 +0200

    feat: style isolation (#713)
    * feat: style isolation utils improvement
    
    * feat: remove angular-remote-components from angular-utils and prepare style isolation utils
    
    * fix: remove literal strings for attributes and fix shell style scoping
    
    * feat: restructure css fetch utils
    
    * fix: slot ng destroy style sheets modifications
    
    * fix: slot standalone revert
    
    * feat: validate css fetching
    
    * fix: removal of style sheet on failed css fetch
    
    * feat: replace prefix of primeng variables based on scope
    
    * fix: revert slot service change
    
    * feat: add comments and refactor method
    
    * refactor: docu changes
    
    * fix: lint fix
    
    * refactor: full JSDoc for utils
    
    * fix: typo fix
    
    * refacor: added docu for slot destroy
    
    * Update libs/angular-utils/src/lib/utils/styles/rc-styles.utils.ts
    
    Co-authored-by: Benjamin <166110368+BenjaminPabst@users.noreply.github.com>
    
    * Update libs/angular-utils/src/lib/utils/styles/styles.utils.ts
    
    Co-authored-by: Benjamin <166110368+BenjaminPabst@users.noreply.github.com>
    
    * feat: remove styles from shell-core
    
    * fix: test fix
    
    * fix: reverted remote components model and injection token move
    
    ---------
    
    Co-authored-by: Benjamin <166110368+BenjaminPabst@users.noreply.github.com>

--- libs/angular-remote-components/src/lib/model/remote-component.ts
+++ libs/angular-remote-components/src/lib/model/remote-component.ts
@@ -1,4 +1,4 @@
-import { RemoteComponentConfig } from './remote-component-config.model'
+import { RemoteComponentConfig } from '@onecx/angular-utils'
 
 export interface ocxRemoteComponent {
   ocxInitRemoteComponent(config: RemoteComponentConfig): void
