aa5251acad07d5e0f4c9df6a6f422954c34e91e0
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2025-05-07 13:40:45 +0200

    feat: add release-with-migrations executor (#716)
    * fix: move migrations to src of library
    
    * refactor: revert migration refactor
    
    * feat: add release-with-migrations executor
    
    * fix: fix package-lock
    
    * refactor: improve error handling and typings
    
    * docs: add inline documentation to executor
    
    * fix: improve error handling and remove broken test
    
    * refactor: generalize executor and reuse it in all libs
    
    * refactor: improve executor
    
    * refactor: re-add build-migrations to package.json command

---
+++ tools/release/src/executors/update-build-publish/executor.ts
@@ -0,0 +1,54 @@
+import { exec } from 'child_process'
+import { promisify } from 'util'
+import updateVersion from 'nx-release/src/executors/update-version/executor'
+import npmPublish from 'nx-release/src/executors/npm-publish/executor'
+// nx-release uses an older version of @nx/devkit
+// --> Type has to be imported from modules of nx-release to avoid version conflicts and type errors
+import { ExecutorContext } from 'nx-release/node_modules/@nx/devkit'
+import { UpdateBuildPublishExecutorOptions } from './schema'
+
+/**
+ * A custom executor that replicates the behavior of `nx-release:build-update-publish` and adds the ability to specify a custom execution target other than `build`.
+ *
+ * @param options Configuration options that can be passed to the executor (defined in schema.json)
+ * @param context Context that the executor is running in --> provided automatically by nx
+ * @returns A promise that resolves to an object with a success indicator
+ */
+export default async function updateBuildPublishExecutor(
+  options: UpdateBuildPublishExecutorOptions,
+  context: ExecutorContext
+): Promise<{ success: boolean }> {
+
+  const buildCommand = `nx ${options.buildTarget} --project ${context.projectName}`;
+
+
+  console.info(`Releasing library ${context.projectName}. Building with command:`)
+  console.info(`  ${buildCommand}`)
+  console.info()
+
+  // Updates version in package.json based on the VERSION environment variable --> default behavior of nx-release
+  await updateVersion({}, context)
+
+  // Builds the project using the specified build target (defaults to build) --> this was not supported in nx-release
+  // The specified target must be defined in the project.json file of the respective library
+  const { stdout: buildOutput, stderr } = await promisify(exec)(
+    buildCommand
+  )
+
+  if (buildOutput) {
+    console.log(buildOutput)
+  }
+
+  if(stderr) {
+    // stderr does not result in a script interruption here because @nx/angular:package seems to wrongfully output normal logs as stderr in some cases
+    // --> led to the wrongful early termination of our script
+    // Instead we rely on exceptions that are being thrown by subprocesses to interrupt the script execution
+    console.log("stderr", stderr)
+  }
+
+  // Publish the package to npm using additional configuration values from environment variables --> default behavior of nx-release
+  await npmPublish({}, context)
+
+  // We can always return success from executor, because exceptions in commands leads to script interruption
+  return { success: true }
+}
