0d735b3c4dbfe0b5c020d2c242020468a1ba1ef2
markuczy <129275100+markuczy@users.noreply.github.com>
2025-05-16 15:41:24 +0200

    fix: fix pia storybook

--- libs/portal-integration-angular/src/lib/core/storybook-theme.module.ts
+++ libs/portal-integration-angular/src/lib/core/storybook-theme.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { providePrimeNG } from 'primeng/config'
-import Aura from '@primeng/themes/aura'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { CustomPreset } from '@onecx/angular-utils'
 
 /**
   A utility module adding theme for Storybook stories
@@ -11,7 +11,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: CustomPreset,
       },
     }),
   ],
