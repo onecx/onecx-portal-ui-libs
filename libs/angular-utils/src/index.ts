e3d1d3d47de73d00f78a2466ddd7dd3afec9ed25
markuczy <129275100+markuczy@users.noreply.github.com>
2025-05-27 18:08:44 +0200

    feat: move PoratlApiConfiguration to angular-integration-interface (#753)
    * feat: move PoratlApiConfiguration to angular-integration-interface
    
    * fix: buid
    
    * refactor: move to angular-utils

--- libs/angular-utils/src/index.ts
+++ libs/angular-utils/src/index.ts
@@ -8,6 +8,7 @@ export * from './lib/utils/translation-path-factory.utils'
 export * from './lib/utils/has-permission-checker-factory'
 export * from './lib/utils/scope.utils'
 export * from './lib/utils/styles'
+export * from './lib/utils/portal-api-configuration.utils'
 
 // Model
 export * from './lib/model/injection-tokens'
