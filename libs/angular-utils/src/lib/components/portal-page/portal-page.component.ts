d27a256948a8943b242f37c1e865d5f8ef6b8480
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-06-05 12:22:57 +0200

    refactor: fix permission.service and improve translation loading

--- libs/angular-utils/src/lib/components/portal-page/portal-page.component.ts
+++ libs/angular-utils/src/lib/components/portal-page/portal-page.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, LOCALE_ID, OnInit, inject } from '@angular/core'
-import { AppStateService, UserService } from '@onecx/angular-integration-interface'
-import { HAS_PERMISSION_CHECKER, HasPermissionChecker } from '../../utils/has-permission-checker'
-import { from, Observable, shareReplay } from 'rxjs'
+import { Component, Input, OnInit, inject } from '@angular/core'
+import { AppStateService } from '@onecx/angular-integration-interface'
+import { Observable, of } from 'rxjs'
 import { CommonModule } from '@angular/common'
 import { TranslateModule } from '@ngx-translate/core'
-import { TRANSLATION_PATH } from '../../utils/create-translate-loader.utils'
+import { PermissionService } from '../../services/permission.service'
 
 @Component({
   selector: 'ocx-portal-page',
@@ -12,59 +11,19 @@ import { TRANSLATION_PATH } from '../../utils/create-translate-loader.utils'
   styleUrls: ['./portal-page.component.scss'],
   standalone: true,
   imports: [CommonModule, TranslateModule],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useFactory: (userService: UserService) => {
-        return userService.lang$.getValue()
-      },
-      deps: [UserService],
-    },
-    {
-      provide: TRANSLATION_PATH,
-      useValue: './onecx-angular-utils/assets/i18n/',
-      multi: true,
-    },
-  ],
 })
 export class PortalPageComponent implements OnInit {
   private appState = inject(AppStateService)
-  private userService = inject(UserService)
-  private hasPermissionChecker = inject<HasPermissionChecker>(HAS_PERMISSION_CHECKER, { optional: true })
+  private permissionService = inject(PermissionService)
+  private trueObservable = of(true)
 
   @Input() permission = ''
   @Input() helpArticleId = ''
   @Input() pageName = ''
   @Input() applicationId = ''
 
-  cachedHasPermissionChecker$: Observable<boolean> | undefined
-  cachedUserService$: Observable<boolean> | undefined
-
   hasAccess(): Observable<boolean> {
-    if (this.cachedHasPermissionChecker$) {
-      return this.cachedHasPermissionChecker$
-    }
-
-    if (this.cachedUserService$) {
-      return this.cachedUserService$
-    }
-
-    if (this.hasPermissionChecker) {
-      const hasPermissionChecker$ = this.permission
-        ? from(this.hasPermissionChecker.hasPermission(this.permission))
-        : from(Promise.resolve(true))
-
-      this.cachedHasPermissionChecker$ = hasPermissionChecker$.pipe(shareReplay(1))
-
-      return this.cachedHasPermissionChecker$
-    }
-
-    const userServiceHasPermission$ = this.permission
-      ? from(this.userService.hasPermission(this.permission))
-      : from(Promise.resolve(true))
-    this.cachedUserService$ = userServiceHasPermission$.pipe(shareReplay(1))
-
-    return this.cachedUserService$
+    return this.permission ? this.permissionService.hasPermission(this.permission) : this.trueObservable 
   }
 
   ngOnInit(): void {
