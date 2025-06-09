65423f6eabb982e2c1adfcabfb8afaa92349a985
markuczy <129275100+markuczy@users.noreply.github.com>
2025-06-09 15:15:50 +0200

    fix: translation path providing for angular utils

--- libs/angular-utils/src/lib/providers/angular-utils.providers.ts
+++ libs/angular-utils/src/lib/providers/angular-utils.providers.ts
@@ -1,10 +1,26 @@
-import { Provider } from '@angular/core';
-import { provideTranslationPaths } from './translation-path.providers';
-import { providePermissionService } from './permission-service.providers';
+import { Provider } from '@angular/core'
+import { providePermissionService } from './permission-service.providers'
+import { provideTranslationPathsForMfe, provideTranslationPathsForRemoteComponent } from './translation-path.providers'
 
-export function provideAngularUtils(): Provider[] {
-  return [
-    ...providePermissionService(),
-    ...provideTranslationPaths(),
-  ];
+export type ContentType = 'microfrontend' | 'remoteComponent'
+
+export interface AngularUtilsProviderOptions {
+  contentType?: ContentType
+}
+
+export function provideAngularUtils(options?: AngularUtilsProviderOptions): Provider[] {
+  const providers = [...providePermissionService()]
+  providers.push(prepareTranslationPathProviders(options?.contentType))
+  return providers
+}
+
+function prepareTranslationPathProviders(contentType?: ContentType) {
+  if (!contentType) return provideTranslationPathsForMfe()
+
+  switch (contentType) {
+    case 'microfrontend':
+      return provideTranslationPathsForMfe()
+    case 'remoteComponent':
+      return provideTranslationPathsForRemoteComponent()
+  }
 }
