23f9a9df24fb4d472ee5e0302ddf271e72eb0c7f
markuczy <129275100+markuczy@users.noreply.github.com>
2025-06-20 14:57:39 +0200

    fix: lint issues

--- libs/angular-integration-interface/src/lib/services/user.service.ts
+++ libs/angular-integration-interface/src/lib/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnDestroy } from '@angular/core'
 import { PermissionsTopic, UserProfileTopic } from '@onecx/integration-interface'
-import { BehaviorSubject, firstValueFrom, map, Observable } from 'rxjs'
+import { BehaviorSubject, firstValueFrom, map } from 'rxjs'
 import { DEFAULT_LANG } from '../api/constants'
 
 @Injectable({ providedIn: 'root' })
