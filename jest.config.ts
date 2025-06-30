06f5d6312be3d1172504b1269e88065a5ed7678f
mholzaepfel <204693613+mholzaepfel@users.noreply.github.com>
2025-06-30 11:47:43 +0200

    chore: minor change

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,6 @@ import { getJestProjects } from '@nx/jest'
 
 export default {
   projects: getJestProjects(),
-  // Remove this line once the Docker pull rate limit issue is resolved
+  //TODO: Remove this line once the Docker pull rate limit issue is resolved
   testPathIgnorePatterns: ['<rootDir>/libs/integration-tests/'],
 }
