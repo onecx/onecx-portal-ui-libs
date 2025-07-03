230341626dd7aa557a6d38af3fdb3a3501852063
Jana <jana@CE53191.corp.capgemini.com>
2025-07-03 10:48:56 +0200

    fix:lint issues

--- libs/ngrx-accelerator/migrations/test-utils/custom-matchers.ts
+++ libs/ngrx-accelerator/migrations/test-utils/custom-matchers.ts
@@ -1,25 +1,3 @@
 import { expect } from '@jest/globals'
-
-expect.extend({
-  toEqualIgnoringWhitespace(received: string, expected: string) {
-    const normalize = (str: string) => str.replace(/\s+/g, ' ').trim()
-
-    const pass = normalize(received) === normalize(expected)
-
-    return {
-      pass,
-      message: () =>
-        pass
-          ? `Expected strings not to be equal ignoring whitespace`
-          : `Expected strings to be equal ignoring whitespace.\n\nExpected:\n${expected}\n\nReceived:\n${received}`,
-    }
-  },
-})
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toEqualIgnoringWhitespace(expected: string): R
-    }
-  }
-}
+import * as matchers from 'jest-extended'
+expect.extend(matchers)
