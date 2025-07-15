ba7b6a3f8210c3e90f757680c735978974bc1645
markuczy <129275100+markuczy@users.noreply.github.com>
2025-07-16 01:07:08 +0200

    fix: remove comment

--- libs/accelerator/src/lib/utils/gatherer.spec.ts
+++ libs/accelerator/src/lib/utils/gatherer.spec.ts
@@ -1,11 +1,3 @@
-/**
- * The test environment that will be used for testing.
- * The default environment in Jest is a Node.js environment.
- * If you are building a web app, you can use a browser-like environment through jsdom instead.
- *
- * @jest-environment jsdom
- */
-
 import { Gatherer } from './gatherer'
 
 describe('Gatherer', () => {
