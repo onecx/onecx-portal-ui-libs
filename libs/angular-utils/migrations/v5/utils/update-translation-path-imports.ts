9cc127cf508a9fd55e2c9763038e19e81e58e967
Jana <jana@CE53191.corp.capgemini.com>
2025-07-25 11:00:55 +0200

    fix: migration fixes

--- libs/angular-utils/migrations/v5/utils/update-translation-path-imports.ts
+++ libs/angular-utils/migrations/v5/utils/update-translation-path-imports.ts
@@ -1,14 +1,14 @@
 import { Tree } from "@nx/devkit";
 import { removeImportSpecifierFromImport } from "@onecx/nx-migration-utils";
-import { addProviderImportInFile } from "@onecx/nx-migration-utils/angular";
+import { addProviderImportInFile } from "@onecx/nx-migration-utils";
 
 export function updateTranslationPathImports(tree: Tree, filePath: string, fileContent: string) {
     // Remove old import specifiers from @onecx/angular-utils
     const specifiers = ['TRANSLATION_PATH', 'remoteComponentTranslationPathFactory', 'translationPathFactory']
     for (const specifier of specifiers) {
         removeImportSpecifierFromImport(tree, filePath, '@onecx/angular-utils', specifier);
-        fileContent = tree.read(filePath, 'utf-8') ?? '';
     }
+    fileContent = tree.read(filePath, 'utf-8') ?? '';
 
     // Add provideTranslationPathFromMeta to imports if not present
     if (fileContent) {
