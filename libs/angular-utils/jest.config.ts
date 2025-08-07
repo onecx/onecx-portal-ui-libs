ee9258dc9c40d7ee397efaa920e3fda55b4eb0b0
mholzaepfel <204693613+mholzaepfel@users.noreply.github.com>
2025-08-07 15:01:33 +0200

    ci: improve GitHub workflow (#845)
    * ci: extend GitHub workflow
    - add SonarCloud analysis on push and PR
    - integrate CodeQL scanning
    - add dependency review
    - separate tasks: lint, build, test
    - extend jest config and add jest-conig-factory.ts
    
    * ci: add custom action setup-environment
    
    * ci: add actions folder
    
    * ci: remove testResultsProccessor
    
    * chore: add lint-diable
    
    * chore: add jest-sonar
    
    * chore: upgrade action-download-artifact to v11
    
    * chore: update nx-set-shas
    
    * chore: upgrade action-download-artifact to v11 *2
    
    * chore: replace download action
    
    * chore: rename affected tasks
    
    * chore: rename ci.yml to ci-p.yml
    
    * chore: rename jobs
    
    * chore: rename main job to ci
    
    * chore: rename jobnames for unique jobnames
    
    ---------
    
    Co-authored-by: Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>

--- libs/angular-utils/jest.config.ts
+++ libs/angular-utils/jest.config.ts
@@ -1,8 +1,10 @@
+/* eslint-disable */
+import { createReportsConfig } from '../../jest-config-factory'
+
 export default {
   displayName: 'angular-utils',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/angular-utils',
   transform: {
     '^.+\\.(mjs|js|html)$': [
       'jest-preset-angular',
@@ -36,4 +38,5 @@ export default {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  ...createReportsConfig('angular-utils'),
 }
--- libs/angular-webcomponents/jest.config.ts
+++ libs/angular-webcomponents/jest.config.ts
@@ -1,9 +1,10 @@
 /* eslint-disable */
+import { createReportsConfig } from '../../jest-config-factory'
+
 export default {
   displayName: 'angular-webcomponents',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/angular-webcomponents',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
@@ -19,4 +20,5 @@ export default {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  ...createReportsConfig('angular-webcomponents'),
 }
