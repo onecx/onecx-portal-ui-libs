71b93324b77280ce2db87dceb18b538312af8caf
markuczy <129275100+markuczy@users.noreply.github.com>
2025-09-22 13:52:55 +0200

    fix: revert accidental removal

--- libs/shell-core/src/lib/components/portal-viewport/portal-viewport.component.ts
+++ libs/shell-core/src/lib/components/portal-viewport/portal-viewport.component.ts
@@ -139,6 +139,7 @@ export class PortalViewportComponent implements OnInit, OnDestroy {
     const mobileBreakpointVar = getComputedStyle(document.documentElement).getPropertyValue('--mobile-break-point')
     const isMobile = window.matchMedia(`(max-width: ${mobileBreakpointVar})`).matches
     // auto show sidebar when changing to desktop, hide when changing to mobile
+    if (isMobile !== this.isMobile) this.menuActive = !isMobile
     this.isMobile = isMobile
   }
 
