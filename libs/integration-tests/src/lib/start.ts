fb82894c7afdd6196c3fb925a826efd848abe114
mholzaepfel <204693613+mholzaepfel@users.noreply.github.com>
2025-11-12 13:50:23 +0100

    chore: remove integration-tests library from portal-libs (#1015)
    * chore: remove integration-tests library from portal-libs
    
    * fix: package-lock.json

--- libs/integration-tests/src/lib/start.ts
+++
@@ -1,8 +0,0 @@
-import { PlatformManager } from './platform/platform-manager'
-
-async function runAllContainers() {
-  const manager = new PlatformManager()
-  await manager.startContainers()
-}
-
-runAllContainers()
