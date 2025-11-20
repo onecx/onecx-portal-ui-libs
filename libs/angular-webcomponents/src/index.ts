4eceff587440d14bbd124ef8bc0caec5c3dc47ee
markuczy <129275100+markuczy@users.noreply.github.com>
2025-11-20 12:08:03 +0100

    feat: add elementName to appId injectable (#1029)
    * feat: add elementName to appId injectable
    
    * fix: lint issues
    
    * fix: code smells

--- libs/angular-webcomponents/src/index.ts
+++ libs/angular-webcomponents/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/utils/dynamic-app-id.utils'
 export * from './lib/utils/webcomponent-bootstrap.utils'
 export * from './lib/utils/webcomponent-router-initializer.utils'
 export * from './lib/utils/webcomponent-router.utils'
