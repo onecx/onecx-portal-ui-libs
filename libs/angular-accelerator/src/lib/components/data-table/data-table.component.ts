b13912f35e5d7ba6418097665c9c884afdf7e4c5
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2025-12-09 16:38:34 +0530

    fix: Fixed the default tooltip for sort action in interactive-data-view

--- libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
+++ libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
@@ -700,7 +700,7 @@ export class DataTableComponent extends DataSortBase implements OnInit, AfterCon
 
   sortIconTitle(sortColumn: string) {
     return this.sortDirectionToTitle(
-      sortColumn !== this.sortDirection
+      sortColumn !== this.sortColumn
         ? DataSortDirection.NONE
         : this.sortStates[this.sortStates.indexOf(this.sortDirection) % this.sortStates.length]
     )
