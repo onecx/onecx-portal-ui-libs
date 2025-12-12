22909f2fcd4d151cd2e3b9331b277748c8615dc9
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2025-12-12 13:22:26 +0000

    fix: addressed review comments

--- libs/angular-accelerator/src/lib/storybook-translate.module.ts
+++ libs/angular-accelerator/src/lib/storybook-translate.module.ts
@@ -1,13 +1,34 @@
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
-import { NgModule } from '@angular/core'
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { provideAppStateServiceMock } from '@onecx/angular-integration-interface/mocks'
 import { registerLocaleData } from '@angular/common'
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import localeDE from '@angular/common/locales/de'
-import { createTranslateLoader, TRANSLATION_PATH } from '@onecx/angular-utils'
+import { NgModule, inject } from '@angular/core'
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
+import { provideAppStateServiceMock } from '@onecx/angular-integration-interface/mocks'
+import { TRANSLATION_PATH, createTranslateLoader } from '@onecx/angular-utils'
+
 /**
-  A utility module adding i18N support for Storybook stories
+ * StorybookTranslateModule
+ *
+ * Add feature-specific translation files (e.g., only pageheader keys) to libs/angular-accelerator/assets/i18n/.
+ *
+ * Reference each file in TRANSLATION_PATH using its base path (e.g., '/assets/i18n/page-header').
+ * The loader will append the language suffix and .json automatically.
+ *
+ * Example:
+ *   { provide: TRANSLATION_PATH, useValue: '/assets/i18n/page-header', multi: true }
  **/
+const STORYBOOK_TRANSLATION_PROVIDERS = [
+  {
+    provide: TRANSLATION_PATH,
+    useValue: '/assets/i18n/',
+    multi: true,
+  },
+  {
+    provide: TRANSLATION_PATH,
+    useValue: '/assets/i18n/storybook-translations/page-header/',
+    multi: true,
+  }
+]
 @NgModule({
   exports: [TranslateModule],
   imports: [
@@ -23,15 +44,12 @@ import { createTranslateLoader, TRANSLATION_PATH } from '@onecx/angular-utils'
   providers: [
     provideAppStateServiceMock(),
     provideHttpClient(withInterceptorsFromDi()),
-    {
-      provide: TRANSLATION_PATH,
-      useValue: './assets/i18n/',
-      multi: true,
-    }
+    ...STORYBOOK_TRANSLATION_PROVIDERS
   ],
 })
 export class StorybookTranslateModule {
-  constructor(translateService: TranslateService) {
+  constructor() {
+    const translateService = inject(TranslateService)
     registerLocaleData(localeDE)
     const lang = translateService.getBrowserLang()
     const supportedLanguages = ['de', 'en']
