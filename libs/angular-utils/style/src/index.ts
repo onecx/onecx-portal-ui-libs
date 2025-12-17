3a9f2e119bb913032ecf99c2f64006f7900debf9
Yuliia Melnyk <191982825+yulmelcorp@users.noreply.github.com.>
2025-12-17 08:47:25 +0100

    fix: style utils file names changes

--- libs/angular-utils/style/src/index.ts
+++ libs/angular-utils/style/src/index.ts
@@ -2,8 +2,8 @@
 export * from './api/constants'
 
 //utils
-export * from './utils/app-style-usage-management.utils'
-export * from './utils/dom-style-management.utils'
-export * from './utils/style-scope-management.utils'
+export * from './utils/app-styles-usage.utils'
+export * from './utils/dom-style.utils'
+export * from './utils/app-styles-scope.utils'
 export * from './utils/app-styles-lifecycle.utils'
 export * from './utils/fetch-app-css.util'
