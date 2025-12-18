498b3228ae7d36a6f6c450f3f2c241d5cfe30e4f
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2025-12-18 10:16:55 +0100

    feat: add stats logs

--- libs/accelerator/src/lib/topic/topic.ts
+++ libs/accelerator/src/lib/topic/topic.ts
@@ -14,6 +14,7 @@ import { TopicMessageType } from './topic-message-type'
 import { TopicPublisher } from './topic-publisher'
 import { TopicResolveMessage } from './topic-resolve-message'
 import '../declarations'
+import { increaseInstanceCount, isStatsEnabled } from '../utils/logs.utils'
 
 export class Topic<T> extends TopicPublisher<T> implements Subscribable<T> {
   protected isInitializedPromise: Promise<void>
@@ -25,6 +26,10 @@ export class Topic<T> extends TopicPublisher<T> implements Subscribable<T> {
 
   constructor(name: string, version: number, sendGetMessage = true) {
     super(name, version)
+
+    if (isStatsEnabled()) {
+      increaseInstanceCount(this.name)
+    }
 
     this.isInitializedPromise = new Promise<void>((resolve) => {
       this.resolveInitPromise = resolve
