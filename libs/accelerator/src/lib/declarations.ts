156d98a02faaef99f9c5ef7d067a99460668bda0
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2025-12-18 10:53:47 +0100

    fix: declarations

--- libs/accelerator/src/lib/declarations.ts
+++ libs/accelerator/src/lib/declarations.ts
@@ -1,10 +1,6 @@
 declare global {
   interface Window {
     '@onecx/accelerator': {
-      gatherer?: {
-        debug?: string[]
-        promises?: { [id: number]: Promise<unknown>[] }
-      }
       topic?: {
         debug?: string[]
         useBroadcastChannel?: boolean
@@ -15,9 +11,6 @@ declare global {
 }
 
 window['@onecx/accelerator'] ??= {}
-window['@onecx/accelerator'].gatherer ??= {}
-window['@onecx/accelerator'].gatherer.promises ??= {}
-window['@onecx/accelerator'].gatherer.debug ??= []
 window['@onecx/accelerator'].topic ??= {}
 window['@onecx/accelerator'].topic.useBroadcastChannel ??= true
 window['@onecx/accelerator'].topic.initDate ??= Date.now()
