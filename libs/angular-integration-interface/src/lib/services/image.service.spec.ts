cab88290c733f1626d8b04281bdf0352b319d272
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2025-12-24 12:58:06 +0000

    chore: fixed test

--- libs/angular-integration-interface/src/lib/services/image.service.spec.ts
+++ libs/angular-integration-interface/src/lib/services/image.service.spec.ts
@@ -19,23 +19,23 @@ describe('ImageService', () => {
 	const mockTopic = new FakeTopic<ImageInfo>();
     service = TestBed.inject(ImageService);
     imageInterface = (service as any).imageInterface;
-	imageInterface.imageTopic = mockTopic as any as ImageTopic;
-	imageInterface.imageTopic?.publish(MOCK_URLS);
+    imageInterface.imageTopic = mockTopic as any as ImageTopic;
+    imageInterface.imageTopic?.publish(MOCK_URLS);
   });
 
   it('should call getUrl without fallback', async () => {
-	const expectedUrl = MOCK_URLS.images[URL_NAME];
-	const spyGetUrl = jest.spyOn(imageInterface, 'getUrl').mockResolvedValue(expectedUrl);
+    const expectedUrl = MOCK_URLS.images[URL_NAME];
+    const spyGetUrl = jest.spyOn(imageInterface, 'getUrl').mockResolvedValue(expectedUrl);
 
     const result = await service.getUrl([URL_NAME]);
 
     expect(result).toBe(expectedUrl);
-	expect(spyGetUrl).toHaveBeenCalledWith([URL_NAME]);
-	expect(result).toEqual(EXPECTED_URL);
+	  expect(spyGetUrl).toHaveBeenCalledWith([URL_NAME]);
+	  expect(result).toEqual(EXPECTED_URL);
   });
 
   it('should call getUrl with fallback', async () => {
-	const NOT_FOUND_NAME = 'notfound';
+	  const NOT_FOUND_NAME = 'notfound';
     const spyGetUrl = jest.spyOn(imageInterface, 'getUrl').mockResolvedValue(FALLBACK_URL);
 
     const result = await service.getUrl([NOT_FOUND_NAME], FALLBACK_URL);
@@ -63,7 +63,7 @@ describe('ImageService', () => {
   it('should test topic getter/setter and isInitialized getter', async () => {    
     const mockIsInitialized = Promise.resolve();
     jest.spyOn(imageInterface, 'isInitialized', 'get').mockReturnValue(mockIsInitialized);
-    
+
     service.imageTopic = imageInterface.imageTopic;
 
     expect(service.imageTopic).toBe(imageInterface.imageTopic);
