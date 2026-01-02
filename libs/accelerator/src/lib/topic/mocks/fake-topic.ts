4664ec3919e4a7444ed90a92eca31d0e02a5c9f3
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2026-01-02 07:53:15 +0000

    refactor: make FakeTopic compatible with Topic

--- libs/accelerator/src/lib/topic/mocks/fake-topic.ts
+++ libs/accelerator/src/lib/topic/mocks/fake-topic.ts
@@ -1,31 +1,13 @@
 import { Subject, Observable, Observer, Subscription, BehaviorSubject, ReplaySubject, OperatorFunction } from 'rxjs'
-import { TopicMessage } from '../topic-message'
+import { Topic } from '../topic'
+import { TopicDataMessage } from '../topic-data-message'
 
-class MockTopicPublisher<T> {
-  public name: string
-  public version: number
-  protected publishPromiseResolver: Record<number, () => void> = {}
-  protected publishBroadcastChannel: BroadcastChannel | undefined
-  constructor(name: string, version: number) {
-    this.name = name
-    this.version = version
-  }
-  public publish(_value: T): Promise<void> {
-    return Promise.resolve()
-  }
-  protected createBroadcastChannel(): void {
-    // No-op for mock
-  }
-  protected sendMessage(_message: any): void {
-    // No-op for mock
-  }
-}
 
 interface MockSubscribable<T> {
   subscribe(observer: Partial<Observer<T>>): Subscription;
 }
 
-export class FakeTopic<T> extends MockTopicPublisher<T> implements MockSubscribable<T> {
+export class FakeTopic<T> extends Topic<T> implements MockSubscribable<T> {
     /**
      * Returns the current value if the state is a BehaviorSubject, else throws an error.
      */
@@ -36,22 +18,19 @@ export class FakeTopic<T> extends MockTopicPublisher<T> implements MockSubscriba
       throw new Error('Only possible for FakeTopic with initial value');
     }
   private state: Subject<T>
-  private resolveInitPromise!: (value: void | PromiseLike<void>) => void
-  private readonly windowEventListener = (m: MessageEvent<TopicMessage>) => this.onWindowMessage(m)
-
-  protected isInit = false
-  protected data: BehaviorSubject<T | undefined>
-  protected isInitializedPromise: Promise<void>
-  protected readonly readBroadcastChannel: BroadcastChannel | undefined = new BroadcastChannel(`Topic-${this.name}|${this.version}`)
+  protected override isInit = false
+  protected override data: BehaviorSubject<TopicDataMessage<T> | undefined>
+  protected override readonly readBroadcastChannel: BroadcastChannel | undefined =
+    typeof BroadcastChannel === 'undefined' ? undefined : new BroadcastChannel(`Topic-${this.name}|${this.version}`)
   
   constructor();
   constructor(initialValue: T);
   constructor(name: string, version: number);
   constructor(name: string, version: number, ...args: any[]);
   constructor(arg1?: string | T, arg2?: number, ...args: any[]) {
-    let name: string = 'mock';
-    let version: number = 0;
-    let initialValue: T | undefined = undefined;
+    let name = 'mock'
+    let version = 0
+    let initialValue: T | undefined = undefined
 
     if (typeof arg1 === 'string' && typeof arg2 === 'number') {
       name = arg1;
@@ -67,101 +46,85 @@ export class FakeTopic<T> extends MockTopicPublisher<T> implements MockSubscriba
     super(name, version);
     if (initialValue !== undefined) {
       this.state = new BehaviorSubject<T>(initialValue);
-      this.data = new BehaviorSubject<T | undefined>(initialValue);
+      this.data = new BehaviorSubject<TopicDataMessage<T> | undefined>({ data: initialValue } as TopicDataMessage<T>);
       this.isInit = true;
     } else {
       this.state = new ReplaySubject<T>(1);
-      this.data = new BehaviorSubject<T | undefined>(undefined);
+      this.data = new BehaviorSubject<TopicDataMessage<T> | undefined>(undefined);
     }
-    this.isInitializedPromise = new Promise<void>((resolve) => {
-      this.resolveInitPromise = resolve;
-    });
   }
 
-  private onBroadcastChannelMessage(_m: MessageEvent<TopicMessage>): any {
-    // No-op for mock
-  }
-
-  protected disableBroadcastChannel(): void {
-    // No-op for mock
-  }
-
-  protected isLogEnabled(): boolean {
-    return false;
-  }
-
-  protected handleTopicResolveMessage(_m: any): void {
-    // No-op for mock
-  }
-
-  protected handleTopicGetMessage(_m: any): void {
-    // No-op for mock
-  }
-
-  protected handleTopicNextMessage(_m: any): void {
-    // No-op for mock
-  }
-
-  get isInitialized(): Promise<void> {
+  override get isInitialized(): Promise<void> {
     return this.isInitializedPromise
   }
 
-  asObservable(): Observable<T> {
+  override asObservable(): Observable<T> {
     return this.state.asObservable()
   }
 
-  subscribe(
-    observerOrNext?: Partial<Observer<T>> | ((value: T) => void),
-    error?: (error: any) => void,
-    complete?: () => void
-  ): Subscription {
-    return (<any>this.asObservable()).subscribe(observerOrNext, error, complete)
+  public override publish(value: T): Promise<void> {
+    try {
+      this.state.next(value)
+    } catch (e) {
+      console.error('Error publishing to FakeTopic', e)
+    }
+    return Promise.resolve()
   }
 
-  pipe(...operations: any[]): any {
+  override subscribe(observerOrNext?: Partial<Observer<T>> | ((value: T) => void)): Subscription;
+  override subscribe(
+    next?: ((value: T) => void) | null,
+    error?: ((error: any) => void) | null,
+    complete?: (() => void) | null
+  ): Subscription;
+  override subscribe(
+    observerOrNext?: Partial<Observer<T>> | ((value: T) => void) | null,
+    error?: ((error: any) => void) | null,
+    complete?: (() => void) | null
+  ): Subscription {
+    return (this.asObservable() as any).subscribe(observerOrNext as any, error as any, complete as any);
+  }
+
+  override pipe(...operations: any[]): any {
     return (this.asObservable() as any).pipe(...operations)
   }
 
   /**
    * @deprecated source is deprecated in rxjs. This is only here to be compatible with the interface.
    */
-  get source() {
+  override get source() {
     return (this.state as any).source
   }
 
   /**
    * @deprecated operator is deprecated in rxjs. This is only here to be compatible with the interface.
    */
-  get operator() {
+  override get operator() {
     return (this.state as any).operator
   }
 
   /**
    * @deprecated lift is deprecated in rxjs. This is only here to be compatible with the interface.
    */
-  lift<R>(operator: OperatorFunction<T, R>): Observable<R> {
+  override lift<R>(operator: OperatorFunction<T, R>): Observable<R> {
     return (this.state as any).lift(operator)
   }
 
   /**
    * @deprecated foreach is deprecated in rxjs. This is only here to be compatible with the interface.
    */
-  forEach(next: (value: T) => void, thisArg?: any): Promise<void> {
+  override forEach(next: (value: T) => void, thisArg?: any): Promise<void> {
     return (this.state as any).forEach(next, thisArg)
   }
 
   /**
    * @deprecated toPromise is deprecated in rxjs. This is only here to be compatible with the interface.
    */
-  toPromise(): Promise<T | undefined> {
+  override toPromise(): Promise<T | undefined> {
     return (this.state as any).toPromise()
   }
 
-  onWindowMessage(_messageEvent: MessageEvent<TopicMessage>): void {
-    // No-op for mock
-  }
-
-  destroy(): void {
+  override destroy(): void {
     this.state.complete()
   }  
 }
