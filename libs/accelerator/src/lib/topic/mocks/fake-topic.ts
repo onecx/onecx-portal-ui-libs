0bdb9e2e3c88d804d6ecaa2a17cf3366f1d6990b
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2026-01-08 14:22:09 +0530

    chore: Improve FakeTopic to not cast it in tests (#1121)
    * chore: create fakeTopic and update mock of public methods
    
    * chore: exclude mocks from sonar coverage
    
    * chore: addressed review comments
    
    * chore: skipping coverage for mocks
    
    * chore: revert property
    
    ---------
    
    Co-authored-by: markuczy <129275100+markuczy@users.noreply.github.com>

--- libs/accelerator/src/lib/topic/mocks/fake-topic.ts
+++ libs/accelerator/src/lib/topic/mocks/fake-topic.ts
@@ -1,4 +1,5 @@
 import { Subject, Observable, Observer, Subscription, UnaryFunction, BehaviorSubject, ReplaySubject } from 'rxjs'
+import { Topic } from '../topic'
 
 export class FakeTopic<T> {
   private state: Subject<T>
@@ -9,20 +10,67 @@ export class FakeTopic<T> {
       this.state = new ReplaySubject<T>(1)
     }
   }
-  asObservable(): Observable<T> {
-    return this.state.asObservable()
+  get isInitialized(): Promise<void> {
+    return Promise.resolve()
   }
-
+  
+  subscribe(observerOrNext?: Partial<Observer<T>> | ((value: T) => void)): Subscription
+  /** @deprecated is deprecated in rxjs. This is only here to be compatible with the interface. */
   subscribe(
-    observerOrNext?: Partial<Observer<T>> | ((value: T) => void),
-    error?: (error: any) => void,
-    complete?: () => void
+    next?: ((value: T) => void) | null,
+    error?: ((error: any) => void) | null,
+    complete?: (() => void) | null
+  ): Subscription
+  subscribe(
+    observerOrNext?: Partial<Observer<T>> | ((value: T) => void) | null,
+    error?: (error: any) => void | null,
+    complete?: () => void | null
   ): Subscription {
     return (<any>this.asObservable()).subscribe(observerOrNext, error, complete)
   }
 
-  pipe(...operations: UnaryFunction<any, any>[]): unknown {
-    return (<any>this.asObservable()).pipe(...operations)
+  /**
+   * @deprecated source is deprecated in rxjs. This is only here to be compatible with the interface.
+   */
+  get source() {
+    return (this.asObservable() as any).source
+  }
+
+  /**
+   * @deprecated operator is deprecated in rxjs. This is only here to be compatible with the interface.
+   */
+  get operator() {
+    return (this.asObservable() as any).operator
+  }
+
+  /**
+   * @deprecated lift is deprecated in rxjs. This is only here to be compatible with the interface.
+   */
+  lift<R>(operator: UnaryFunction<T, R>): Observable<R> {
+    return (this.asObservable() as any).lift(operator)
+  }
+
+  /**
+   * @deprecated foreach is deprecated in rxjs. This is only here to be compatible with the interface.
+   */
+  forEach(next: (value: T) => void, thisArg?: any): Promise<void> {
+    return this.asObservable().forEach(next, thisArg)
+  }
+
+  /**
+   * @deprecated toPromise is deprecated in rxjs. This is only here to be compatible with the interface.
+   */
+  toPromise(): Promise<T | undefined> {
+    return this.asObservable().toPromise()
+  }
+
+  // The following are already present: publish, destroy, getValue
+  asObservable(): Observable<T> {
+    return this.state.asObservable()
+  }
+
+  pipe<R = Observable<T>>(...operations: UnaryFunction<any, any>[]): R {
+    return (this.asObservable() as any).pipe(...operations)
   }
 
   publish(value: T): Promise<void> {
@@ -39,5 +87,9 @@ export class FakeTopic<T> {
       return this.state.getValue()
     }
     throw new Error('Only possible for FakeTopic with initial value')
+  }
+
+  static create<T>(initialValue: T | undefined = undefined): Topic<T> {
+    return new FakeTopic<T>(initialValue) as unknown as Topic<T>
   }
 }
