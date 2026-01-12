3516463067069ba471e8a65786fc297b39e60e68
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2026-01-12 19:10:36 +0530

    fix: Imporve Token Interceptor Timming

--- libs/angular-auth/src/lib/token.interceptor.ts
+++ libs/angular-auth/src/lib/token.interceptor.ts
@@ -2,12 +2,14 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable, inject } from '@angular/core'
 import { Observable, from, mergeMap } from 'rxjs'
 import { AuthProxyService } from './auth-proxy.service'
+import { AppStateService } from '@onecx/angular-integration-interface'
 
 const WHITELIST = ['assets']
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   private authService = inject(AuthProxyService)
+  private appStateService = inject(AppStateService)
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const skip = WHITELIST.some((str) => request.url.includes(str))
@@ -15,7 +17,8 @@ export class TokenInterceptor implements HttpInterceptor {
       return next.handle(request)
     }
 
-    return from(this.authService.updateTokenIfNeeded()).pipe(
+    return from(this.appStateService.isAuthenticated$.isInitialized).pipe(
+      mergeMap(() => from(this.authService.updateTokenIfNeeded())),
       mergeMap(() => {
         const headerValues = this.authService.getHeaderValues()
         let headers = request.headers
