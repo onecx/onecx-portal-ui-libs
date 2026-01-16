3ce939b99df3ac36ca6ecf3ab12796ccf947f99b
Jan-Gerrit Schettler-Koehler <jan-gerrit.schettler-koehler@capgemini.com>
2026-01-16 08:41:38 +0100

    chore: tests for BasicDirective

---
+++ libs/angular-accelerator/src/lib/directives/basic.directive.spec.ts
@@ -0,0 +1,160 @@
+import { Component, ViewChild } from '@angular/core'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed'
+import { ComponentHarness, HarnessLoader } from '@angular/cdk/testing'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { provideTranslateTestingService } from '@onecx/angular-testing'
+// eslint-disable-next-line deprecation/deprecation
+import { RouterTestingModule } from '@angular/router/testing'
+
+import { BasicDirective } from './basic.directive'
+import { SearchHeaderComponent } from '../components/search-header/search-header.component'
+import { PageHeaderComponent } from '../components/page-header/page-header.component'
+import { AngularAcceleratorModule } from '../angular-accelerator.module'
+import { SearchHeaderHarness } from '../../../testing/search-header.harness'
+
+class BasicContentHarness extends ComponentHarness {
+  static readonly hostSelector = '#basic-content'
+
+  async text(): Promise<string> {
+    return (await this.host()).text()
+  }
+
+  async id(): Promise<string | null> {
+    return (await this.host()).getAttribute('id')
+  }
+}
+
+class HostInsideSearchHeaderHarness extends ComponentHarness {
+  static readonly hostSelector = 'ocx-search-header'
+
+  private readonly basicContentLocator = this.locatorForOptional(BasicContentHarness)
+  private readonly allBasicContentLocator = this.locatorForAll(BasicContentHarness)
+
+  async getBasicContent(): Promise<BasicContentHarness | null> {
+    return this.basicContentLocator()
+  }
+
+  async getBasicContentCount(): Promise<number> {
+    return (await this.allBasicContentLocator()).length
+  }
+}
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
+  standalone: false,
+  template: `
+    <ocx-search-header>
+      <ng-template ocxBasic>
+        <div id="basic-content">Basic Content</div>
+      </ng-template>
+    </ocx-search-header>
+  `,
+})
+class HostInsideSearchHeaderComponent {
+  @ViewChild(SearchHeaderComponent)
+  searchHeader!: SearchHeaderComponent
+
+  @ViewChild(BasicDirective)
+  directive!: BasicDirective
+}
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
+  standalone: false,
+  template: `
+    <ng-template ocxBasic>
+      <div id="basic-content">Basic Content</div>
+    </ng-template>
+  `,
+})
+class HostOutsideSearchHeaderComponent {
+  @ViewChild(BasicDirective)
+  directive!: BasicDirective
+}
+
+describe('BasicDirective', () => {
+  let loader: HarnessLoader
+
+  describe('when used outside SearchHeaderComponent', () => {
+    it('should throw an error during component creation', async () => {
+      await TestBed.configureTestingModule({
+        declarations: [HostOutsideSearchHeaderComponent, BasicDirective],
+      }).compileComponents()
+
+      expect(() => {
+        const fixture = TestBed.createComponent(HostOutsideSearchHeaderComponent)
+        fixture.detectChanges()
+      }).toThrow('Basic directive can only be used inside search header component')
+    })
+  })
+
+  describe('when used inside SearchHeaderComponent', () => {
+    let fixture: ComponentFixture<HostInsideSearchHeaderComponent>
+    let component: HostInsideSearchHeaderComponent
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [HostInsideSearchHeaderComponent, SearchHeaderComponent, PageHeaderComponent, BasicDirective],
+        // eslint-disable-next-line deprecation/deprecation
+        imports: [RouterTestingModule, AngularAcceleratorModule],
+        providers: [
+          provideHttpClient(withInterceptorsFromDi()),
+          provideHttpClientTesting(),
+          provideTranslateTestingService({}),
+        ],
+      }).compileComponents()
+
+      fixture = TestBed.createComponent(HostInsideSearchHeaderComponent)
+      component = fixture.componentInstance
+      fixture.detectChanges()
+
+      loader = TestbedHarnessEnvironment.loader(fixture)
+    })
+
+    it('should render basic template when viewMode is basic', async () => {
+      component.searchHeader.viewMode = 'basic'
+      fixture.detectChanges()
+
+      const hostHarness = await loader.getHarness(HostInsideSearchHeaderHarness)
+      const contentHarness = await hostHarness.getBasicContent()
+      expect(contentHarness).not.toBeNull()
+      expect(await contentHarness!.id()).toBe('basic-content')
+      expect(await contentHarness!.text()).toContain('Basic Content')
+
+      // Harness smoke: search header exists.
+      expect(await loader.getHarness(SearchHeaderHarness)).toBeTruthy()
+    })
+
+    it('should not render basic template when viewMode is advanced', async () => {
+      component.searchHeader.viewMode = 'advanced'
+      fixture.detectChanges()
+
+      const hostHarness = await loader.getHarness(HostInsideSearchHeaderHarness)
+      expect(await hostHarness.getBasicContentCount()).toBe(0)
+    })
+
+    it('should clear basic template when toggling from basic to advanced', async () => {
+      component.searchHeader.viewMode = 'basic'
+      fixture.detectChanges()
+      const hostHarness = await loader.getHarness(HostInsideSearchHeaderHarness)
+      expect(await hostHarness.getBasicContentCount()).toBe(1)
+
+      component.searchHeader.viewMode = 'advanced'
+      fixture.detectChanges()
+      expect(await hostHarness.getBasicContentCount()).toBe(0)
+    })
+
+    it('should not create a second embedded view when change detection runs again in basic mode', async () => {
+      component.searchHeader.viewMode = 'basic'
+      fixture.detectChanges()
+
+      fixture.detectChanges()
+      fixture.detectChanges()
+
+      const hostHarness = await loader.getHarness(HostInsideSearchHeaderHarness)
+      expect(await hostHarness.getBasicContentCount()).toBe(1)
+    })
+  })
+})
