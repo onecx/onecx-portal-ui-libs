902297a4b94543f3d9e41238776fc1f7d33208e8
Jan-Gerrit Schettler-Koehler <jan-gerrit.schettler-koehler@capgemini.com>
2026-01-26 10:24:44 +0100

    feat: prefer-translate-params rule

--- libs/angular-linter-rules/src/lib/rules/index.ts
+++ libs/angular-linter-rules/src/lib/rules/index.ts
@@ -1,7 +1,9 @@
 import { noSubscribeAssignment } from './no-subscribe-assignment.rule'
 import { noTranslateInstant } from './no-translate-instant.rule'
+import { preferTranslateParams } from './prefer-translate-params.rule'
 
 export const rules = {
   'no-subscribe-assignment': noSubscribeAssignment,
   'no-translate-instant': noTranslateInstant,
+  'prefer-translate-params': preferTranslateParams,
 } as const
