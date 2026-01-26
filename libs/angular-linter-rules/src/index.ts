b63a5b84b301dee5481a45091df3f32d76dda8ab
Jan-Gerrit Schettler-Koehler <jan-gerrit.schettler-koehler@capgemini.com>
2026-01-26 09:29:04 +0100

    feat: no-translate-instant rule

--- libs/angular-linter-rules/src/index.ts
+++ libs/angular-linter-rules/src/index.ts
@@ -1,9 +1,12 @@
-export { rules } from './lib/rules'
-export { configs } from './lib/configs'
+import { rules } from './lib/rules'
+import { configs } from './lib/configs'
+
+export { rules, configs }
 
 export const plugin = {
-	rules,
-	configs,
+
+  rules,
+  configs,
 }
 
 export type { AngularLinterRulesPlugin } from './lib/types'
--- libs/angular-linter-rules/src/lib/rules/index.ts
+++ libs/angular-linter-rules/src/lib/rules/index.ts
@@ -1,5 +1,7 @@
 import { noSubscribeAssignment } from './no-subscribe-assignment.rule'
+import { noTranslateInstant } from './no-translate-instant.rule'
 
 export const rules = {
   'no-subscribe-assignment': noSubscribeAssignment,
+  'no-translate-instant': noTranslateInstant,
 } as const
