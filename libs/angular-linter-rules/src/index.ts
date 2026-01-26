f6ea64af546f46e175a48f1ad7f7a0132ea44890
Jan-Gerrit Schettler-Koehler <jan-gerrit.schettler-koehler@capgemini.com>
2026-01-26 09:11:28 +0100

    feat: no-subscribe-assignment rule

---
+++ libs/angular-linter-rules/src/index.ts
@@ -0,0 +1,9 @@
+export { rules } from './lib/rules'
+export { configs } from './lib/configs'
+
+export const plugin = {
+	rules,
+	configs,
+}
+
+export type { AngularLinterRulesPlugin } from './lib/types'
---
+++ libs/angular-linter-rules/src/lib/rules/index.ts
@@ -0,0 +1,5 @@
+import { noSubscribeAssignment } from './no-subscribe-assignment.rule'
+
+export const rules = {
+  'no-subscribe-assignment': noSubscribeAssignment,
+} as const
