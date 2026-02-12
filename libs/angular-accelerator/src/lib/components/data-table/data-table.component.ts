5a6a4cccce7bd32e5e624555cee52a4d549cea1b
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2026-02-12 10:58:12 +0530

    fix: filter reset updated

--- libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
+++ libs/angular-accelerator/src/lib/components/data-table/data-table.component.ts
@@ -130,7 +130,7 @@ export class DataTableComponent extends DataSortBase implements OnInit, AfterCon
     return this._filters$.getValue()
   }
   set filters(value: Filter[]) {
-    if (this._filters$.getValue().length > value.length ) {
+    if (this._filters$.getValue().length) {
       this.resetPage();
     }
     this._filters$.next(value)
