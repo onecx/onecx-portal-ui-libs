1c7acb7b98c04d99834cf36cf202cef358d2eb13
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2026-03-02 15:03:13 +0100

    feat: first version of getShareScope

--- libs/angular-remote-components/src/lib/utils/get-share-scope.ts
+++ libs/angular-remote-components/src/lib/utils/get-share-scope.ts
@@ -1,15 +1,37 @@
+import { inject } from '@angular/core'
+import { Manifest } from '../model/manifest'
 import { createLogger } from './logger.utils'
-// TODO: Type manifest properly and ensure that default is used when no version can be determined or when version is below 21
-export function getShareScope(manifest: any): string {
+import { HttpClient } from '@angular/common/http'
+import { firstValueFrom } from 'rxjs'
+
+export async function getShareScope(manifestUrl: string): Promise<string> {
+  const http = inject(HttpClient)
   const logger = createLogger('getShareScope')
+  let manifest: Manifest
+
+  try {
+    manifest = await firstValueFrom(http.get<Manifest>(manifestUrl))
+  } catch (error) {
+    logger.warn(
+      'Failed to load manifest from URL:',
+      manifestUrl,
+      'Falling back to default share scope.',
+      'Error:',
+      error
+    )
+    return 'default'
+  }
+
   const angularCore = manifest.shared.find((s: any) => s.name === '@angular/core')
+
   if (!angularCore) {
     logger.warn('Could not determine Angular version from manifest. Using default share scope.')
     return 'default'
   }
 
-  const version = angularCore.version.split('.')[0]
-  if (!version || version === '18' || version === '19') {
+  const version = parseInt(angularCore.version.split('.')[0], 10)
+  
+  if (isNaN(version) || version < 21) {
     logger.warn('Using default share scope for Angular version:', version)
     return 'default'
   }
