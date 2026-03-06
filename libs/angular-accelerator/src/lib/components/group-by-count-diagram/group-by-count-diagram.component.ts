ecc48a7091d1ae1f1d546c84dc4c0c581d40205e
priyankaSandeepRade <priyanka.sandeep-rade@capgemini.com>
2026-03-06 15:39:52 +0530

    fix: code smells

--- libs/angular-accelerator/src/lib/components/group-by-count-diagram/group-by-count-diagram.component.ts
+++ libs/angular-accelerator/src/lib/components/group-by-count-diagram/group-by-count-diagram.component.ts
@@ -18,7 +18,7 @@ export interface GroupByCountDiagramComponentState {
 })
 export class GroupByCountDiagramComponent implements OnInit {
   private translateService = inject(TranslateService)
-  private _data$ = new BehaviorSubject<unknown[]>([])
+  private readonly _data$ = new BehaviorSubject<unknown[]>([])
   @Input() sumKey = 'SEARCH.SUMMARY_TITLE'
   @Input() diagramType = DiagramType.PIE
   @Input() fullHeight = false
@@ -59,7 +59,7 @@ export class GroupByCountDiagramComponent implements OnInit {
   }
   diagramData$: Observable<DiagramData[]> | undefined
 
-  private _columnType$ = new BehaviorSubject<ColumnType>(ColumnType.STRING)
+  private readonly _columnType$ = new BehaviorSubject<ColumnType>(ColumnType.STRING)
   @Input()
   get columnType(): ColumnType {
     return this._columnType$.getValue()
@@ -68,7 +68,7 @@ export class GroupByCountDiagramComponent implements OnInit {
     this._columnType$.next(value)
   }
 
-  private _columnField$ = new BehaviorSubject<string>('')
+  private readonly _columnField$ = new BehaviorSubject<string>('')
   @Input()
   get columnField(): string {
     return this._columnField$.getValue()
@@ -86,7 +86,7 @@ export class GroupByCountDiagramComponent implements OnInit {
     this.columnField = value.id
   }
 
-  private _colors$ = new BehaviorSubject<Record<string, string>>({})
+  private readonly _colors$ = new BehaviorSubject<Record<string, string>>({})
   @Input()
   get colors(): Record<string, string> {
     return this._colors$.getValue()
