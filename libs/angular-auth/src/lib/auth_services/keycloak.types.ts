46e1841e846d4d1e46850d9dfb6afcf7b7c85361
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2026-03-10 13:59:51 +0100

    refactor: remove keycloak dep

---
+++ libs/angular-auth/src/lib/auth_services/keycloak.types.ts
@@ -0,0 +1,58 @@
+export interface KeycloakConfig {
+	/**
+	 * URL to the Keycloak server, for example: http://keycloak-server/auth
+	 */
+	url?: string;
+	/**
+	 * Name of the realm, for example: 'myrealm'
+	 */
+	realm: string;
+	/**
+	 * Client identifier, example: 'myapp'
+	 */
+	clientId: string;
+}
+
+export interface KeycloakInitOptions {
+  onLoad?: 'check-sso' | 'login-required'
+  checkLoginIframe?: boolean
+  silentCheckSsoRedirectUri?: string
+  idToken?: string
+  refreshToken?: string
+  token?: string
+}
+
+export interface KeycloakLoginOptions {
+  [key: string]: unknown
+}
+
+export interface KeycloakLogoutOptions {
+  redirectUri?: string
+  logoutMethod?: 'GET' | 'POST'
+}
+
+export interface KeycloakError {
+  error: string
+  error_description: string
+}
+
+export type KeycloakActionStatus = 'success' | 'cancelled' | 'error'
+
+export interface KeycloakInstance {
+  authenticated?: boolean
+  token?: string
+  idToken?: string
+  refreshToken?: string
+  onAuthError?: (errorData: KeycloakError) => void
+  onAuthLogout?: () => void
+  onAuthRefreshSuccess?: () => void
+  onAuthRefreshError?: () => void
+  onAuthSuccess?: () => void
+  onTokenExpired?: () => void
+  onActionUpdate?: (status: KeycloakActionStatus) => void
+  onReady?: (authenticated?: boolean) => void
+  init(initOptions: KeycloakInitOptions): Promise<boolean>
+  login(options?: KeycloakLoginOptions): Promise<void>
+  logout(options?: KeycloakLogoutOptions): Promise<void>
+  updateToken(minValidity?: number): Promise<boolean>
+}
