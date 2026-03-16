1433571260d48bf46256f1d021ce6c8379b05707
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2026-03-16 10:22:00 +0530

    fix: chart overflow hidden issue v7 (#1266)
    * fix: add height related changes in v7
    
    * fix: add missing attribute
    
    * fix: code smells

--- libs/angular-accelerator/src/lib/components/group-by-count-diagram/group-by-count-diagram.component.stories.ts
+++ libs/angular-accelerator/src/lib/components/group-by-count-diagram/group-by-count-diagram.component.stories.ts
@@ -17,6 +17,28 @@ import { ColumnType } from '../../model/column-type.model'
 import { StorybookThemeModule } from '../../storybook-theme.module'
 import { TooltipModule } from 'primeng/tooltip';
 
+const FRUIT_TYPE: string[] = []
+
+function generateGroupByCountMockData(fruitCount = 10, itemsPerFruit = 2) {
+  const fruits = Array.from({ length: fruitCount }, (_, i) => `Fruit ${i + 1}`)
+
+  let id = 1
+  const data: { id: number; fruitType: string; name: string }[] = []
+  const fruitNames = fruits.slice(0, fruitCount)
+
+  fruitNames.forEach((fruit, index) => {
+    for (let i = 0; i < itemsPerFruit; i++) {
+      data.push({
+        id: id++,
+        fruitType: `chart.fruit${index + 1}`,
+        name: `${fruit}${i + 1}`,
+      })
+    }
+  })
+
+  return data
+}
+
 export default {
   title: 'Components/GroupByCountDiagramComponent',
   component: GroupByCountDiagramComponent,
@@ -79,9 +101,37 @@ const mockData = [
     id: 5,
     fruitType: 'Banana',
     name: 'Banana2',
-  },
+  }
 ]
 
+
+const mockDataWithTranslationKeys = [
+  {
+    id: 1,
+    fruitType: 'chart.fruit1',
+    name: 'Apple1',
+  },
+  {
+    id: 2,
+    fruitType: 'chart.fruit1',
+    name: 'Apple2',
+  },
+  {
+    id: 3,
+    fruitType: 'chart.fruit1',
+    name: 'Apple3',
+  },
+  {
+    id: 4,
+    fruitType: 'chart.fruit2',
+    name: 'Banana1',
+  },
+  {
+    id: 5,
+    fruitType: 'chart.fruit2',
+    name: 'Banana2',
+  }
+]
 export const PieChart = {
   render: Template,
 
@@ -172,4 +222,73 @@ export const WithForcedCustomColors = {
     },
     fillMissingColors: true,
   },
+}
+
+export const withAllLabels = {
+  render: Template,
+  args: {
+    diagramType: DiagramType.VERTICAL_BAR,
+    data: mockData,
+    sumKey: 'With all Labels',
+    column: {
+      id: 'fruitType',
+      type: ColumnType.STRING,
+    },
+    fillMissingColors: false,
+    showAllLabels: false,
+    columnType: ColumnType.TRANSLATION_KEY,
+    allLabels: ['Apple', 'Banana', 'Orange']
+  },
+}
+
+export const withAllLabelAndTranslationKeys = {
+  render: Template,
+  args: {
+    diagramType: DiagramType.VERTICAL_BAR,
+    data: mockDataWithTranslationKeys,
+    sumKey: 'With all labels and translation keys',
+    column: {
+      id: 'fruitType',
+      columnType: ColumnType.TRANSLATION_KEY,
+    },
+    fillMissingColors: false,
+    showAllLabels: true,
+    allLabelKeys: ['chart.fruit1', 'chart.fruit2', 'chart.fruit3']
+  }
+}
+
+const TemplateWithContainer: StoryFn<GroupByCountDiagramComponent> = (args) => ({
+  template: `
+    <div class="flex justify-content-center">
+    <div style="height: 450px; width:350px;"> <!--Container should have fixed height-->
+      <ocx-group-by-count-diagram
+        [diagramType]="diagramType"
+        [data]="data"
+        [sumKey]="sumKey"
+        [column]="column"
+        [fillMissingColors]="fillMissingColors"
+        [showAllLabels]="showAllLabels"
+        [allLabelKeys]="allLabelKeys"
+        [fullHeight]="true"     
+      ></ocx-group-by-count-diagram>
+      </div>
+    </div>
+  `,
+  props: args,
+})
+
+export const WithChartFillingContainerHeight = {
+  render: TemplateWithContainer,
+  args: {
+    diagramType: DiagramType.PIE,
+    data: generateGroupByCountMockData(20, 1),
+    sumKey: 'Responsive Height Enabled',
+    column: {
+      id: 'fruitType',
+      columnType: ColumnType.TRANSLATION_KEY,
+    },
+    fillMissingColors: false,
+    showAllLabels: true,
+    allLabelKeys: FRUIT_TYPE
+  }
 }
