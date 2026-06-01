3d63f0de9a521e841b8b0797c5d1ad7b1f896e2f
Kamil Nowak <kamil.nowak@capgemini.com>
2026-06-01 11:32:23 +0200

    test(react-utils): adapt withBaseProviders test to dynamicScoping changes; fix imports

--- libs/react-utils/src/lib/utils/withBaseProviders.test.tsx
+++ libs/react-utils/src/lib/utils/withBaseProviders.test.tsx
@@ -1,26 +1,27 @@
-import React, { type ReactNode } from 'react'
-import { render } from '@testing-library/react'
-import { withBaseProviders } from './withBaseProviders'
+const React = require('react')
+const { render } = require('@testing-library/react')
 
 jest.mock('@onecx/react-integration-interface', () => {
   const React = require('react')
   return {
-    AppStateProvider: ({ children }: { children?: ReactNode }) => React.createElement('div', { 'data-testid': 'app-state' }, children),
-    ConfigurationProvider: ({ children }: { children?: ReactNode }) => React.createElement('div', { 'data-testid': 'config' }, children),
-    UserProvider: ({ children }: { children?: ReactNode }) => React.createElement('div', { 'data-testid': 'user' }, children),
+    AppStateProvider: ({ children }) => React.createElement('div', { 'data-testid': 'app-state' }, children),
+    ConfigurationProvider: ({ children }) => React.createElement('div', { 'data-testid': 'config' }, children),
+    UserProvider: ({ children }) => React.createElement('div', { 'data-testid': 'user' }, children),
   }
-})
+}, { virtual: true })
 
 jest.mock('@onecx/react-webcomponents', () => {
   const React = require('react')
   return {
-    SyncedRouterProvider: ({ children }: { children?: ReactNode }) => React.createElement('div', { 'data-testid': 'router' }, children),
+    SyncedRouterProvider: ({ children }) => React.createElement('div', { 'data-testid': 'router' }, children),
   }
-})
+}, { virtual: true })
 
 jest.mock('./translationBridge', () => ({
   TranslationBridge: () => null,
 }))
+
+const { withBaseProviders } = require('./withBaseProviders')
 
 describe('withBaseProviders', () => {
   it('should wrap a component with all base providers', () => {
