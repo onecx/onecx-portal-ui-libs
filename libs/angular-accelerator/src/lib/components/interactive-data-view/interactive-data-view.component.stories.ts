bc67d7036572629b3b3cfbabf04cd1b425abc6a9
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2026-06-12 10:06:15 +0530

    fix: spelling error

--- libs/angular-accelerator/src/lib/components/interactive-data-view/interactive-data-view.component.stories.ts
+++ libs/angular-accelerator/src/lib/components/interactive-data-view/interactive-data-view.component.stories.ts
@@ -343,7 +343,7 @@ const CustomHeaderPerColumnLayouts: StoryFn<InteractiveDataViewComponent> = (arg
 })
 
 export const WithCustomHeaderPerColumnLayouts = {
-  name: 'Custom Header Distict Layouts Per Column',
+  name: 'Custom Header Distinct Layouts Per Column',
   render: CustomHeaderPerColumnLayouts,
   args: defaultComponentArgs,
 }
