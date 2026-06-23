db03aac9364d407a2bf322c0a47da71e2c385143
Maximilian Ollendorff <ollendorffmaximilian@gmail.com>
2026-06-23 11:30:14 +0200

    feat: add button css rules

--- libs/angular-utils/theme/primeng/src/utils/mapper/css-rules/usage-css-rules.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/css-rules/usage-css-rules.ts
@@ -1,8 +1,10 @@
 import type { CssRule } from '../mapper.types';
+import { buttonCssRules } from './usages/button.rules';
 import { carouselCssRules } from './usages/carousel.rules';
 import { datatableCssRules } from './usages/datatable.rules';
 
 export const usageCssRules: CssRule[] = [
+  ...buttonCssRules,
   ...carouselCssRules,
   ...datatableCssRules,
 ];
