fa72df7b9252cde7f795a8151f6d5cded85a1bfc
Jana <178573057+janaau@users.noreply.github.com>
2026-07-02 15:53:50 +0200

    feat: fixes

--- libs/angular-utils/theme/primeng/src/utils/mapper/theme-path.types.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/theme-path.types.ts
@@ -103,7 +103,7 @@ type Usages = NonNullable<RequiredThemeV2['usages']>;
  *
  * Constrains `MappingRule.from` / `CssDeclaration.from` so typos are caught
  * at compile time. Depth covers the deepest real path:
- * `usages.table.row.defaultStates.even.defaultState.cell.defaultState.border.width.top`.
+ * `usages.table.row.defaultState.even.defaultState.cell.defaultState.border.width.top`.
  */
 export type ThemePath =
   | `primitives.${LeafPaths<NonNullable<Primitives>>}`
