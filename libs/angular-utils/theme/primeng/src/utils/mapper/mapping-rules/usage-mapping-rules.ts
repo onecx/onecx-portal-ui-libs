120198eeb153bdf9165956fb450097be0a8fc0ce
Jana Auer <178573057+janaau@users.noreply.github.com>
2026-07-06 09:35:59 +0200

    feat: p- badge theming usages (#1551)
    * feat: first draft
    
    * feat: added missing rules
    
    * fix: remove adjustments
    
    * fix: remove skill.md
    
    * fix: revert changes
    
    * fix: revert changes
    
    * fix: package-lock changes
    
    * fix: review comments

--- libs/angular-utils/theme/primeng/src/utils/mapper/mapping-rules/usage-mapping-rules.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/mapping-rules/usage-mapping-rules.ts
@@ -1,4 +1,5 @@
 import type { MappingRule } from '../mapper.types';
+import { badgeMappingRules } from './usages/badge.rules';
 import { datatableMappingRules } from './usages/datatable.rules';
 import { dialogMappingRules } from './usages/dialog.rules';
 import { tooltipMappingRules } from './usages/tooltip.rules';
@@ -7,6 +8,7 @@ import { diagramMappingRules } from './usages/diagram.rules';
 import { carouselMappingRules } from './usages/carousel.rules';
 
 export const usageMappingRules: MappingRule[] = [
+  ...badgeMappingRules,
   ...datatableMappingRules,
   ...tooltipMappingRules,  
   ...dialogMappingRules,
