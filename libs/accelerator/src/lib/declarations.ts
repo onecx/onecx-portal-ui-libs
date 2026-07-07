c52994775f48f674db423c3e26e115abffeeec25
Jan-Gerrit Schettler-Koehler <62466958+SchettlerKoehler@users.noreply.github.com>
2026-07-07 14:16:55 +0200

    feat: Added AiContextGatherer
    * feat: Added AiContextGatherer
    
    * feat: Add docs for AIContextGatherer

--- libs/accelerator/src/lib/declarations.ts
+++ libs/accelerator/src/lib/declarations.ts
@@ -1,6 +1,8 @@
 import { ensureProperty } from './utils/ensure-property.utils'
 
-const topicUseBroadcastChannel = ensureProperty(globalThis as object, ['@onecx/accelerator', 'topic', 'useBroadcastChannel'], 'V2' as 'V2' | boolean)
+const gatherPromises = ensureProperty(globalThis as object, ['@onecx/accelerator', 'gatherer', 'promises'], {} as Record<number, Array<Promise<any>>>)
+const gatherDebug = ensureProperty(gatherPromises, ['@onecx/accelerator', 'gatherer', 'debug'], [] as string[])
+const topicUseBroadcastChannel = ensureProperty(gatherDebug, ['@onecx/accelerator', 'topic', 'useBroadcastChannel'], 'V2' as 'V2' | boolean)
 const topicInitDate = ensureProperty(topicUseBroadcastChannel, ['@onecx/accelerator', 'topic', 'initDate'], Date.now())
 const topicTabId = ensureProperty(topicInitDate, ['@onecx/accelerator', 'topic', 'tabId'], Math.ceil(globalThis.performance?.now?.() ?? 0))
 
