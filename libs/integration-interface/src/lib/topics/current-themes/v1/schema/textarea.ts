eb56f3cea1e08e7feca28bfa6eff63437a64f00d
Jana <178573057+janaau@users.noreply.github.com>
2026-07-13 09:22:18 +0200

    fix: usage structure

--- libs/integration-interface/src/lib/topics/current-themes/v1/schema/textarea.ts
+++ libs/integration-interface/src/lib/topics/current-themes/v1/schema/textarea.ts
@@ -19,92 +19,125 @@ export const textareaSize = z
 export const textarea = z
   .object({
     settings: (textareaSettings as typeof textareaSettings).optional(),
-    default: z
+    defaultVariant: z
       .object({
-        background: z
-          .union([bg, withRef(z.string())])
-          .default('{{primitives.area.surface.defaultState.defaultVariant.bg}}'),
-        color: color.default('{{primitives.area.surface.defaultState.defaultVariant.contrast}}'),
-        border: color.default("{{primitives.area.surface.defaultState.defaultVariant.color}}"),
-        placeholderColor: color.default('{{primitives.area.surface.defaultState.defaultVariant.contrast}}'),
-        hover: z
-          .object({
-            borderColor: color.default("{{primitives.area.surface.state.hover.defaultVariant.border.defaultVariant.color}}"),
-          })
-          .optional(),
-        focus: z
-          .object({
-            borderColor: color.default("{{primitives.area.surface.state.focus.defaultVariant.border.defaultVariant.color}}"),
-            focusRing: (borderWithShadow as typeof borderWithShadow).optional(),
-          })
-          .optional(),
-        invalid: z
-          .object({
-            borderColor: color.default("{{primitives.area.surface.state.invalid.defaultVariant.border.defaultVariant.color}}"),
-            placeholderColor: color.default("{{primitives.area.surface.state.invalid.defaultVariant.contrast}}"),
-          })
-          .optional(),
-        disabled: z
+        defaultState: z
           .object({
             background: z
               .union([bg, withRef(z.string())])
-              .default('{{primitives.variant.primary.state.disabled.defaultVariant.bg}}'),
-            color: color.default("{{primitives.area.surface.state.disabled.defaultVariant.contrast}}"),
-            borderColor: color.default("{{primitives.area.surface.state.disabled.defaultVariant.border.defaultVariant.color}}"),
+              .default('{{primitives.defaultVariant.defaultState.defaultVariant.bg}}'),
+            color: color.default('{{primitives.defaultVariant.defaultState.defaultVariant.contrast}}'),
+            placeholderColor: color.default('{{primitives.defaultVariant.defaultState.defaultVariant.contrast}}'),
+            border: {
+              color: color.default('{{primitives.defaultVariant.defaultState.defaultVariant.border.defaultVariant.color}}'),
+              radius: withRef(z.string()).default("{{primitives.radius.md}}"),
+            },
+            shadow: withRef(z.string()).default("{{primitives.shadow.none}}"),
+            paddingX: withRef(z.string()).default("{{primitives.space.sm}}"),
+            paddingY: withRef(z.string()).default("{{primitives.space.sm}}"),
+            transitionDuration: withRef(z.number()).default("{{primitives.transition.duration}}"),
+            sm: (textareaSize as typeof textareaSize).optional().default({
+              fontSize: "{{primitives.font.size}}",
+              paddingX: "{{primitives.space.xs}}",
+              paddingY: "{{primitives.space.xs}}",
+            }),
+            lg: (textareaSize as typeof textareaSize).optional().default({
+              fontSize: "{{primitives.font.size}}",
+              paddingX: "{{primitives.space.md}}",
+              paddingY: "{{primitives.space.md}}",
+            }),
+          })
+          .optional(),
+        state: z
+          .object({
+            hover: z
+              .object({
+                borderColor: color.default("{{primitives.defaultVariant.state.hover.defaultVariant.border.defaultVariant.color}}"),
+              })
+              .optional(),
+            focus: z
+              .object({
+                borderColor: color.default("{{primitives.defaultVariant.state.focus.defaultVariant.border.defaultVariant.color}}"),
+                focusRing: (borderWithShadow as typeof borderWithShadow).optional(),
+              })
+              .optional(),
+            invalid: z
+              .object({
+                borderColor: color.default("{{primitives.defaultVariant.state.invalid.defaultVariant.border.defaultVariant.color}}"),
+                placeholderColor: color.default("{{primitives.defaultVariant.state.invalid.defaultVariant.contrast}}"),
+              })
+              .optional(),
+            disabled: z
+              .object({
+                background: z
+                  .union([bg, withRef(z.string())])
+                  .default('{{primitives.defaultVariant.state.disabled.defaultVariant.bg}}'),
+                color: color.default("{{primitives.defaultVariant.state.disabled.defaultVariant.contrast}}"),
+                borderColor: color.default("{{primitives.defaultVariant.state.disabled.defaultVariant.border.defaultVariant.color}}"),
+              })
+              .optional(),
           })
           .optional(),
       })
       .optional(),
-    filled: z
+    filledVariant: z
       .object({
-        background: z
-          .union([bg, withRef(z.string())])
-          .default('{{primitives.variant.primary.defaultState.defaultVariant.bg}}'),
-        hover: z
+        defaultState: z
           .object({
             background: z
               .union([bg, withRef(z.string())])
-              .default('{{primitives.variant.primary.state.hover.defaultVariant.bg}}'),
+              .default('{{primitives.variant.primary.defaultState.defaultVariant.bg}}'),
+            color: color.default('{{primitives.variant.primary.defaultState.defaultVariant.contrast}}'),
+            placeholderColor: color.default('{{primitives.variant.primary.defaultState.defaultVariant.contrast}}'),
+            border: {
+              color: color.default('{{primitives.variant.primary.defaultState.defaultVariant.border.defaultVariant.color}}'),
+              radius: withRef(z.string()).default("{{primitives.radius.md}}"),
+            },
+            shadow: withRef(z.string()).default("{{primitives.shadow.none}}"),
+            paddingX: withRef(z.string()).default("{{primitives.space.sm}}"),
+            paddingY: withRef(z.string()).default("{{primitives.space.sm}}"),
+            transitionDuration: withRef(z.number()).default("{{primitives.transition.duration}}"),
+            sm: (textareaSize as typeof textareaSize).optional().default({
+              fontSize: "{{primitives.font.size}}",
+              paddingX: "{{primitives.space.xs}}",
+              paddingY: "{{primitives.space.xs}}",
+            }),
+            lg: (textareaSize as typeof textareaSize).optional().default({
+              fontSize: "{{primitives.font.size}}",
+              paddingX: "{{primitives.space.md}}",
+              paddingY: "{{primitives.space.md}}",
+            }),
           })
           .optional(),
-        focus: z
+        state: z
           .object({
-            background: z
-              .union([bg, withRef(z.string())])
-              .default('{{primitives.variant.primary.state.focus.defaultVariant.bg}}'),
-          })
-          .optional(),
-        disabled: z
-          .object({
-            background: z
-              .union([bg, withRef(z.string())])
-              .default('{{primitives.variant.primary.state.disabled.defaultVariant.bg}}'),
+            hover: z
+              .object({
+                background: z
+                  .union([bg, withRef(z.string())])
+                  .default('{{primitives.variant.primary.state.hover.defaultVariant.bg}}'),
+                borderColor: color.default("{{primitives.variant.primary.state.hover.defaultVariant.border.defaultVariant.color}}"),
+              })
+              .optional(),
+            focus: z
+              .object({
+                background: z
+                  .union([bg, withRef(z.string())])
+                  .default('{{primitives.variant.primary.state.focus.defaultVariant.bg}}'),
+                focusRing: (borderWithShadow as typeof borderWithShadow).optional(),
+              })
+              .optional(),
+            disabled: z
+              .object({
+                background: z
+                  .union([bg, withRef(z.string())])
+                  .default('{{primitives.variant.primary.state.disabled.defaultVariant.bg}}'),
+                borderColor: color.default("{{primitives.variant.primary.state.disabled.defaultVariant.border.defaultVariant.color}}"),
+                })
+              .optional(),
           })
           .optional(),
       })
       .optional(),
-
-    shadow: withRef(z.string()).default("{{primitives.shadow.none}}"),
-    paddingX: withRef(z.string()).default("{{primitives.space.sm}}"),
-    paddingY: withRef(z.string()).default("{{primitives.space.sm}}"),
-    borderRadius: withRef(z.string()).default("{{primitives.radius.md}}"),
-    focusRing: borderWithShadow.default({
-      width: "{{primitives.focusRing.width}}",
-      style: "{{primitives.focusRing.style}}",
-      color: "{{primitives.focusRing.color}}",
-      offset: "{{primitives.focusRing.offset}}",
-      shadow: "{{primitives.focusRing.shadow}}",
-    }),
-    transitionDuration: withRef(z.number()).default("{{primitives.transition.duration}}"),
-    sm: (textareaSize as typeof textareaSize).optional().default({
-      fontSize: "{{primitives.font.size}}",
-      paddingX: "{{primitives.space.xs}}",
-      paddingY: "{{primitives.space.xs}}",
-    }),
-    lg: (textareaSize as typeof textareaSize).optional().default({
-      fontSize: "{{primitives.font.size}}",
-      paddingX: "{{primitives.space.md}}",
-      paddingY: "{{primitives.space.md}}",
-    }),
   })
   .register(themeSchemaRegistry, { id: "textarea" });
