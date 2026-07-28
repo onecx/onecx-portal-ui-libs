f16e9f3549e5312854d81b8f56bd2623371ab59a
Priyanka <priyanka.sandeep-rade@capgemini.com>
2026-07-28 13:16:20 +0000

    chore: add toColorString

--- libs/angular-utils/theme/primeng/src/utils/mapper/mapping-rules/usages/message.rules.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/mapping-rules/usages/message.rules.ts
@@ -1,4 +1,5 @@
 import { MappingRule } from '../../mapper.types'
+import { toColorString } from '../../mapper.utils'
 
 const closeButton_default: MappingRule[] = [
   {
@@ -49,15 +50,7 @@ const outlined: MappingRule[] = [
   {
     from: 'usages.message.info.outlined.color',
     to: 'components.message.info.outlined.color',
-  },
-  {
-    from: 'usages.message.info.outlined.border.color',
-    to: 'components.message.info.outlined.borderColor',
-  },
-  // -- info simple --
-  {
-    from: 'usages.message.info.simple.color',
-    to: 'components.message.info.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -84,18 +77,6 @@ const root: MappingRule[] = [
   },
 
   //------ info ------
-  {
-    from: 'usages.message.info.background.color',
-    to: 'components.message.info.background',
-  },
-  {
-    from: 'usages.message.info.border.color',
-    to: 'components.message.info.borderColor',
-  },
-  {
-    from: 'usages.message.info.color',
-    to: 'components.message.info.color',
-  },
   {
     from: 'usages.message.info.border.shadow',
     to: 'components.message.info.shadow',
@@ -159,10 +140,12 @@ const closeButton_info: MappingRule[] = [
   {
     from: 'usages.message.info.close.button.hover.background.color',
     to: 'components.message.info.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.info.close.button.focus.focusRing.color',
     to: 'components.message.info.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.info.close.button.focus.focusRing.shadow',
@@ -174,10 +157,12 @@ const closeButton_success: MappingRule[] = [
   {
     from: 'usages.message.success.close.button.hover.background.color',
     to: 'components.message.success.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.close.button.focus.focusRing.color',
     to: 'components.message.success.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.close.button.focus.focusRing.shadow',
@@ -189,10 +174,12 @@ const closeButton_warn: MappingRule[] = [
   {
     from: 'usages.message.warning.close.button.hover.background.color',
     to: 'components.message.warn.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.close.button.focus.focusRing.color',
     to: 'components.message.warn.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.close.button.focus.focusRing.shadow',
@@ -204,10 +191,12 @@ const closeButton_error: MappingRule[] = [
   {
     from: 'usages.message.error.close.button.hover.background.color',
     to: 'components.message.error.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.close.button.focus.focusRing.color',
     to: 'components.message.error.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.close.button.focus.focusRing.shadow',
@@ -219,10 +208,12 @@ const closeButton_secondary: MappingRule[] = [
   {
     from: 'usages.message.secondary.close.button.hover.background.color',
     to: 'components.message.secondary.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.close.button.focus.focusRing.color',
     to: 'components.message.secondary.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.close.button.focus.focusRing.shadow',
@@ -234,10 +225,12 @@ const closeButton_contrast: MappingRule[] = [
   {
     from: 'usages.message.contrast.close.button.hover.background.color',
     to: 'components.message.contrast.closeButton.hoverBackground',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.close.button.focus.focusRing.color',
     to: 'components.message.contrast.closeButton.focusRing.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.close.button.focus.focusRing.shadow',
@@ -249,14 +242,17 @@ const message_success_container: MappingRule[] = [
   {
     from: 'usages.message.success.background.color',
     to: 'components.message.success.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.border.color',
     to: 'components.message.success.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.color',
     to: 'components.message.success.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.border.shadow',
@@ -266,16 +262,19 @@ const message_success_container: MappingRule[] = [
   {
     from: 'usages.message.success.outlined.color',
     to: 'components.message.success.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.success.outlined.border.color',
     to: 'components.message.success.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.success.simple.color',
     to: 'components.message.success.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -284,14 +283,17 @@ const message_warn_container: MappingRule[] = [
   {
     from: 'usages.message.warning.background.color',
     to: 'components.message.warn.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.border.color',
     to: 'components.message.warn.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.color',
     to: 'components.message.warn.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.border.shadow',
@@ -301,16 +303,19 @@ const message_warn_container: MappingRule[] = [
   {
     from: 'usages.message.warning.outlined.color',
     to: 'components.message.warn.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.warning.outlined.border.color',
     to: 'components.message.warn.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.warning.simple.color',
     to: 'components.message.warn.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -320,14 +325,17 @@ const message_error_container: MappingRule[] = [
   {
     from: 'usages.message.error.background.color',
     to: 'components.message.error.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.border.color',
     to: 'components.message.error.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.color',
     to: 'components.message.error.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.border.shadow',
@@ -337,16 +345,19 @@ const message_error_container: MappingRule[] = [
   {
     from: 'usages.message.error.outlined.color',
     to: 'components.message.error.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.error.outlined.border.color',
     to: 'components.message.error.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.error.simple.color',
     to: 'components.message.error.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -356,33 +367,29 @@ const message_info_container: MappingRule[] = [
   {
     from: 'usages.message.info.background.color',
     to: 'components.message.info.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.info.border.color',
     to: 'components.message.info.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.info.color',
     to: 'components.message.info.color',
-  },
-  {
-    from: 'usages.message.info.border.shadow',
-    to: 'components.message.info.shadow',
-  },
-  // -- outlined --
-  {
-    from: 'usages.message.info.outlined.color',
-    to: 'components.message.info.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.info.outlined.border.color',
     to: 'components.message.info.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.info.simple.color',
     to: 'components.message.info.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -392,14 +399,17 @@ const message_secondary_container: MappingRule[] = [
   {
     from: 'usages.message.secondary.background.color',
     to: 'components.message.secondary.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.border.color',
     to: 'components.message.secondary.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.color',
     to: 'components.message.secondary.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.border.shadow',
@@ -409,16 +419,19 @@ const message_secondary_container: MappingRule[] = [
   {
     from: 'usages.message.secondary.outlined.color',
     to: 'components.message.secondary.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.secondary.outlined.border.color',
     to: 'components.message.secondary.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.secondary.simple.color',
     to: 'components.message.secondary.simple.color',
+    transform: toColorString,
   },
 ]
 
@@ -428,14 +441,17 @@ const message_contrast_container: MappingRule[] = [
   {
     from: 'usages.message.contrast.background.color',
     to: 'components.message.contrast.background',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.border.color',
     to: 'components.message.contrast.borderColor',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.color',
     to: 'components.message.contrast.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.border.shadow',
@@ -445,16 +461,19 @@ const message_contrast_container: MappingRule[] = [
   {
     from: 'usages.message.contrast.outlined.color',
     to: 'components.message.contrast.outlined.color',
+    transform: toColorString,
   },
   {
     from: 'usages.message.contrast.outlined.border.color',
     to: 'components.message.contrast.outlined.borderColor',
+    transform: toColorString,
   },
 
   // -- simple --
   {
     from: 'usages.message.contrast.simple.color',
     to: 'components.message.contrast.simple.color',
+    transform: toColorString,
   },
 ]
 
