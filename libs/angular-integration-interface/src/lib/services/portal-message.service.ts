4d1bd101d432265eb569c03bbe46798915106068
syedsuah <316155313+syedsuah@users.noreply.github.com>
2026-08-13 14:16:52 +0530

    fix: v7-portal-message-service to warn (#1641)
    Co-authored-by: Prathamesh Malavi <239937892+prathamMalavi@users.noreply.github.com>

--- libs/angular-integration-interface/src/lib/services/portal-message.service.ts
+++ libs/angular-integration-interface/src/lib/services/portal-message.service.ts
@@ -45,7 +45,7 @@ export class PortalMessageService implements OnDestroy {
   }
 
   warning(msg: Message) {
-    this.addTranslated('warning', msg)
+    this.addTranslated('warn', msg)
   }
 
   private addTranslated(severity: string, msg: Message) {
