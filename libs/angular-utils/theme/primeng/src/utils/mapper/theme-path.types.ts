947ba518f0f519105eeea976f3506258bcee3ac8
Prathamesh Malavi <239937892+prathamMalavi@users.noreply.github.com>
2026-09-22 13:30:23 +0530

    feat: test ci: Remove unused usages from theme-path types

--- libs/angular-utils/theme/primeng/src/utils/mapper/theme-path.types.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/theme-path.types.ts
@@ -118,13 +118,7 @@ export type ThemePath =
   | `usages.accordion.${LeafPaths<NonNullable<Usages['accordion']>>}`
   | `usages.message.${LeafPaths<NonNullable<Usages['message']>>}`
   | `usages.selectbutton.${LeafPaths<NonNullable<Usages['selectbutton']>>}`
-  | `usages.content.${LeafPaths<NonNullable<Usages['content']>>}`
-  | `usages.loadingIndicator.${LeafPaths<NonNullable<Usages['loadingIndicator']>>}`
-  | `usages.panelmenu.${LeafPaths<NonNullable<Usages['panelmenu']>>}`
-  | `usages.menu.${LeafPaths<NonNullable<Usages['menu']>>}`
-  | `usages.breadcrumb.${LeafPaths<NonNullable<Usages['breadcrumb']>>}`
-  | `usages.pageHeader.${LeafPaths<NonNullable<Usages['pageHeader']>>}`
-  | `usages.dataview.${LeafPaths<NonNullable<Usages['dataview']>>}`
+  | `usages.content.${LeafPaths<NonNullable<Usages['content']>>}``
 // ─── Preset Paths (to) ───────────────────────────────────────────────────────
 
 /**
