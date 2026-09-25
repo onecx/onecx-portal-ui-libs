801c3b807f01d186b5a8bc10691d6f23e4b142ac
priyankaSandeepRade <priyanka.sandeep-rade@capgemini.com>
2026-09-25 10:27:51 +0000

    fix: build

--- libs/angular-utils/theme/primeng/src/utils/mapper/css-rules/usages/diagram.rules.ts
+++ libs/angular-utils/theme/primeng/src/utils/mapper/css-rules/usages/diagram.rules.ts
@@ -86,106 +86,16 @@ const SELECT_BUTTON_ROOT: CssRule = {
       property: 'border-radius',
       from: 'usages.diagram.selectButton.border.radius',
     },
-  ],
-}
-
-const SELECT_BUTTON_ICON: CssRule = {
-  selector: 'ocx-diagram .p-selectbutton .p-button-icon',
-  declarations: [
     {
-      property: 'color',
-      from: 'usages.diagram.selectButton.button.icon.color',
+      property: 'padding-left',
+      from: 'usages.diagram.selectButton.paddingX',
+    },
+    {
+      property: 'padding-right',
+      from: 'usages.diagram.selectButton.paddingY',
     },
   ],
 }
-
-const SELECT_BUTTON: CssRule = {
-  selector: 'ocx-diagram .p-selectbutton .p-togglebutton-content',
-  declarations: [
-    {
-      property: 'background-color',
-      from: 'usages.diagram.selectButton.button.background',
-    },
-    {
-      property: 'border-color',
-      from: 'usages.diagram.selectButton.button.border.color',
-    },
-    {
-      property: 'color',
-      from: 'usages.diagram.selectButton.button.color',
-    },
-  ],
-}
-
-const SELECT_BUTTON_STATES_HOVER: CssRule = {
-  selector: 'ocx-diagram .p-selectbutton .p-togglebutton:hover .p-togglebutton-content',
-  declarations: [
-    {
-      property: 'background-color',
-      from: 'usages.diagram.selectButton.button.hover.background',
-    },
-    {
-      property: 'color',
-      from: 'usages.diagram.selectButton.button.hover.color',
-    },
-    {
-      property: 'border-color',
-      from: 'usages.diagram.selectButton.button.hover.border.color',
-    }
-  ]
-}
-
-const SELECT_BUTTON_STATES_SELECTED: CssRule = {
-  selector: 'ocx-diagram .p-selectbutton .p-togglebutton-checked .p-togglebutton-content',
-  declarations: [
-    {
-      property: 'background-color',
-      from: 'usages.diagram.selectButton.button.selected.background',
-    },
-    {
-      property: 'color',
-      from: 'usages.diagram.selectButton.button.selected.color',
-    },
-    {
-      property: 'border-color',
-      from: 'usages.diagram.selectButton.button.selected.border.color',
-    }
-  ]
-}
-
-const SELECT_BUTTON_STATES_FOCUS: CssRule = {
-  selector: 'ocx-diagram .p-selectbutton .p-togglebutton:focus-within .p-togglebutton-content',
-  declarations: [
-    {
-      property: 'background-color',
-      from: 'usages.diagram.selectButton.button.focus.background',
-    },
-    {
-      property: 'color',
-      from: 'usages.diagram.selectButton.button.focus.color',
-    },
-    {
-      property: 'border-color',
-      from: 'usages.diagram.selectButton.button.focus.border.color',
-    }
-  ]
-}
-
-const SELECT_BUTTON_STATE_ICONS: CssRule[] = [
-  {
-    selector: 'ocx-diagram .p-selectbutton .p-togglebutton:hover .p-button-icon',
-    declarations: [{ property: 'color', from: 'usages.diagram.selectButton.button.hover.color' }],
-  },
-  {
-    selector: 'ocx-diagram .p-selectbutton .p-togglebutton-checked .p-button-icon',
-    declarations: [{ property: 'color', from: 'usages.diagram.selectButton.button.selected.color' }],
-  },
-  {
-    selector: 'ocx-diagram .p-selectbutton .p-togglebutton:focus-within .p-button-icon',
-    declarations: [{ property: 'color', from: 'usages.diagram.selectButton.button.focus.color' }],
-  },
-]
-
 const FOOTER: CssRule = {
   selector: 'ocx-diagram .footer',
   declarations: [
@@ -209,11 +119,5 @@ export const diagramCssRules: CssRule[] = [
   HEADER,
   DESCRIPTION,
   SELECT_BUTTON_ROOT,
-  SELECT_BUTTON_ICON,
-  SELECT_BUTTON,
-  SELECT_BUTTON_STATES_HOVER,
-  SELECT_BUTTON_STATES_SELECTED,
-  SELECT_BUTTON_STATES_FOCUS,
-  ...SELECT_BUTTON_STATE_ICONS,
   FOOTER,
 ]
